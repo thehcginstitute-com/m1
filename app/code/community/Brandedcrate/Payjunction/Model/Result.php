7b5406ad4421adc6904acfe295ee24905676d090
Dmitrii Fediuk <dfediuk@gmail.com>
2023-12-15 14:10:26 +0300

    https://github.com/thehcginstitute-com/m1/issues/49

--- app/code/community/Brandedcrate/Payjunction/Model/Result.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-class Brandedcrate_Payjunction_Model_Result extends Varien_Object
-{
-}
--- app/code/community/Brandedcrate/Payjunction/client/test/bootstrap.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-// require_once('lib/BrandedCrate/PayJunction/Client.php');
-// require_once('lib/BrandedCrate/PayJunction/TransactionClient.php');
-// require_once('lib/BrandedCrate/PayJunction/ReceiptClient.php');
-// require_once('lib/BrandedCrate/PayJunction/CustomerClient.php');
