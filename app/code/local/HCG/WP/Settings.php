7fd6858c441c0f2fff163debaab6391b16c401e2
Dmitrii Fediuk <dfediuk@gmail.com>
2023-12-24 19:56:00 +0300

    https://github.com/thehcginstitute-com/m1/issues/63

--- app/code/local/HCG/WP/Settings.php
+++ app/code/local/HCG/WP/Settings.php
@@ -5,6 +5,7 @@ final class HCG_WP_Settings {
 	 * 2023-12-24
 	 * "The main logo link address should not be hardcoded": https://github.com/thehcginstitute-com/m1/issues/72
 	 * @used-by app/design/frontend/default/mobileshoppe/template/page/html/header.phtml
+	 * @used-by app/design/frontend/default/mobileshoppe/template/page/html/topmenu.phtml
 	 */
 	static function url():string {return
 		(trim(Mage::getStoreConfig('hcg_wp/primary/url'), '/ ') ?: 'https://www.thehcginstitute.com') . '/'
