9b50a19cda9688fb0a61cd24aadf9c9a8ea89345
Dmitrii Fediuk <dfediuk@gmail.com>
2023-12-30 03:17:51 +0300

    https://github.com/thehcginstitute-com/m1/issues/42

--- skin/frontend/default/default/js/mgt_lazy_image_loader/common.js
+++
@@ -1,7 +0,0 @@
-$.noConflict();
-jQuery(document).ready(function($) {
-  jQuery('img.lazy').jail({
-    event: 'load+scroll',
-    placeholder : "/skin/frontend/default/default/images/mgt_lazy_image_loader/loader.gif",
-  });
-});
