a6ee3db5ad17782d29c203a1c4eb5ef0c5ff747d
Dmitrii Fediuk <dfediuk@gmail.com>
2024-01-04 08:07:12 +0000

    https://github.com/thehcginstitute-com/m1/issues/85

--- app/code/community/IWD/OnepageCheckoutSignature/Model/Order/Pdf/Invoice.php
+++ app/code/community/IWD/OnepageCheckoutSignature/Model/Order/Pdf/Invoice.php
@@ -97,348 +97,348 @@ class IWD_OnepageCheckoutSignature_Model_Order_Pdf_Invoice extends Mage_Sales_Mo
 		$this->y -= 20;
 	}
 	
-    
-    /**
-     * Return PDF document
-     *
-     * @param  array $invoices
-     * @return Zend_Pdf
-     */
+
+	/**
+	 * Return PDF document
+	 *
+	 * @param  array $invoices
+	 * @return Zend_Pdf
+	 */
 	public function getPdf($invoices = array())
-    {	if(Mage::getVersion()>='1.7')
-   		 {   $this->_beforeGetPdf();
-	        $this->_initRenderer('invoice');
+	{	if(Mage::getVersion()>='1.7')
+		 {   $this->_beforeGetPdf();
+			$this->_initRenderer('invoice');
 	
-	        $pdf = new Zend_Pdf();
-	        $this->_setPdf($pdf);
-	        $style = new Zend_Pdf_Style();
-	        $this->_setFontBold($style, 10);
+			$pdf = new Zend_Pdf();
+			$this->_setPdf($pdf);
+			$style = new Zend_Pdf_Style();
+			$this->_setFontBold($style, 10);
 	
-	        foreach ($invoices as $invoice) {
-	            if ($invoice->getStoreId()) {
-	                Mage::app()->getLocale()->emulate($invoice->getStoreId());
-	                Mage::app()->setCurrentStore($invoice->getStoreId());
-	            }
-	            $page  = $this->newPage();
-	            $order = $invoice->getOrder();
-	            /* Add image */
-	            $this->insertLogo($page, $invoice->getStore());
-	            /* Add address */
-	            $this->insertAddress($page, $invoice->getStore());
-	            /* Add head */
-	            $this->insertOrder(
-	                $page,
-	                $order,
-	                Mage::getStoreConfigFlag(self::XML_PATH_SALES_PDF_INVOICE_PUT_ORDER_ID, $order->getStoreId())
-	            );
-	            $this->insertSignature($page, $order);
-	            /* Add document text and number */
-	           
-	            $this->insertDocumentNumber(
-	                $page,
-	                Mage::helper('sales')->__('Invoice # ') . $invoice->getIncrementId()
-	            );
-	            /* Add table */
-	            $this->_drawHeader($page);
-		            /* Add body */
-	            foreach ($invoice->getAllItems() as $item){
-	                if ($item->getOrderItem()->getParentItem()) {
-	                    continue;
-	                }
-	                /* Draw item */
-	                $this->_drawItem($item, $page, $order);
-	                $page = end($pdf->pages);
-	            }
-	            /* Add totals */
-	            $this->insertTotals($page, $invoice);
-	            if ($invoice->getStoreId()) {
-	                Mage::app()->getLocale()->revert();
-	            }
-	        } 
-	        $this->_afterGetPdf();
-	        return $pdf;
-    }
-    else
-    {
-    	
-    	$this->_beforeGetPdf();
-    	$this->_initRenderer('invoice');
-    	
-    	$pdf = new Zend_Pdf();
-    	$this->_setPdf($pdf);
-    	$style = new Zend_Pdf_Style();
-    	$this->_setFontBold($style, 10);
-    	
-    	foreach ($invoices as $invoice) {
-    		if ($invoice->getStoreId()) {
-    			Mage::app()->getLocale()->emulate($invoice->getStoreId());
-    			Mage::app()->setCurrentStore($invoice->getStoreId());
-    		}
-    		$page = $pdf->newPage(Zend_Pdf_Page::SIZE_A4);
-    		$pdf->pages[] = $page;
-    	
-    		$order = $invoice->getOrder();
-    	
-    		/* Add image */
-    		$this->insertLogo($page, $invoice->getStore());
-    	
-    		/* Add address */
-    		$this->insertAddress($page, $invoice->getStore());
-    		
-    		/* Add head */
-    		$this->insertOrder($page, $order, Mage::getStoreConfigFlag(self::XML_PATH_SALES_PDF_INVOICE_PUT_ORDER_ID, $order->getStoreId()));
-    	
-    		$this->insertSignature($page, $order);
-    		$page->setFillColor(new Zend_Pdf_Color_GrayScale(1));
-    		$this->_setFontRegular($page);
-    		$page->drawText(Mage::helper('sales')->__('Invoice # ') . $invoice->getIncrementId(), 35, 780, 'UTF-8');
-    	
-    		/* Add table */
-    		$page->setFillColor(new Zend_Pdf_Color_RGB(0.93, 0.92, 0.92));
-    		$page->setLineColor(new Zend_Pdf_Color_GrayScale(0.5));
-    		$page->setLineWidth(0.5);
-    	
-    		$page->drawRectangle(25, $this->y, 570, $this->y -15);
-    		$this->y -=10;
-    	
-    		/* Add table head */
-    		$page->setFillColor(new Zend_Pdf_Color_RGB(0.4, 0.4, 0.4));
-    		$page->drawText(Mage::helper('sales')->__('Products'), 35, $this->y, 'UTF-8');
-    		$page->drawText(Mage::helper('sales')->__('SKU'), 255, $this->y, 'UTF-8');
-    		$page->drawText(Mage::helper('sales')->__('Price'), 380, $this->y, 'UTF-8');
-    		$page->drawText(Mage::helper('sales')->__('Qty'), 430, $this->y, 'UTF-8');
-    		$page->drawText(Mage::helper('sales')->__('Tax'), 480, $this->y, 'UTF-8');
-    		$page->drawText(Mage::helper('sales')->__('Subtotal'), 535, $this->y, 'UTF-8');
-    	
-    		$this->y -=15;
-    	
-    		$page->setFillColor(new Zend_Pdf_Color_GrayScale(0));
-    	
-    		/* Add body */
-    		foreach ($invoice->getAllItems() as $item){
-    			if ($item->getOrderItem()->getParentItem()) {
-    				continue;
-    			}
-    	
-    			if ($this->y < 15) {
-    				$page = $this->newPage(array('table_header' => true));
-    			}
-    	
-    			/* Draw item */
-    			$page = $this->_drawItem($item, $page, $order);
-    		}
-    	
-    		/* Add totals */
-    		$page = $this->insertTotals($page, $invoice);
-    	
-    		if ($invoice->getStoreId()) {
-    			Mage::app()->getLocale()->revert();
-    		}
-    	}
-    	
-    	$this->_afterGetPdf();
-    	
-    	return $pdf;
-    }
+			foreach ($invoices as $invoice) {
+				if ($invoice->getStoreId()) {
+					Mage::app()->getLocale()->emulate($invoice->getStoreId());
+					Mage::app()->setCurrentStore($invoice->getStoreId());
+				}
+				$page  = $this->newPage();
+				$order = $invoice->getOrder();
+				/* Add image */
+				$this->insertLogo($page, $invoice->getStore());
+				/* Add address */
+				$this->insertAddress($page, $invoice->getStore());
+				/* Add head */
+				$this->insertOrder(
+					$page,
+					$order,
+					Mage::getStoreConfigFlag(self::XML_PATH_SALES_PDF_INVOICE_PUT_ORDER_ID, $order->getStoreId())
+				);
+				$this->insertSignature($page, $order);
+				/* Add document text and number */
+
+				$this->insertDocumentNumber(
+					$page,
+					Mage::helper('sales')->__('Invoice # ') . $invoice->getIncrementId()
+				);
+				/* Add table */
+				$this->_drawHeader($page);
+					/* Add body */
+				foreach ($invoice->getAllItems() as $item){
+					if ($item->getOrderItem()->getParentItem()) {
+						continue;
+					}
+					/* Draw item */
+					$this->_drawItem($item, $page, $order);
+					$page = end($pdf->pages);
+				}
+				/* Add totals */
+				$this->insertTotals($page, $invoice);
+				if ($invoice->getStoreId()) {
+					Mage::app()->getLocale()->revert();
+				}
+			}
+			$this->_afterGetPdf();
+			return $pdf;
+	}
+	else
+	{
+
+		$this->_beforeGetPdf();
+		$this->_initRenderer('invoice');
+
+		$pdf = new Zend_Pdf();
+		$this->_setPdf($pdf);
+		$style = new Zend_Pdf_Style();
+		$this->_setFontBold($style, 10);
+
+		foreach ($invoices as $invoice) {
+			if ($invoice->getStoreId()) {
+				Mage::app()->getLocale()->emulate($invoice->getStoreId());
+				Mage::app()->setCurrentStore($invoice->getStoreId());
+			}
+			$page = $pdf->newPage(Zend_Pdf_Page::SIZE_A4);
+			$pdf->pages[] = $page;
+
+			$order = $invoice->getOrder();
+
+			/* Add image */
+			$this->insertLogo($page, $invoice->getStore());
+
+			/* Add address */
+			$this->insertAddress($page, $invoice->getStore());
+
+			/* Add head */
+			$this->insertOrder($page, $order, Mage::getStoreConfigFlag(self::XML_PATH_SALES_PDF_INVOICE_PUT_ORDER_ID, $order->getStoreId()));
+
+			$this->insertSignature($page, $order);
+			$page->setFillColor(new Zend_Pdf_Color_GrayScale(1));
+			$this->_setFontRegular($page);
+			$page->drawText(Mage::helper('sales')->__('Invoice # ') . $invoice->getIncrementId(), 35, 780, 'UTF-8');
+
+			/* Add table */
+			$page->setFillColor(new Zend_Pdf_Color_RGB(0.93, 0.92, 0.92));
+			$page->setLineColor(new Zend_Pdf_Color_GrayScale(0.5));
+			$page->setLineWidth(0.5);
+
+			$page->drawRectangle(25, $this->y, 570, $this->y -15);
+			$this->y -=10;
+
+			/* Add table head */
+			$page->setFillColor(new Zend_Pdf_Color_RGB(0.4, 0.4, 0.4));
+			$page->drawText(Mage::helper('sales')->__('Products'), 35, $this->y, 'UTF-8');
+			$page->drawText(Mage::helper('sales')->__('SKU'), 255, $this->y, 'UTF-8');
+			$page->drawText(Mage::helper('sales')->__('Price'), 380, $this->y, 'UTF-8');
+			$page->drawText(Mage::helper('sales')->__('Qty'), 430, $this->y, 'UTF-8');
+			$page->drawText(Mage::helper('sales')->__('Tax'), 480, $this->y, 'UTF-8');
+			$page->drawText(Mage::helper('sales')->__('Subtotal'), 535, $this->y, 'UTF-8');
+
+			$this->y -=15;
+
+			$page->setFillColor(new Zend_Pdf_Color_GrayScale(0));
+
+			/* Add body */
+			foreach ($invoice->getAllItems() as $item){
+				if ($item->getOrderItem()->getParentItem()) {
+					continue;
+				}
+
+				if ($this->y < 15) {
+					$page = $this->newPage(array('table_header' => true));
+				}
+
+				/* Draw item */
+				$page = $this->_drawItem($item, $page, $order);
+			}
+
+			/* Add totals */
+			$page = $this->insertTotals($page, $invoice);
+
+			if ($invoice->getStoreId()) {
+				Mage::app()->getLocale()->revert();
+			}
+		}
+
+		$this->_afterGetPdf();
+
+		return $pdf;
+	}
    }
 
    protected function insertSignature(&$page, $order) {
-    	$this->y = $this->y ? $this->y : 815;
-    	$answer =Mage::helper('opcsignature')->getSignature($order);
-    	if($answer)
-    	{	
-    		$fist_name = $order->getBillingAddress()->getData('firstname');
-    		$middle_name = $order->getBillingAddress()->getData('middlename');
+		$this->y = $this->y ? $this->y : 815;
+		$answer =Mage::helper('opcsignature')->getSignature($order);
+		if($answer)
+		{
+			$fist_name = $order->getBillingAddress()->getData('firstname');
+			$middle_name = $order->getBillingAddress()->getData('middlename');
 
-    		$last_name = $order->getBillingAddress()->getData('lastname');
+			$last_name = $order->getBillingAddress()->getData('lastname');
 
-    		
-    		$top = $this->y;
 
-    		$top += 8;
+			$top = $this->y;
 
-    		$page->setFillColor(new Zend_Pdf_Color_Rgb(0.93, 0.92, 0.92));
+			$top += 8;
 
-    		$page->setLineColor(new Zend_Pdf_Color_GrayScale(0.5));
+			$page->setFillColor(new Zend_Pdf_Color_Rgb(0.93, 0.92, 0.92));
 
-    		$page->setLineWidth(0.5);
+			$page->setLineColor(new Zend_Pdf_Color_GrayScale(0.5));
 
-    		$page->drawRectangle(25, $top, 570, ($top - 15));
+			$page->setLineWidth(0.5);
 
-    		
+			$page->drawRectangle(25, $top, 570, ($top - 15));
 
-    		$this->_setFontRegular($page, 10);
 
-    		$page->setFillColor(new Zend_Pdf_Color_GrayScale(0));
 
-    		$page->drawText(Mage::helper('sales')->__('Signature'), 35, $this->y-2, 'UTF-8');
+			$this->_setFontRegular($page, 10);
+
+			$page->setFillColor(new Zend_Pdf_Color_GrayScale(0));
+
+			$page->drawText(Mage::helper('sales')->__('Signature'), 35, $this->y-2, 'UTF-8');
 			
 
-    		
-    		$top-=20;	$page->setFillColor(new Zend_Pdf_Color_GrayScale(1));
 
-    		
-    	
+			$top-=20;	$page->setFillColor(new Zend_Pdf_Color_GrayScale(1));
 
-    		
-	    	if($answer['type'] == 'image')
-	    	{
-	    		$image = $answer['value'];
 
-	    	
-	    		if (is_file($image)) {
 
-	    			$image       = Zend_Pdf_Image::imageWithPath($image);
 
-	    			$top         = $this->y-20; //top border of the page
 
-	    			$widthLimit  = 90; //half of the page width
+			if($answer['type'] == 'image')
+			{
+				$image = $answer['value'];
 
-	    			$heightLimit = 90; //assuming the image is not a "skyscraper"
 
-	    			$width       = $image->getPixelWidth();
+				if (is_file($image)) {
 
-	    			$height      = $image->getPixelHeight();
+					$image       = Zend_Pdf_Image::imageWithPath($image);
 
-	    	
+					$top         = $this->y-20; //top border of the page
 
-	    			//preserving aspect ratio (proportions)
+					$widthLimit  = 90; //half of the page width
 
-	    			$ratio = $width / $height;
+					$heightLimit = 90; //assuming the image is not a "skyscraper"
 
-	    			if ($ratio > 1 && $width > $widthLimit) {
+					$width       = $image->getPixelWidth();
 
-	    				$width  = $widthLimit;
+					$height      = $image->getPixelHeight();
 
-	    				$height = $width / $ratio;
 
-	    			} elseif ($ratio < 1 && $height > $heightLimit) {
 
-	    				$height = $heightLimit;
+					//preserving aspect ratio (proportions)
 
-	    				$width  = $height * $ratio;
+					$ratio = $width / $height;
 
-	    			} elseif ($ratio == 1 && $height > $heightLimit) {
+					if ($ratio > 1 && $width > $widthLimit) {
 
-	    				$height = $heightLimit;
+						$width  = $widthLimit;
 
-	    				$width  = $widthLimit;
+						$height = $width / $ratio;
 
-	    			}
+					} elseif ($ratio < 1 && $height > $heightLimit) {
 
-	    	
+						$height = $heightLimit;
 
-	    			$y1 = $top - 10 -$height;
+						$width  = $height * $ratio;
 
-	    			$y2 = $top-10;
+					} elseif ($ratio == 1 && $height > $heightLimit) {
 
-	    			$x1 = 35;
+						$height = $heightLimit;
 
-	    			$x2 = $x1 + $width;
+						$width  = $widthLimit;
 
-	    	
+					}
 
-	    			//coordinates after transformation are rounded by Zend
 
-	    			$font = $this->_setFontRegular($page, 10);
 
-	    			$page->drawRectangle(25, ($top +15), 570, $top - 210);
+					$y1 = $top - 10 -$height;
 
-	    			$page->setFillColor(new Zend_Pdf_Color_GrayScale(0));
-	    			$page->drawText($fist_name, 35, $top, 'UTF-8');
-	    			$right_margin = $this->widthForStringUsingFontSize($fist_name, $font, 10)+40;
-	    			if($middle_name)
-	    			{
-	    				$page->drawText($middle_name, $right_margin, $top, 'UTF-8');
-	    				$right_margin += $this->widthForStringUsingFontSize($middle_name, $font, 10)+5;
-	    			}
-	     
-	    			$page->drawText($last_name, $right_margin, $top, 'UTF-8');
-	    			
-	    			$page->drawImage($image, $x1, $y1, $x2, $y2);
+					$y2 = $top-10;
+
+					$x1 = 35;
+
+					$x2 = $x1 + $width;
+
+
+
+					//coordinates after transformation are rounded by Zend
+
+					$font = $this->_setFontRegular($page, 10);
+
+					$page->drawRectangle(25, ($top +15), 570, $top - 210);
+
+					$page->setFillColor(new Zend_Pdf_Color_GrayScale(0));
+					$page->drawText($fist_name, 35, $top, 'UTF-8');
+					$right_margin = $this->widthForStringUsingFontSize($fist_name, $font, 10)+40;
+					if($middle_name)
+					{
+						$page->drawText($middle_name, $right_margin, $top, 'UTF-8');
+						$right_margin += $this->widthForStringUsingFontSize($middle_name, $font, 10)+5;
+					}
+
+					$page->drawText($last_name, $right_margin, $top, 'UTF-8');
+
+					$page->drawImage($image, $x1, $y1, $x2, $y2);
 					$this->y -=63;
-			    
-			    // $page->drawText(Mage::getModel('core/variable')->setStoreId(Mage::app()->getStore()->getId())->loadByCode('sinnature_text1')->getValue('html'), 35, $top, 'UTF-8');
-	    		 $dta=explode("/",Mage::getModel('core/variable')->setStoreId(Mage::app()->getStore()->getId())->loadByCode('sinnature_text1')->getValue('html'));
+
+				// $page->drawText(Mage::getModel('core/variable')->setStoreId(Mage::app()->getStore()->getId())->loadByCode('sinnature_text1')->getValue('html'), 35, $top, 'UTF-8');
+				 $dta=explode("/",Mage::getModel('core/variable')->setStoreId(Mage::app()->getStore()->getId())->loadByCode('sinnature_text1')->getValue('html'));
 				$signd=count($dta);
-				 	$this->y -=40;
+					$this->y -=40;
 				 $page->setFillColor(new Zend_Pdf_Color_GrayScale(0));
 				 foreach($dta as $iss)
 				  {
 					$page->drawText(strip_tags(ltrim($iss)),30, $this->y - 13, 'UTF-8');            
-                     $this->y -=15;
+					 $this->y -=15;
 
 				 }
 					
 					
 					$this->_setFontRegular($page, 10);
  
-	    			$this->y -=63;
+					$this->y -=63;
 					
-	    			@unlink($answer['value']);
+					@unlink($answer['value']);
 
-	    		}
+				}
 
-	    	}
-	    	elseif($answer['type'] == 'typed')
-	    	{
-	    		
-	    		$font = $this->_setFontRegular($page, 10);
-	    		
-	    		$page->setFillColor(new Zend_Pdf_Color_GrayScale(1));
+			}
+			elseif($answer['type'] == 'typed')
+			{
 
-	    		$page->drawRectangle(25, ($top+5 ), 570, $top - 210);
-	    		$page->setFillColor(new Zend_Pdf_Color_GrayScale(0));
-	    		$this->_setFontRegular($page, 10);
-	    		$top-=10;
-	    		$page->drawText($fist_name, 35, $top, 'UTF-8');
-	    		$right_margin = $this->widthForStringUsingFontSize($fist_name, $font, 10)+40;
+				$font = $this->_setFontRegular($page, 10);
 
-	    		if($middle_name)
-	    		{
-	    			$page->drawText($middle_name, $right_margin, $top, 'UTF-8');
-	    			$right_margin += $this->widthForStringUsingFontSize($middle_name, $font, 10)+5;
-	    		}
-	    		
-               
-	    		$page->drawText($last_name, $right_margin, $top, 'UTF-8');
-	    		$font = $this->_setFontItalic($page, 10);
-	    		$page->setFillColor(new Zend_Pdf_Color_GrayScale(0));
+				$page->setFillColor(new Zend_Pdf_Color_GrayScale(1));
+
+				$page->drawRectangle(25, ($top+5 ), 570, $top - 210);
+				$page->setFillColor(new Zend_Pdf_Color_GrayScale(0));
+				$this->_setFontRegular($page, 10);
+				$top-=10;
+				$page->drawText($fist_name, 35, $top, 'UTF-8');
+				$right_margin = $this->widthForStringUsingFontSize($fist_name, $font, 10)+40;
+
+				if($middle_name)
+				{
+					$page->drawText($middle_name, $right_margin, $top, 'UTF-8');
+					$right_margin += $this->widthForStringUsingFontSize($middle_name, $font, 10)+5;
+				}
+
+
+				$page->drawText($last_name, $right_margin, $top, 'UTF-8');
+				$font = $this->_setFontItalic($page, 10);
+				$page->setFillColor(new Zend_Pdf_Color_GrayScale(0));
 				$font = $this->_setFontHarlowItalic($page, 10);
 				$page->setFillColor(new Zend_Pdf_Color_Html('#1f497d'));
-	    		$page->drawText(Mage::helper('sales')->__($answer['value']), 35, ($top-15), 'UTF-8');
+				$page->drawText(Mage::helper('sales')->__($answer['value']), 35, ($top-15), 'UTF-8');
 				$font = $this->_setFontItalic($page, 10);
-	    		$page->setFillColor(new Zend_Pdf_Color_GrayScale(0));
+				$page->setFillColor(new Zend_Pdf_Color_GrayScale(0));
 				//$page->drawText(Mage::getModel('core/variable')->setStoreId(Mage::app()->getStore()->getId())->loadByCode('sinnature_text1')->getValue('html'), 35, $top, 'UTF-8');
-		        $dta=explode("/",Mage::getModel('core/variable')->setStoreId(Mage::app()->getStore()->getId())->loadByCode('sinnature_text1')->getValue('html'));
-			    $signd=count($dta);
+				$dta=explode("/",Mage::getModel('core/variable')->setStoreId(Mage::app()->getStore()->getId())->loadByCode('sinnature_text1')->getValue('html'));
+				$signd=count($dta);
 				$this->y -=40;
 				 foreach($dta as $iss)
 				  {
 			 
-					    $page->drawText(strip_tags(ltrim($iss)), 30, $this->y - 13, 'UTF-8');            
-                         $this->y -=15;
+						$page->drawText(strip_tags(ltrim($iss)), 30, $this->y - 13, 'UTF-8');
+						 $this->y -=15;
 				 }
 				 $this->_setFontRegular($page, 10);
-	    		$this->y -=55;
-	    	}
-	    	
-    	}
-    }
-    
-    
-    /**
+				$this->y -=55;
+			}
 
-     * After getPdf processing
+		}
+	}
 
-     */
 
-    protected function _afterGetPdf() {
+	/**
 
-    	$translate = Mage::getSingleton('core/translate');
+	 * After getPdf processing
 
-    	/* @var $translate Mage_Core_Model_Translate */
+	 */
 
-    	$translate->setTranslateInline(true);
-    }
+	protected function _afterGetPdf() {
+
+		$translate = Mage::getSingleton('core/translate');
+
+		/* @var $translate Mage_Core_Model_Translate */
+
+		$translate->setTranslateInline(true);
+	}
    }
