2149cbf191169de168c5f3c4b50e08b61df1ed1a
Dmitrii Fediuk <dfediuk@gmail.com>
2024-01-04 09:27:07 +0000

    https://github.com/thehcginstitute-com/m1/issues/89

--- app/code/community/Mage/Payment/Model/Method/Cc.php
+++ app/code/community/Mage/Payment/Model/Method/Cc.php
@@ -1,48 +1,21 @@
 <?php
-/**
- * Magento
- *
- * NOTICE OF LICENSE
- *
- * This source file is subject to the Open Software License (OSL 3.0)
- * that is bundled with this package in the file LICENSE.txt.
- * It is also available through the world-wide-web at this URL:
- * http://opensource.org/licenses/osl-3.0.php
- * If you did not receive a copy of the license and are unable to
- * obtain it through the world-wide-web, please send an email
- * to license@magentocommerce.com so we can send you a copy immediately.
- *
- * DISCLAIMER
- *
- * Do not edit or add to this file if you wish to upgrade Magento to newer
- * versions in the future. If you wish to customize Magento for your
- * needs please refer to http://www.magentocommerce.com for more information.
- *
- * @category    Mage
- * @package     Mage_Payment
- * @copyright   Copyright (c) 2013 Magento Inc. (http://www.magentocommerce.com)
- * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
- */
-
-
-class Mage_Payment_Model_Method_Cc extends Mage_Payment_Model_Method_Abstract
-{
-	public function prepareSave()
-	{
-
+# 2024-01-04 Dmitrii Fediuk https://upwork.com/fl/mage2pro
+# "Refactor `app/code/community/Mage/Payment/Model/Method/Cc.php` modifications":
+# https://github.com/thehcginstitute-com/m1/issues/89
+class Mage_Payment_Model_Method_Cc extends Mage_Payment_Model_Method_Abstract {
+	function prepareSave() {
 		$is_enable = Mage::getStoreConfig('cvv/group_displaycvv/displaycvv_select');
 		$info = $this->getInfoInstance();
-		if ($this->_canSaveCc)
-		{
-			if($is_enable == 1){
+		if ($this->_canSaveCc)  {
+			if ($is_enable == 1) {
 				$info->setCcNumberEnc($info->encrypt($info->getCcNumber()));
 				$info->setCcCid_enc($info->getCcCid());
-			}else{
+			}
+			else {
 				$info->setCcNumberEnc($info->encrypt($info->getCcNumber()));
 			}
 		}
 		$info->setCcNumber(null)->setCcCid(null);
 		return $this;
-
 	}
 }
