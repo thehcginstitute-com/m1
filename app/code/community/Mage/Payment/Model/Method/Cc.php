bbb98d352e865dccd4ad8a0bdc18ff05e8b711d0
Dmitrii Fediuk <dfediuk@gmail.com>
2024-01-04 09:32:51 +0000

    https://github.com/thehcginstitute-com/m1/issues/89

--- app/code/community/Mage/Payment/Model/Method/Cc.php
+++ app/code/community/Mage/Payment/Model/Method/Cc.php
@@ -1,7 +1,14 @@
 <?php
-# 2024-01-04 Dmitrii Fediuk https://upwork.com/fl/mage2pro
-# "Refactor `app/code/community/Mage/Payment/Model/Method/Cc.php` modifications":
-# https://github.com/thehcginstitute-com/m1/issues/89
+/**
+ * 2024-01-04 Dmitrii Fediuk https://upwork.com/fl/mage2pro
+ * "Refactor `app/code/community/Mage/Payment/Model/Method/Cc.php` modifications":
+ * https://github.com/thehcginstitute-com/m1/issues/89
+ * @see Mage_Payment_Model_Method_Cc (from `app/code/core`)
+ * @see Mage_Payment_Model_Method_Ccsave (used: https://github.com/thehcginstitute-com/m1/issues/89#issuecomment-1876738314)
+ * @see OX_AmexSavedCC_Model_Method_Ccsave (used: https://github.com/thehcginstitute-com/m1/issues/89#issuecomment-1876738314)
+ * @see Mage_Paygate_Model_Authorizenet (unused)
+ * @see Mage_Paypal_Model_Direct (unused)
+ */
 class Mage_Payment_Model_Method_Cc extends Mage_Payment_Model_Method_Abstract {
 	function prepareSave() {
 		$is_enable = Mage::getStoreConfig('cvv/group_displaycvv/displaycvv_select');
