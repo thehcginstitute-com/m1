9c43431e10841c2526dae00289f3692ae94cd1c7
Dmitrii Fediuk <dfediuk@gmail.com>
2024-01-11 00:44:49 +0300

    https://github.com/thehcginstitute-com/m1/issues/190

---
+++ app/code/local/Df/Core/lib/lang/prop.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * 2019-09-08
+ * 2024-01-11 "Port `DF_N` from `mage2pro/core`": https://github.com/thehcginstitute-com/m1/issues/190
+ * @used-by df_n_get()
+ * @used-by df_n_set()
+ * @used-by df_prop()
+ * @used-by df_prop_k()
+ * @used-by \Df\Core\Json::bSort()
+ */
+const DF_N = 'df-null';
