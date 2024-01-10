1f6676ffc52395795035e5a03004a5e63d50c2d7
Dmitrii Fediuk <dfediuk@gmail.com>
2024-01-10 20:53:05 +0000

    refactoring

--- app/code/local/Df/Core/lib/caller/entry.php
+++ app/code/local/Df/Core/lib/caller/entry.php
@@ -36,18 +36,3 @@ function df_caller_entry($p = 0, F $predicate = null) {
 	}
 	return df_eta($r); /** 2021-10-05 @uses array_shift() returns `null` for an empty array */
 }
-
-/**
- * 2016-08-10
- * The original (not used now) implementation: https://github.com/mage2pro/core/blob/6.7.3/Core/lib/caller.php#L109-L111
- * 2017-01-12
- * The df_caller_ff() implementation: https://github.com/mage2pro/core/blob/6.7.3/Core/lib/caller.php#L113-L123
- * 2020-07-08 The function's new implementation is from the previous df_caller_ff() function.
- * @used-by df_log_e()
- * @used-by df_log_l()
- * @used-by df_oqi_amount()
- * @used-by df_prop()
- * @param int $o [optional]
- * @return string
- */
-function df_caller_f($o = 0) {return df_caller_entry(++$o)['function'];}
