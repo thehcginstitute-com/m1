3a7bf2ae1544f144e8fa8e0abd6040b7d9a5814d
Dmitrii Fediuk <dfediuk@gmail.com>
2024-01-10 21:01:02 +0000

    refactoring

--- app/code/local/Df/Qa/lib/bt/main.php
+++ app/code/local/Df/Qa/lib/bt/main.php
@@ -1,26 +1,46 @@
 <?php
-use Exception as E;
+use Throwable as Th; # 2023-08-03 "Treat `\Throwable` similar to `\Exception`": https://github.com/mage2pro/core/issues/311
+
 /**
  * 2021-10-04
+ * 2023-07-27  The first entry in the result is the caller of df_bt().
  * @used-by df_bt_has()
  * @used-by df_bt_s()
  * @used-by df_caller_entry()
  * @used-by dfs_con()
- * @param int $limit
- * @param E|int|null|array(array(string => string|int)) $p [optional]
+ * @used-by \Df\Qa\Method::caller()
+ * @param Th|int|null|array(array(string => string|int)) $p [optional]
  * @return array(array(string => mixed))
  */
-function df_bt($p = 0, $limit = 0) {return is_array($p) ? $p : ($p instanceof E ? $p->getTrace() : df_slice(
-	debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS, !$limit ? 0 : 1 + $p + $limit), 1 + $p, $limit
-));}
+function df_bt($p = 0, int $limit = 0):array {
+	$r = is_array($p) ? $p : (df_is_th($p) ? df_bt_th($p) :
+		debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS, !$limit ? 0 : 1 + $p + $limit)
+	);
+	# 2023-07-27 "Shift the `file` and `line` keys to an entry back in `df_bt()`": https://github.com/mage2pro/core/issues/283
+	list($f, $l) = ['', 0]; /** @var string $f */ /** @var int $l */
+	foreach ($r as &$e) {/** @var array(string => int|string) $e */
+		list($f2, $l2) = [df_bt_entry_file($e), df_bt_entry_line($e)]; /** @var string $f2 */ /** @var int $l2 */
+		$e = ['file' => $f, 'line' => $l] + $e;
+		list($f, $l) = [$f2, $l2];
+	}
+	/**
+	 * 2023-07-28 We skip the first entry: `df_bt`.
+	 * 2023-08-25
+	 * For the @see \Throwable case we do not need to skip df_bt() (because it is absent in the trace),
+	 * but we still need to skip the first frame because it is empty after we shifted the `file` and `line` keys (see above).
+	 * The first frame was artificial: @see df_bt_th()
+	 */
+	$r = df_slice($r, 1);
+	return is_array($p) || df_is_th($p) ? $r : df_slice($r, $p, $limit);
+}
 
 /**
  * 2021-10-04
  * @used-by df_bt_log()
  * @used-by df_bt_s()
  * @used-by df_caller_entry()
- * @param E|int|null|array(array(string => string|int)) $p
- * @param int $o
- * @return E|int
+ * @used-by df_caller_module()
+ * @param Th|int|null|array(array(string => string|int)) $p
+ * @return Th|int
  */
-function df_bt_inc($p, $o = 1) {return is_array($p) || $p instanceof E ? $p : $o + $p;}
+function df_bt_inc($p, int $o = 1) {return is_array($p) || df_is_th($p) ? $p : $o + $p;}
