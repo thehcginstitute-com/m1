3dfffd615a20970e829629b1c0f3ea5ab5db6e72
Dmitrii Fediuk <dfediuk@gmail.com>
2024-01-11 00:55:48 +0300

    https://github.com/thehcginstitute-com/m1/issues/177

--- app/code/local/Df/Qa/lib/log.php
+++ app/code/local/Df/Qa/lib/log.php
@@ -78,3 +78,16 @@ function df_report($f, $m, $append = false) {
 		df_file_write($append ? "$p/$f" : df_file_name($p, $f), $m, $append);
 	}
 }
+
+/**
+ * 2020-01-31
+ * 2023-07-19
+ * «mb_strtolower(): Passing null to parameter #1 ($string) of type string is deprecated
+ * in vendor/mage2pro/core/Qa/lib/log.php on line 122»: https://github.com/mage2pro/core/issues/233
+ * 2024-01-11 "Port `df_report_prefix` from `mage2pro/core`": https://github.com/thehcginstitute-com/m1/issues/177
+ * @used-by df_log_l()
+ * @param string|object|null $m [optional]
+ */
+function df_report_prefix($m = null, string $pref = ''):string {return df_ccc('--',
+	mb_strtolower($pref), !$m ? null : df_module_name_lc($m, '-')
+);}
