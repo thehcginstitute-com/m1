3bdab2b13d1cde9d480015b36fb1a25fd6669110
Dmitrii Fediuk <dfediuk@gmail.com>
2024-01-11 01:46:28 +0300

    https://github.com/thehcginstitute-com/m1/issues/167

--- app/code/local/Df/Framework/lib/fs/main.php
+++ app/code/local/Df/Framework/lib/fs/main.php
@@ -3,21 +3,12 @@ use Df\Framework\Fs;
 
 /**
  * 2015-11-29
- * 2015-11-30
- * @see \Magento\Framework\Filesystem\Directory\Write::openFile() creates the parent directories automatically:
- * https://github.com/magento/magento2/blob/2.0.0/lib/internal/Magento/Framework/Filesystem/Directory/Write.php#L247
- * 2017-04-03 The possible directory types for filesystem operations: https://mage2.pro/t/3591
- * 2017-04-22
- * С не-строковым значением $contents @uses \Magento\Framework\Filesystem\Driver\File::fileWrite() упадёт,
- * потому что там стоит код: $lenData = strlen($data);
  * 2018-07-06 The `$append` parameter has been added.
  * 2020-02-14 If $append is `true`, then $contents will be written on a new line.
  * 2024-01-11 "Port `df_file_write` from `rm3`": https://github.com/thehcginstitute-com/m1/issues/167
  * @used-by df_report()
- * @used-by df_sync()
- * @param string $p
  */
-function df_file_write($p, string $contents, bool $append = false):void {
+function df_file_write(string $p, string $contents, bool $append = false):void {
 	Fs::createAndMakeWritable($p);
 	$abs = BP . "/$p"; /** #var string $abs */
 	$append = $append && file_exists($abs) && 0 !== filesize($abs);
