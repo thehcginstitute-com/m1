ccbe1b72797ce317ab432ab8e9fa70517d6ed294
Dmitrii Fediuk <dfediuk@gmail.com>
2024-01-11 00:24:53 +0300

    https://github.com/thehcginstitute-com/m1/issues/186

--- app/code/local/Df/Qa/lib/validation/assert/eq.php
+++ app/code/local/Df/Qa/lib/validation/assert/eq.php
@@ -1,6 +1,20 @@
 <?php
 use Exception as E;
 use Throwable as Th; # 2023-08-03 "Treat `\Throwable` similar to `\Exception`": https://github.com/mage2pro/core/issues/311
+/**
+ * 2024-01-11 "Port `df_assert_eq` from `mage2pro/core`": https://github.com/thehcginstitute-com/m1/issues/186
+ * @used-by df_module_name_by_path()
+ * @used-by df_vector_sum()
+ * @used-by \Df\Qa\Trace\Frame::url()
+ * @param string|int|float|bool $expected
+ * @param string|int|float|bool $v
+ * @param string|Th|null $m [optional]
+ * @return string|int|float|bool
+ * @throws E
+ */
+function df_assert_eq($expected, $v, $m = null) {return $expected === $v ? $v : df_error($m ?: sprintf(
+	"Expected «%s», got «%s».", df_dump($expected), df_dump($v)
+));}
 
 /**
  * 2024-01-10 "Port `df_assert_lt` from `mage2pro/core`": https://github.com/thehcginstitute-com/m1/issues/164
