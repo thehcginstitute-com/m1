d74617101599f2c22358f774d216406d3c7ce349
Dmitrii Fediuk <dfediuk@gmail.com>
2024-01-11 01:02:56 +0300

    https://github.com/thehcginstitute-com/m1/issues/195

--- app/code/local/Df/Core/lib/reflection/text/explode.php
+++ app/code/local/Df/Core/lib/reflection/text/explode.php
@@ -8,3 +8,15 @@
  * https://3v4l.org/k6Hd5
  */
 function df_explode_class($c) {return df_explode_multiple(['\\', '_'], df_cts($c));}
+
+/**
+ * 2016-04-11
+ * 2016-10-20
+ * 1) Making $c optional leads to the error «get_class() called without object from outside a class»: https://3v4l.org/k6Hd5
+ * 2) Dfe_CheckoutCom => [dfe, checkout, com]
+ * 2024-01-11 "Port `df_explode_class_lc_camel` from `mage2pro/core`": https://github.com/thehcginstitute-com/m1/issues/195
+ * @used-by df_module_name_lc()
+ * @param string|object $c
+ * @return string[]
+ */
+function df_explode_class_lc_camel($c):array {return df_lcfirst(df_explode_class_camel($c));}
