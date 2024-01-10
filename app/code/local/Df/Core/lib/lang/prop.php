76f41acdc7f65ff550b8ed579f10a227358f099b
Dmitrii Fediuk <dfediuk@gmail.com>
2024-01-11 00:48:01 +0300

    https://github.com/thehcginstitute-com/m1/issues/191

--- app/code/local/Df/Core/lib/lang/prop.php
+++ app/code/local/Df/Core/lib/lang/prop.php
@@ -57,3 +57,92 @@ function df_prop($o, $v = DF_N, $d = null, string $type = '') {/** @var object|m
 	}
 	return $isGet && 'int' === $type ? intval($r) : $r;
 }
+
+/**
+ * 2022-10-28
+ * 2023-07-29
+ * 1) @noinspection PhpVariableVariableInspection
+ * 2) "Suppress the «Variable variable used» inspection for the code intended for PHP < 8.2":
+ * https://github.com/mage2pro/core/issues/294
+ * 2024-01-11 "Port `df_prop_k` from `mage2pro/core`": https://github.com/thehcginstitute-com/m1/issues/191
+ * @used-by df_prop()
+ * @param object|ArrayAccess $o
+ * @param mixed|string $v [optional]
+ * @param string|mixed|null $d [optional]
+ * @return mixed|object|ArrayAccess|null
+ */
+function df_prop_k($o, string $k, $v = DF_N, $d = null) {/** @var object|mixed|null $r */
+	/**
+	 * 2019-09-08
+	 * 1) My 1st solution was comparing $v with `null`,
+	 * but it is wrong because it fails for a code like `$object->property(null)`.
+	 * 2) My 2nd solution was using @see func_num_args():
+	 * «How to tell if optional parameter in PHP method/function was set or not?»
+	 * https://stackoverflow.com/a/3471863
+	 * It is wrong because the $v argument is alwaus passed to df_prop()
+	 */
+	$isGet = DF_N === $v; /** @vae bool $isGet */
+	if ($o instanceof ArrayAccess) {
+		if ($isGet) {
+			$r = !$o->offsetExists($k) ? $d : $o->offsetGet($k);
+		}
+		else {
+			$o->offsetSet($k, $v);
+			$r = $o;
+		}
+	}
+	else {
+		$a = '_' . __FUNCTION__; /** @var string $a */
+		/**
+		 * 2022-10-18
+		 * 1) Dynamic properties are deprecated since PHP 8.2:
+		 * https://php.net/manual/migration82.deprecated.php#migration82.deprecated.core.dynamic-properties
+		 * https://wiki.php.net/rfc/deprecate_dynamic_properties
+		 * 2) @see dfc()
+		 */
+		static $hasWeakMap; /** @var bool $hasWeakMap */
+		# 2024-01-10
+		# 1) The previous code: `@class_exists('WeakMap')`.
+		# 2) I changed the code by analogy with
+		# https://github.com/thehcginstitute-com/m1/blob/2024-01-10-2/app/code/local/Df/Core/lib/cache/dfc.php#L58-L62
+		if (!($hasWeakMap = !is_null($hasWeakMap) ? $hasWeakMap : class_exists('WeakMap', false))) {
+			if (!isset($o->$a)) {
+				$o->$a = [];
+			}
+			if ($isGet) {
+				$r = dfa($o->$a, $k, $d);
+			}
+			else {
+				# 2022-10-18
+				# The previous code was:
+				# 		$prop =& $o->$a;
+				#		$prop[$k] = $v;
+				# The new code works correctly in PHP ≤ 8.2: https://3v4l.org/8agSI1
+				$o->{$a}[$k] = $v;
+				$r = $o;
+			}
+		}
+		else {
+			static $map; /** @var WeakMap $map */
+			$map = $map ?: new WeakMap;
+			if (!$map->offsetExists($o)) {
+				$map[$o] = [];
+			}
+			# 2022-10-17 https://3v4l.org/6cVAu
+			$map2 =& $map[$o]; /** @var array(string => mixed) $map2 */
+			if (!isset($map2[$a])) {
+				$map2[$a] = [];
+			}
+			# 2022-10-18 https://3v4l.org/1tS4v
+			$prop =& $map2[$a]; /** array(string => mixed) $prop */
+			if ($isGet) {
+				$r = dfa($prop, $k, $d);
+			}
+			else {
+				$prop[$k] = $v;
+				$r = $o;
+			}
+		}
+	}
+	return $r;
+}
