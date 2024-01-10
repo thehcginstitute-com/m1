c78a6343c33bcf204267df9c35c3dec7f38208cd
Dmitrii Fediuk <dfediuk@gmail.com>
2024-01-11 00:10:54 +0300

    https://github.com/thehcginstitute-com/m1/issues/174

--- app/code/local/Df/Qa/lib/bt/entry.php
+++ app/code/local/Df/Qa/lib/bt/entry.php
@@ -56,6 +56,15 @@ function df_bt_entry_func(array $e):string {return dfa($e, 'function', '');}
 function df_bt_entry_is_method(array $e):bool {return dfa_has_keys($e, ['class', 'function']);}
 
 /**
+ * 2023-07-26
+ * 2024-01-11 "Port `df_bt_entry_is_phtml` from `mage2pro/core`": https://github.com/thehcginstitute-com/m1/issues/174
+ * @see \Df\Qa\Trace\Frame::isPHTML()
+ * @used-by df_log_l()
+ * @used-by df_caller_entry_m()
+ */
+function df_bt_entry_is_phtml(array $e):bool {return df_is_phtml(df_bt_entry_file($e));}
+
+/**
  * 2023-07-27 `line` is absent in @see call_user_func() calls.
  * @see \Df\Qa\Trace\Frame::line()
  * @used-by df_bt()
