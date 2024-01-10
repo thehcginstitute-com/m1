d297594bb3dbe43f9e558efc15033ef34a02330c
Dmitrii Fediuk <dfediuk@gmail.com>
2024-01-11 00:39:19 +0300

    https://github.com/thehcginstitute-com/m1/issues/187

--- app/code/local/Df/Core/lib/json.php
+++ app/code/local/Df/Core/lib/json.php
@@ -1,4 +1,21 @@
 <?php
+use Df\Core\Json as J;
+
+/**
+ * 2023-08-04 "Implement `df_json_dont_sort()`": https://github.com/mage2pro/core/issues/313
+ * 2024-01-11 "Port `df_json_dont_sort` from `mage2pro/core`": https://github.com/thehcginstitute-com/m1/issues/187
+ * @see df_json_sort()
+ * @used-by df_dump_ds()
+ * @return mixed
+ */
+function df_json_dont_sort(F $f) {/** @var mixed $r */
+	$prev = J::bSort(); /** @var bool $prev */
+	J::bSort(false);
+	try {$r = $f();}
+	finally {J::bSort($prev);}
+	return $r;
+}
+
 /**
  * 2015-12-06
  * @used-by df_json_encode_partial()
