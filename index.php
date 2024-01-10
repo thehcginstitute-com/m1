5e118bd4ac227a12cb4bde0ab8422fd275ab5c8e
Dmitrii Fediuk <dfediuk@gmail.com>
2024-01-11 01:57:09 +0300

    https://github.com/thehcginstitute-com/m1/issues/117

--- index.php
+++ index.php
@@ -34,6 +34,13 @@ Whoops, it looks like you have an invalid PHP version.</h3></div><p>Magento supp
     exit;
 }
 
+# 2023-07-08 Dmitrii Fediuk https://upwork.com/fl/mage2pro
+# 2023-07-10 # `^` instead of `&~` is incorrect:
+# https://github.com/magento-russia/3/blob/2023-07-10/app/code/local/Df/Core/Boot.php#L146-L154
+# 2024-01-11 "Port the modifications of `index.php` to Magento 1.9.4.5": https://github.com/thehcginstitute-com/m1/issues/117
+error_reporting((E_ALL | E_STRICT) &~ (E_DEPRECATED));
+ini_set('display_errors', 1);
+
 /**
  * Compilation includes configuration file
  */
