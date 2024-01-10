c962e0bb0119876d41e61fdf7ce58d1e68053463
Dmitrii Fediuk <dfediuk@gmail.com>
2024-01-11 01:04:48 +0300

    https://github.com/thehcginstitute-com/m1/issues/197

--- app/code/local/Df/Core/lib/reflection/text/explode.php
+++ app/code/local/Df/Core/lib/reflection/text/explode.php
@@ -10,6 +10,17 @@
 function df_explode_class($c) {return df_explode_multiple(['\\', '_'], df_cts($c));}
 
 /**
+ * 2016-04-11 Dfe_CheckoutCom => [Dfe, Checkout, Com]
+ * 2016-10-20
+ * Making $c optional leads to the error «get_class() called without object from outside a class»: https://3v4l.org/k6Hd5
+ * 2024-01-11 "Port `df_explode_class_camel` from `mage2pro/core`": https://github.com/thehcginstitute-com/m1/issues/197
+ * @used-by df_explode_class_lc_camel()
+ * @param string|object $c
+ * @return string[]
+ */
+function df_explode_class_camel($c):array {return dfa_flatten(df_explode_camel(explode('\\', df_cts($c))));}
+
+/**
  * 2016-04-11
  * 2016-10-20
  * 1) Making $c optional leads to the error «get_class() called without object from outside a class»: https://3v4l.org/k6Hd5
