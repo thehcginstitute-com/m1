287b75cb81fa3dad18c3bd807603f8883e9d9406
Dmitrii Fediuk <dfediuk@gmail.com>
2024-02-03 18:47:18 +0300

    https://github.com/thehcginstitute-com/m1/issues/351

--- app/code/local/Df/Core/lib/array/part.php
+++ app/code/local/Df/Core/lib/array/part.php
@@ -10,3 +10,12 @@
  * @return array
  */
 function df_slice(array $a, $offset, $length = 0) {return array_slice($a, $offset, df_etn($length));}
+
+/**
+ * Отсекает первый элемент массива и возвращает хвост (аналог CDR в Lisp).
+ * Обратите внимание, что если исходный массив содержит меньше 2 элементов, то функция вернёт пустой массив.
+ * 2024-02-03 "Port `df_tail()` from `mage2pro/core`": https://github.com/thehcginstitute-com/m1/issues/351
+ * @see df_first()
+ * @see df_last()
+ */
+function df_tail(array $a):array {return array_slice($a, 1);}
