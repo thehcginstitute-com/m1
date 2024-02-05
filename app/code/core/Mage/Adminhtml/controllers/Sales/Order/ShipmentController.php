54e070a611a40d6c9d12dc69f96d1ad99df34e29
Dmitrii Fediuk <dfediuk@gmail.com>
2024-02-05 14:19:08 +0000

    https://github.com/thehcginstitute-com/m1/issues/375

--- app/code/core/Mage/Adminhtml/controllers/Sales/Order/ShipmentController.php
+++ app/code/core/Mage/Adminhtml/controllers/Sales/Order/ShipmentController.php
@@ -185,7 +185,8 @@ class Mage_Adminhtml_Sales_Order_ShipmentController extends Mage_Adminhtml_Contr
         }
 
         $responseAjax = new Varien_Object();
-        $isNeedCreateLabel = isset($data['create_shipping_label']) && $data['create_shipping_label'];
+		# 2024-02-05 Dmitrii Fediuk https://upwork.com/fl/mage2pro
+		# "Delete the shipping labels feature because it is unused": https://github.com/thehcginstitute-com/m1/issues/375
 
         try {
             $shipment->register();
