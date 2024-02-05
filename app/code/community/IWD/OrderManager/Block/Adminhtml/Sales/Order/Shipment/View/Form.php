371d48c43be9b424dc5fadc86a26c95b60b0e6fb
Dmitrii Fediuk <dfediuk@gmail.com>
2024-02-05 14:16:30 +0000

    https://github.com/thehcginstitute-com/m1/issues/375

--- app/code/community/IWD/OrderManager/Block/Adminhtml/Sales/Order/Shipment/View/Form.php
+++ app/code/community/IWD/OrderManager/Block/Adminhtml/Sales/Order/Shipment/View/Form.php
@@ -2,13 +2,6 @@
 
 class IWD_OrderManager_Block_Adminhtml_Sales_Order_Shipment_View_Form extends Mage_Adminhtml_Block_Sales_Order_Shipment_View_Form
 {
-    public function canCreateShippingLabel()
-    {
-        $carrier = $this->getOrder()->getShippingCarrier();
-        if (method_exists($carrier, 'isShippingLabelsAvailable')) {
-            return $carrier->isShippingLabelsAvailable();
-        } else {
-            return false;
-        }
-    }
+	# 2024-02-05 Dmitrii Fediuk https://upwork.com/fl/mage2pro
+	# "Delete the shipping labels feature because it is unused": https://github.com/thehcginstitute-com/m1/issues/375
 }
