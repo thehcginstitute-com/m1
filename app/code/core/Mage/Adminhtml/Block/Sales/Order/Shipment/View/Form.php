44a454492a2f6b56405cc9e6a89a6e0dbe1bcd3f
Dmitrii Fediuk <dfediuk@gmail.com>
2024-02-05 14:09:45 +0000

    https://github.com/thehcginstitute-com/m1/issues/375

--- app/code/core/Mage/Adminhtml/Block/Sales/Order/Shipment/View/Form.php
+++ app/code/core/Mage/Adminhtml/Block/Sales/Order/Shipment/View/Form.php
@@ -53,24 +53,6 @@ class Mage_Adminhtml_Block_Sales_Order_Shipment_View_Form extends Mage_Adminhtml
         return $this->getShipment();
     }
 
-    /**
-     * Get create label button html
-     *
-     * @return string
-     */
-    public function getCreateLabelButton()
-    {
-        $data['shipment_id'] = $this->getShipment()->getId();
-        $url = $this->getUrl('*/sales_order_shipment/createLabel', $data);
-        return $this->getLayout()
-            ->createBlock('adminhtml/widget_button')
-            ->setData([
-                'label'   => Mage::helper('sales')->__('Create Shipping Label...'),
-                'onclick' => 'packaging.showWindow();',
-            ])
-            ->toHtml();
-    }
-
 	# 2024-02-05 Dmitrii Fediuk https://upwork.com/fl/mage2pro
 	# "Delete the shipping labels feature because it is unused": https://github.com/thehcginstitute-com/m1/issues/375
 
