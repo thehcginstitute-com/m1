b70a3d52dddda1b79c1bdc9302c8dcae72d81db4
Dmitrii Fediuk <dfediuk@gmail.com>
2024-02-16 06:38:37 +0000

    https://github.com/thehcginstitute-com/m1/issues/34

--- js/varien/js.js
+++ js/varien/js.js
@@ -1,26 +1,15 @@
 /**
- * Magento
- *
- * NOTICE OF LICENSE
+ * OpenMage
  *
  * This source file is subject to the Academic Free License (AFL 3.0)
  * that is bundled with this package in the file LICENSE_AFL.txt.
- * It is also available through the world-wide-web at this URL:
- * http://opensource.org/licenses/afl-3.0.php
- * If you did not receive a copy of the license and are unable to
- * obtain it through the world-wide-web, please send an email
- * to license@magento.com so we can send you a copy immediately.
- *
- * DISCLAIMER
- *
- * Do not edit or add to this file if you wish to upgrade Magento to newer
- * versions in the future. If you wish to customize Magento for your
- * needs please refer to http://www.magento.com for more information.
+ * It is also available at https://opensource.org/license/afl-3-0-php
  *
  * @category    Varien
  * @package     js
- * @copyright   Copyright (c) 2006-2020 Magento, Inc. (http://www.magento.com)
- * @license     http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
+ * @copyright   Copyright (c) 2006-2020 Magento, Inc. (https://www.magento.com)
+ * @copyright   Copyright (c) 2020-2023 The OpenMage Contributors (https://www.openmage.org)
+ * @license     https://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
  */
 function popWin(url,win,para) {
     var win = window.open(url,win,para);
@@ -36,41 +25,6 @@ function setPLocation(url, setFocus){
         window.opener.focus();
     }
     window.opener.location.href = encodeURI(url);
-}
-
-/**
- * @deprecated
- */
-function setLanguageCode(code, fromCode){
-    //TODO: javascript cookies have different domain and path than php cookies
-    var href = window.location.href;
-    var after = '', dash;
-    if (dash = href.match(/\#(.*)$/)) {
-        href = href.replace(/\#(.*)$/, '');
-        after = dash[0];
-    }
-
-    if (href.match(/[?]/)) {
-        var re = /([?&]store=)[a-z0-9_]*/;
-        if (href.match(re)) {
-            href = href.replace(re, '$1'+code);
-        } else {
-            href += '&store='+code;
-        }
-
-        var re = /([?&]from_store=)[a-z0-9_]*/;
-        if (href.match(re)) {
-            href = href.replace(re, '');
-        }
-    } else {
-        href += '?store='+code;
-    }
-    if (typeof(fromCode) != 'undefined') {
-        href += '&from_store='+fromCode;
-    }
-    href += after;
-
-    setLocation(href);
 }
 
 /**
@@ -613,11 +567,13 @@ Varien.FileElement.prototype = {
     }
 };
 
-Validation.addAllThese([
-    ['validate-custom', ' ', function(v,elm) {
-        return elm.validate();
-    }]
-]);
+if (typeof Validation !== 'undefined') {
+    Validation.addAllThese([
+        ['validate-custom', ' ', function(v,elm) {
+            return elm.validate();
+        }]
+    ]);
+}
 
 function truncateOptions() {
     $$('.truncated').each(function(element){
@@ -648,18 +604,6 @@ Element.addMethods({
         return element.innerHTML.stripScripts().unescapeHTML().replace(/[\n\r\s]+/g, ' ').strip();
     }
 });
-
-/*
-if (!("console" in window) || !("firebug" in console))
-{
-    var names = ["log", "debug", "info", "warn", "error", "assert", "dir", "dirxml",
-    "group", "groupEnd", "time", "timeEnd", "count", "trace", "profile", "profileEnd"];
-
-    window.console = {};
-    for (var i = 0; i < names.length; ++i)
-        window.console[names[i]] = function() {}
-}
-*/
 
 /**
  * Executes event handler on the element. Works with event handlers attached by Prototype,
@@ -772,7 +716,8 @@ function customFormSubmitToParent(url, parametersArray, method) {
 }
 
 function buttonDisabler() {
-    const buttons = document.querySelectorAll('button.save');
-    buttons.forEach(button => button.disabled = true);
+    var buttons = document.querySelectorAll('button.save');
+    buttons.forEach(function(button) {
+        button.disabled = true;
+    });
 }
-
