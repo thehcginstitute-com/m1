fca72f60117763a39f55b4b923629707b9fc6adb
Dmitrii Fediuk <dfediuk@gmail.com>
2024-02-18 18:28:40 +0000

    https://github.com/thehcginstitute-com/m1/issues/380

--- app/code/local/Amasty/Base/sql/ambase_setup/mysql4-upgrade-1.0.0-1.0.1.php
+++ app/code/local/Amasty/Base/sql/ambase_setup/mysql4-upgrade-1.0.0-1.0.1.php
@@ -6,19 +6,6 @@
  */
 
 $this->startSetup();
-
-Amasty_Base_Helper_Module::baseModuleInstalled();
-
-$feedData = array();
-$feedData[] = array(
-    'severity'      => 4,
-    'date_added'    => gmdate('Y-m-d H:i:s', time()),
-    'title'         => 'Amasty`s extension has been installed. Remember to flush all cache, recompile, log-out and log back in.',
-    'description'   => 'You can see versions of the installed extensions right in the admin, as well as configure notifications about major updates.',
-    'url'           => 'http://amasty.com/news/updates-and-notifications-configuration-9.html'
-//    'url'           => Mage::getModel('adminhtml/url')->getUrl('adminhtml/system_config/edit', array('section'=>'ambase')),
-);
-
-Mage::getModel('adminnotification/inbox')->parse($feedData);
-
+# 2024-02-18 Dmitrii Fediuk https://upwork.com/fl/mage2pro
+# "Prevent Amasty from spamming the Magento's backend": https://github.com/thehcginstitute-com/m1/issues/380
 $this->endSetup();
