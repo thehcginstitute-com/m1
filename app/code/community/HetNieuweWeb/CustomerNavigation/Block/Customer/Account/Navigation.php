f4bc584f65e1cc8000e910d5fa757c7c6a727f42
Dmitrii Fediuk <dfediuk@gmail.com>
2024-02-20 21:17:37 +0000

    refactoring

--- app/code/community/HetNieuweWeb/CustomerNavigation/Block/Customer/Account/Navigation.php
+++ app/code/community/HetNieuweWeb/CustomerNavigation/Block/Customer/Account/Navigation.php
@@ -27,7 +27,20 @@
 		}
 		
 		public function renameLinkByName() {
-			$NavigationLinks = array('account'=>'account', 'account_edit'=>'account_edit', 'address_book'=>'address_book', 'orders'=>'orders', 'billing_agreements'=>'billing_agreements', 'recurring_profiles'=>'recurring_profiles', 'reviews'=>'reviews', 'tags'=>'tags', 'wishlist'=>'wishlist', 'OAuth Customer Tokens'=>'oauth_customer_tokens', 'newsletter'=>'newsletter', 'downloadable_products'=>'downloadable_products');			
+			$NavigationLinks = [
+				'account'=>'account'
+				, 'account_edit'=>'account_edit'
+				, 'address_book'=>'address_book'
+				, 'orders'=>'orders'
+				, 'billing_agreements'=>'billing_agreements'
+				, 'recurring_profiles'=>'recurring_profiles'
+				, 'reviews'=>'reviews'
+				, 'tags'=>'tags'
+				, 'wishlist'=>'wishlist'
+				, 'OAuth Customer Tokens'=>'oauth_customer_tokens'
+				, 'newsletter'=>'newsletter'
+				, 'downloadable_products'=>'downloadable_products'
+			];
 			
 			foreach ($NavigationLinks as $link => $configName) {
 				$name = Mage::getStoreConfig('customer_navigation/rename/'.$configName);
