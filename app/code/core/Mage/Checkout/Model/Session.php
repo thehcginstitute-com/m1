67f5b39544c7441f637bc78f3b47d70c1c99b394
Dmitrii Fediuk <dfediuk@gmail.com>
2024-02-20 21:13:26 +0000

    https://github.com/thehcginstitute-com/m1/issues/400

--- app/code/core/Mage/Checkout/Model/Session.php
+++ app/code/core/Mage/Checkout/Model/Session.php
@@ -44,8 +44,8 @@
  *
  * @method int getLastAddedProductId()
  * @method $this setLastAddedProductId(int $value)
- * @method int getLastBillingAgreementId()
- * @method $this setLastBillingAgreementId(int $value)
+ * 2024-02-21 Dmitrii Fediuk https://upwork.com/fl/mage2pro
+ * "Delete the unused «Billing Agreements» feature": https://github.com/thehcginstitute-com/m1/issues/400
  * @method int getLastOrderId()
  * @method $this setLastOrderId(int $value)
  * @method $this unsLastOrderId()
