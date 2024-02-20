42626c48f8f486d1631ed65aa788175b5b11660e
Dmitrii Fediuk <dfediuk@gmail.com>
2024-02-20 22:28:31 +0000

    https://github.com/thehcginstitute-com/m1/issues/407

--- app/code/core/Mage/Sales/Model/Quote.php
+++ app/code/core/Mage/Sales/Model/Quote.php
@@ -1002,20 +1002,8 @@ class Mage_Sales_Model_Quote extends Mage_Core_Model_Abstract
      */
     public function addItem(Mage_Sales_Model_Quote_Item $item)
     {
-        /**
-         * Temporary workaround for purchase process: it is too dangerous to purchase more than one nominal item
-         * or a mixture of nominal and non-nominal items, although technically possible.
-         *
-         * The problem is that currently it is implemented as sequential submission of nominal items and order, by one click.
-         * It makes logically impossible to make the process of the purchase failsafe.
-         * Proper solution is to submit items one by one with customer confirmation each time.
-         */
-        if ($item->isNominal() && $this->hasItems() || $this->hasNominalItems()) {
-            Mage::throwException(
-                Mage::helper('sales')->__('Nominal item can be purchased standalone only. To proceed please remove other items from the quote.')
-            );
-        }
-
+		# 2024-02-21 Dmitrii Fediuk https://upwork.com/fl/mage2pro
+		# "Delete the unused «Nominal products» feature": https://github.com/thehcginstitute-com/m1/issues/407
         $item->setQuote($this);
         if (!$item->getId()) {
             $this->getItemsCollection()->addItem($item);
