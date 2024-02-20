5d6845da7845eec0af565c9dea53efeae592a6ef
Dmitrii Fediuk <dfediuk@gmail.com>
2024-02-20 21:59:01 +0000

    https://github.com/thehcginstitute-com/m1/issues/401

--- app/code/core/Mage/Sales/controllers/DownloadController.php
+++ app/code/core/Mage/Sales/controllers/DownloadController.php
@@ -103,48 +103,8 @@ class Mage_Sales_DownloadController extends Mage_Core_Controller_Front_Action
         return true;
     }
 
-    /**
-     * Profile custom options download action
-     */
-    public function downloadProfileCustomOptionAction()
-    {
-        $recurringProfile = Mage::getModel('sales/recurring_profile')->load($this->getRequest()->getParam('id'));
-
-        if (!$recurringProfile->getId()) {
-            $this->_forward('noRoute');
-        }
-
-        $orderItemInfo = $recurringProfile->getData('order_item_info');
-        try {
-            $request = unserialize($orderItemInfo['info_buyRequest'], ['allowed_classes' => false]);
-
-            if ($request['product'] != $orderItemInfo['product_id']) {
-                $this->_forward('noRoute');
-                return;
-            }
-
-            $optionId = $this->getRequest()->getParam('option_id');
-            if (!isset($request['options'][$optionId])) {
-                $this->_forward('noRoute');
-                return;
-            }
-            // Check if the product exists
-            $product = Mage::getModel('catalog/product')->load($request['product']);
-            if (!$product || !$product->getId()) {
-                $this->_forward('noRoute');
-                return;
-            }
-            // Try to load the option
-            $option = $product->getOptionById($optionId);
-            if (!$option || !$option->getId() || $option->getType() != 'file') {
-                $this->_forward('noRoute');
-                return;
-            }
-            $this->_downloadFileAction($request['options'][$this->getRequest()->getParam('option_id')]);
-        } catch (Exception $e) {
-            $this->_forward('noRoute');
-        }
-    }
+	# 2024-02-21 Dmitrii Fediuk https://upwork.com/fl/mage2pro
+	# "Delete the unused «Recurring Profiles» feature": https://github.com/thehcginstitute-com/m1/issues/401
 
     /**
      * Custom options download action
