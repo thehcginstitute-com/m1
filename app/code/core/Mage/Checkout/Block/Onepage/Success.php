0f166bd2054c2ee9ea5a8cdc386e23eca878086c
Dmitrii Fediuk <dfediuk@gmail.com>
2024-02-20 21:46:50 +0000

    https://github.com/thehcginstitute-com/m1/issues/401

--- app/code/core/Mage/Checkout/Block/Onepage/Success.php
+++ app/code/core/Mage/Checkout/Block/Onepage/Success.php
@@ -102,8 +102,8 @@ class Mage_Checkout_Block_Onepage_Success extends Mage_Core_Block_Template
     {
         $this->_prepareLastOrder();
 		# 2024-02-21 Dmitrii Fediuk https://upwork.com/fl/mage2pro
-		# "Delete the unused «Billing Agreements» feature": https://github.com/thehcginstitute-com/m1/issues/400
-        $this->_prepareLastRecurringProfiles();
+		# 1) "Delete the unused «Billing Agreements» feature": https://github.com/thehcginstitute-com/m1/issues/400
+		# 2) "Delete the unused «Recurring Profiles» feature": https://github.com/thehcginstitute-com/m1/issues/401
         return parent::_beforeToHtml();
     }
 
