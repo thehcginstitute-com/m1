5f904d017e67ac870a1c02368acd8e7aeb77c4ba
Dmitrii Fediuk <dfediuk@gmail.com>
2024-02-20 21:12:33 +0000

    https://github.com/thehcginstitute-com/m1/issues/400

--- app/code/core/Mage/Sales/sql/sales_setup/mysql4-upgrade-1.4.0.18-1.4.0.19.php
+++ app/code/core/Mage/Sales/sql/sales_setup/mysql4-upgrade-1.4.0.18-1.4.0.19.php
@@ -1,25 +1,3 @@
 <?php
-/**
- * OpenMage
- *
- * This source file is subject to the Open Software License (OSL 3.0)
- * that is bundled with this package in the file LICENSE.txt.
- * It is also available at https://opensource.org/license/osl-3-0-php
- *
- * @category   Mage
- * @package    Mage_Sales
- * @copyright  Copyright (c) 2006-2020 Magento, Inc. (https://www.magento.com)
- * @copyright  Copyright (c) 2020-2022 The OpenMage Contributors (https://www.openmage.org)
- * @license    https://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
- */
-
-/** @var Mage_Sales_Model_Entity_Setup $installer */
-$installer = $this;
-
-$billingAgreementTable = $installer->getTable('sales/billing_agreement');
-
-$installer->getConnection()->addColumn(
-    $billingAgreementTable,
-    'agreement_label',
-    'varchar(255)'
-);
+# 2024-02-21 Dmitrii Fediuk https://upwork.com/fl/mage2pro
+# "Delete the unused «Billing Agreements» feature": https://github.com/thehcginstitute-com/m1/issues/400
