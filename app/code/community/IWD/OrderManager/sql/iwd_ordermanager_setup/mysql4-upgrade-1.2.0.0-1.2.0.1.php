6fdb342a50ed732232f4661a85e495ff4407985d
Dmitrii Fediuk <dfediuk@gmail.com>
2024-02-21 09:08:28 +0000

    https://github.com/thehcginstitute-com/m1/issues/412

--- app/code/community/IWD/OrderManager/sql/iwd_ordermanager_setup/mysql4-upgrade-1.2.0.0-1.2.0.1.php
+++ app/code/community/IWD/OrderManager/sql/iwd_ordermanager_setup/mysql4-upgrade-1.2.0.0-1.2.0.1.php
@@ -1,25 +1,3 @@
 <?php
-
-/** @var $installer Mage_Core_Model_Resource_Setup */
-$installer = $this;
-
-$installer->startSetup();
-
-try {
-    $installer->run(
-        "DROP TABLE IF EXISTS {$this->getTable('iwd_backup_flat_sales')};
-        CREATE TABLE {$this->getTable('iwd_backup_flat_sales')}(
-          `id` INT(11) NOT NULL AUTO_INCREMENT,
-          `admin_user_id` INT(11) NOT NULL,
-          `object_type` ENUM('order','invoice','shipment','creditmemo'),
-          `deletion_at` TIMESTAMP NOT NULL,
-          `object` TEXT,
-          `object_items` TEXT,
-          PRIMARY KEY (`id`)
-        ) ENGINE=INNODB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;"
-    );
-} catch (Exception $e) {
-    Mage::log($e->getMessage(), null, 'iwd_ordermanager_install.log');
-}
-
-$installer->endSetup();
+# 2024-02-21 Dmitrii Fediuk https://upwork.com/fl/mage2pro
+# "Delete the unused «Backup Sales» feature of `IWD_OrderManager`": https://github.com/thehcginstitute-com/m1/issues/412
