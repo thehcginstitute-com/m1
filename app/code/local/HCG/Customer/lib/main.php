2b6070ac4fe8b9cb0e37a1e3fc2bb81dab4f3a54
Dmitrii Fediuk <dfediuk@gmail.com>
2024-03-03 10:15:20 +0000

    https://github.com/thehcginstitute-com/m1/issues/443

--- app/code/local/HCG/Customer/lib/main.php
+++ app/code/local/HCG/Customer/lib/main.php
@@ -12,6 +12,7 @@ function hcg_customer_is_new():bool {return 4 === df_customer_group();}
 /**
  * 2024-03-03 Dmitrii Fediuk https://upwork.com/fl/mage2pro
  * "Implement the `hcg_is_patient()` function` template": https://github.com/thehcginstitute-com/m1/issues/443
- * @used-by app/design/frontend/default/mobileshoppe/template/catalog/category/view.phtml()
+ * @used-by app/design/frontend/default/mobileshoppe/template/catalog/category/view.phtml
+ * @used-by app/design/frontend/default/mobileshoppe/template/customer/account/dashboard/hello.phtml
  */
 function hcg_is_patient():bool {return 1 === df_customer_group();}
