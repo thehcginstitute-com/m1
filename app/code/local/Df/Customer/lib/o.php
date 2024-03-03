21c6671f1b50e24f7f9744e5675fefd173a64be1
Dmitrii Fediuk <dfediuk@gmail.com>
2024-03-03 09:55:44 +0000

    https://github.com/thehcginstitute-com/m1/issues/445

--- app/code/local/Df/Customer/lib/o.php
+++ app/code/local/Df/Customer/lib/o.php
@@ -3,5 +3,7 @@ use Mage_Customer_Model_Session as S;
 /**
  * 2024-03-03 Dmitrii Fediuk https://upwork.com/fl/mage2pro
  * "Implement the `df_customer_session()` function": https://github.com/thehcginstitute-com/m1/issues/445
+ * @used-by hcg_customer_is_new()
+ * @used-by hcg_is_patient()
  */
 function df_customer_session():S {return Mage::getSingleton('customer/session');}
