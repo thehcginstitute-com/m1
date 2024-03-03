c408f1e316f0604b9c9bf7ba429f86d8f203dfb4
Dmitrii Fediuk <dfediuk@gmail.com>
2024-03-03 17:19:08 +0300

    https://github.com/thehcginstitute-com/m1/issues/453

--- app/code/local/Df/Core/lib/array/clean/filter.php
+++ app/code/local/Df/Core/lib/array/clean/filter.php
@@ -16,3 +16,39 @@
  * @return array(int|string => mixed)
  */
 function df_filter($a1, $a2):array {return df_filter_f($a1, $a2, 'array_filter');}
+
+/**
+ * 2023-07-26
+ * 2024-03-03 "Port `df_filter_f()` from `mage2pro/core`": https://github.com/thehcginstitute-com/m1/issues/453
+ * @used-by df_filter()
+ * @used-by df_filter_head()
+ * @used-by df_filter_tail()
+ * @param callable|array(int|string => mixed)|array[]Traversable $a1
+ * @param callable|array(int|string => mixed)|array[]|Traversable $a2
+ * @param callable $fA
+ * @return array(int|string => mixed)
+ */
+function df_filter_f($a1, $a2, $fA):array {/** @var array $r */
+	# 2020-03-02, 2022-10-31
+	# 1) Symmetric array destructuring requires PHP ≥ 7.1:
+	#		[$a, $b] = [1, 2];
+	# https://github.com/mage2pro/core/issues/96#issuecomment-593392100
+	# We should support PHP 7.0.
+	# https://3v4l.org/3O92j
+	# https://php.net/manual/migration71.new-features.php#migration71.new-features.symmetric-array-destructuring
+	# https://stackoverflow.com/a/28233499
+	list($a, $fI) = dfaf($a1, $a2); /** @var iterable $a */ /** @var callable $fI */
+	$a = df_ita($a);
+	$r = call_user_func($fA, $a, $fI);
+	/**
+	 * 2017-02-16
+	 * Если исходный массив был неассоциативным, то после удаления из него элементов в индексах будут бреши.
+	 * Это может приводить к неприятным последствиям:
+	 * 1) @see df_is_assoc() для такого массива уже будет возвращать false, а не true, как для входного массива.
+	 * 2) @see df_json_encode() будет кодировать такой массив как объект, а не как массив,
+	 * что может привести (и приводит, например, у 2Checkout) к сбоям различных API
+	 * 3) Последующие алгоритмы, считающие, что массив — неассоциативный, могут работать сбойно.
+	 * По всем этим причинам привожу результат к неассоциативному виду, если исходный массив был неассоциативным.
+	 */
+	return df_is_assoc($a) ? $r : array_values($r);
+}
