eecdd871b5e1700255d05415449b6885d771374d
Dmitrii Fediuk <dfediuk@gmail.com>
2024-03-03 10:07:55 +0000

    https://github.com/thehcginstitute-com/m1/issues/447

--- app/code/local/Df/Customer/lib/main.php
+++ app/code/local/Df/Customer/lib/main.php
@@ -2,5 +2,6 @@
 /**
  * 2024-03-03
  * "Implement the `df_customer_is_anon()` function": https://github.com/thehcginstitute-com/m1/issues/447
+ * @used-by app/design/frontend/default/mobileshoppe/template/catalog/category/view.phtml()
  */
 function df_customer_is_anon():bool {return !df_customer_group();}
--- app/code/local/HCG/Customer/lib/main.php
+++ app/code/local/HCG/Customer/lib/main.php
@@ -9,5 +9,6 @@ function hcg_customer_is_new():bool {return 4 === df_customer_group();}
 /**
  * 2024-03-03 Dmitrii Fediuk https://upwork.com/fl/mage2pro
  * "Implement the `hcg_is_patient()` function` template": https://github.com/thehcginstitute-com/m1/issues/443
+ * @used-by app/design/frontend/default/mobileshoppe/template/catalog/category/view.phtml()
  */
 function hcg_is_patient():bool {return 1 === df_customer_group();}
