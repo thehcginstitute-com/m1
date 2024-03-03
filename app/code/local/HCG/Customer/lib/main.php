1caba0c006f0119988f43f7781038fd3603be823
Dmitrii Fediuk <dfediuk@gmail.com>
2024-03-03 10:11:15 +0000

    https://github.com/thehcginstitute-com/m1/issues/442

--- app/code/local/HCG/Customer/lib/main.php
+++ app/code/local/HCG/Customer/lib/main.php
@@ -2,6 +2,7 @@
 /**
  * 2024-03-03 Dmitrii Fediuk https://upwork.com/fl/mage2pro
  * "Implement the `hcg_customer_is_new()` function` template": https://github.com/thehcginstitute-com/m1/issues/442
+ * @used-by app/design/frontend/default/mobileshoppe/template/catalog/product/compare/sidebar.phtml
  * @used-by app/design/frontend/default/mobileshoppe/template/customer/account/navigation.phtml
  */
 function hcg_customer_is_new():bool {return 4 === df_customer_group();}
