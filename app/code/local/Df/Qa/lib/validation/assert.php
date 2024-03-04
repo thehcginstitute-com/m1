d69d0d876d5f1f77c9d3b1b67a9a35886dc668e3
Dmitrii Fediuk <dfediuk@gmail.com>
2024-03-04 21:40:40 +0000

    https://github.com/thehcginstitute-com/m1/issues/459

--- app/code/local/Df/Qa/lib/validation/assert.php
+++ app/code/local/Df/Qa/lib/validation/assert.php
@@ -1,6 +1,7 @@
 <?php
 use Df\Qa\Method as Q;
 use Exception as E;
+use Throwable as Th; # 2023-08-03 "Treat `\Throwable` similar to `\Exception`": https://github.com/mage2pro/core/issues/311
 
 /**
  * 2020-01-21
@@ -25,6 +26,19 @@ function df_assert_sne($v, $sl = 0) {
 	# The previous code `if (!$v)` was wrong because it rejected the '0' string.
 	return !df_es($v) ? $v : Q::raiseErrorVariable(__FUNCTION__, [Q::NES], $sl);
 }
+
+/**
+ * 2016-08-09
+ * 2024-03-05 "Port `df_assert_traversable()` from `mage2pro/core`": https://github.com/thehcginstitute-com/m1/issues/459
+ * @used-by dfaf()
+ * @param Traversable|array $v
+ * @param string|Th $m [optional]
+ * @return Traversable|array
+ * @throws E
+ */
+function df_assert_traversable($v, $m = null) {return is_iterable($v) ? $v : df_error($m ?:
+	'A variable is expected to be a Traversable or an array, ' . 'but actually it is %s.', df_type($v)
+);}
 
 /**
  * @used-by \HetNieuweWeb_CustomerNavigation_Block_Customer_Account_Navigation::removeLinkByName()
