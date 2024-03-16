2e291b678fdd9d5ac0b182b69547ad38c11b4d2c
Dmitrii Fediuk <dfediuk@gmail.com>
2024-03-16 14:50:26 +0100

    https://github.com/thehcginstitute-com/m1/issues/487

--- app/code/local/Df/Qa/lib/validation/error.php
+++ app/code/local/Df/Qa/lib/validation/error.php
@@ -3,6 +3,7 @@ use Exception as E;
 
 /**
  * 2020-01-21
+ * 2024-03-16 "Port `df_error` from `mage2pro/core`": https://github.com/thehcginstitute-com/m1/issues/487
  * @used-by df_assert()
  * @param string|string[]|mixed|E|null ...$m
  * @throws E
