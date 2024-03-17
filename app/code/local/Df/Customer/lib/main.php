68ac5e48d92bf6bda829187295064551304fad18
Dmitrii Fediuk <dfediuk@gmail.com>
2024-03-17 08:08:59 +0300

    https://github.com/thehcginstitute-com/m1/issues/498

--- app/code/local/Df/Customer/lib/main.php
+++ app/code/local/Df/Customer/lib/main.php
@@ -1,4 +1,20 @@
 <?php
+use Mage_Customer_Model_Customer as C;
+
+/**
+ * 2016-12-04
+ * 2024-03-17 "Port `df_customer_id()` from `mage2pro/core`": https://github.com/thehcginstitute-com/m1/issues/498
+ * @used-by df_context()
+ * @used-by df_customer()
+ * @used-by df_customer_is_need_confirm()
+ * @used-by vendor/inkifi/mediaclip-legacy/view/frontend/templates/savedproject.phtml
+ * @param C|int|null $c [optional]
+ * @return int|null
+ */
+function df_customer_id($c = null) {return !$c && !df_is_backend() ? df_customer_session()->getId() : (
+	$c instanceof C ? $c->getId() : $c
+);}
+
 /**
  * 2024-03-03
  * "Implement the `df_customer_is_anon()` function": https://github.com/thehcginstitute-com/m1/issues/447
--- app/code/local/Df/Customer/lib/o.php
+++ app/code/local/Df/Customer/lib/o.php
@@ -4,5 +4,6 @@ use Mage_Customer_Model_Session as S;
  * 2024-03-03 Dmitrii Fediuk https://upwork.com/fl/mage2pro
  * "Implement the `df_customer_session()` function": https://github.com/thehcginstitute-com/m1/issues/445
  * @used-by df_customer_group()
+ * @used-by df_customer_id()
  */
 function df_customer_session():S {return Mage::getSingleton('customer/session');}
