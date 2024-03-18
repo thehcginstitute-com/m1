1e059fdb66bf3ee9749312196b0bf5931fe2171c
Dmitrii Fediuk <dfediuk@gmail.com>
2024-03-18 02:45:28 +0100

    https://github.com/thehcginstitute-com/m1/issues/518

--- app/code/community/Widgento/Login/Model/Observer.php
+++ app/code/community/Widgento/Login/Model/Observer.php
@@ -1,22 +1,22 @@
 <?php
 class Widgento_Login_Model_Observer
 {
-    private static $availableActionNames = array(
-        'widgentologin_index_index',
-    );
+	private static $availableActionNames = array(
+		'widgentologin_index_index',
+	);
 
-    /**
-     * @event websiterestriction_frontend
-     * @param Varien_Event_Observer $observer
-     */
-    function removeFrontendRestrictions(Varien_Event_Observer $observer)
-    {
-        /* @var $controller Mage_Core_Controller_Varien_Action */
-        $controller = $observer->getController();
-        $result     = $observer->getResult();
+	/**
+	 * @event websiterestriction_frontend
+	 * @param Varien_Event_Observer $observer
+	 */
+	function removeFrontendRestrictions(Varien_Event_Observer $observer)
+	{
+		/* @var $controller Mage_Core_Controller_Varien_Action */
+		$controller = $observer->getController();
+		$result     = $observer->getResult();
 
-        if (in_array($controller->getFullActionName(), self::$availableActionNames)) {
-            $result->setShouldProceed(false);
-        }
-    }
+		if (in_array($controller->getFullActionName(), self::$availableActionNames)) {
+			$result->setShouldProceed(false);
+		}
+	}
 }
