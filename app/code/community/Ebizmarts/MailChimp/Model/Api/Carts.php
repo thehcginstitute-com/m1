ac8175a6b3d88d423e62ae7f7ca0e9547865ab8d
Dmitrii Fediuk <dfediuk@gmail.com>
2024-03-22 04:54:22 +0100

    https://github.com/thehcginstitute-com/m1/issues/524

--- app/code/community/Ebizmarts/MailChimp/Model/Api/Carts.php
+++ app/code/community/Ebizmarts/MailChimp/Model/Api/Carts.php
@@ -384,7 +384,7 @@ class Ebizmarts_MailChimp_Model_Api_Carts extends Ebizmarts_MailChimp_Model_Api_
 		$r = ''; /** @var string|false $r */
 		$apiProducts = self::apiProducts(); /** @var ApiProducts $apiProducts */
 		$apiProducts->setMagentoStoreId($sid = $this->getMagentoStoreId());
-		$campaignId = $cart->getMailchimpCampaignId();
+		$campaignId = $cart['mailchimp_campaign_id'];
 		$ra = ['id' => $cart->getEntityId()];
 		$customer = $this->_getCustomer($cart, $sid);
 		if (!empty($customer)) {
