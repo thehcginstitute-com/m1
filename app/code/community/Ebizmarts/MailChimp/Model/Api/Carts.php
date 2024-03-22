c6e4b71c73518c9933a035b5c3b02eaed7c7da97
Dmitrii Fediuk <dfediuk@gmail.com>
2024-03-22 05:00:00 +0100

    https://github.com/thehcginstitute-com/m1/issues/524

--- app/code/community/Ebizmarts/MailChimp/Model/Api/Carts.php
+++ app/code/community/Ebizmarts/MailChimp/Model/Api/Carts.php
@@ -2,7 +2,7 @@
 # 2024-03-22 Dmitrii Fediuk https://upwork.com/fl/mage2pro
 # "Refactor the `Ebizmarts_MailChimp` module": https://github.com/thehcginstitute-com/m1/issues/524
 use Ebizmarts_MailChimp_Model_Api_Products as ApiProducts;
-use Varien_Object as VO;
+use Mage_Core_Model_Abstract as M;
 class Ebizmarts_MailChimp_Model_Api_Carts extends Ebizmarts_MailChimp_Model_Api_ItemSynchronizer
 {
 	const BATCH_LIMIT = 100;
@@ -380,7 +380,7 @@ class Ebizmarts_MailChimp_Model_Api_Carts extends Ebizmarts_MailChimp_Model_Api_
 	 * @used-by self::_getNewQuotes()
 	 * @return string|false
 	 */
-	private function makeCart(VO $cart, bool $isModified) {
+	private function makeCart(M $cart, bool $isModified) {
 		$r = ''; /** @var string|false $r */
 		$apiProducts = self::apiProducts(); /** @var ApiProducts $apiProducts */
 		$apiProducts->setMagentoStoreId($sid = $this->getMagentoStoreId());
