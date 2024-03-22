886e36bc2aafef17f15026f83a623f1a41adff7a
Dmitrii Fediuk <dfediuk@gmail.com>
2024-03-22 06:48:59 +0100

    https://github.com/thehcginstitute-com/m1/issues/524

--- app/code/community/Ebizmarts/MailChimp/Model/Api/ItemSynchronizer.php
+++ app/code/community/Ebizmarts/MailChimp/Model/Api/ItemSynchronizer.php
@@ -315,6 +315,7 @@ class Ebizmarts_MailChimp_Model_Api_ItemSynchronizer
 	 * @used-by Ebizmarts_MailChimp_Model_Api_PromoCodes::deletePromoCodeSyncData()
 	 * @used-by Ebizmarts_MailChimp_Model_Api_PromoCodes::getPromoCodesForRule()
 	 * @used-by Ebizmarts_MailChimp_Model_Api_PromoRules::makeModifiedAndDeletedPromoRulesCollection()
+	 * @used-by Ebizmarts_MailChimp_Model_Api_PromoRules::deletePromoRuleSyncData()
 	 */
 	function getMailchimpEcommerceSyncDataModel():SyncD {return new SyncD;}
 
