8754153e0d092e1c3b232f0217d231b009e77189
Dmitrii Fediuk <dfediuk@gmail.com>
2024-03-22 06:45:28 +0100

    https://github.com/thehcginstitute-com/m1/issues/524

--- app/code/community/Ebizmarts/MailChimp/Model/Api/ItemSynchronizer.php
+++ app/code/community/Ebizmarts/MailChimp/Model/Api/ItemSynchronizer.php
@@ -302,6 +302,7 @@ class Ebizmarts_MailChimp_Model_Api_ItemSynchronizer
 	 * @used-by self::markAllSyncDataAsModified()
 	 * @used-by Ebizmarts_MailChimp_Model_Api_Carts::_processCartLines()
 	 * @used-by Ebizmarts_MailChimp_Model_Api_Customers::createBatchJson()
+	 * @used-by Ebizmarts_MailChimp_Model_Api_Orders::_getPayloadDataLines()
 	 */
 	function getMailchimpEcommerceSyncDataModel():SyncD {return new SyncD;}
 
