8a78fdd146991a04bde806184ed0b856c9202c5a
Dmitrii Fediuk <dfediuk@gmail.com>
2024-03-23 20:42:26 +0100

    https://github.com/thehcginstitute-com/m1/issues/524

--- app/code/community/Ebizmarts/MailChimp/controllers/Adminhtml/EcommerceController.php
+++ app/code/community/Ebizmarts/MailChimp/controllers/Adminhtml/EcommerceController.php
@@ -13,131 +13,131 @@
  */
 class Ebizmarts_MailChimp_Adminhtml_EcommerceController extends Mage_Adminhtml_Controller_Action
 {
-    function renderresendecomAction()
-    {
-        $this->loadLayout();
-        $this->renderLayout();
-    }
+	function renderresendecomAction()
+	{
+		$this->loadLayout();
+		$this->renderLayout();
+	}
 
-    function resetLocalErrorsAction()
-    {
-        $helper = $this->makeHelper();
-        $mageApp = $helper->getMageApp();
-        $request = $mageApp->getRequest();
-        $scope = $request->getParam('scope');
-        $scopeId = $request->getParam('scope_id');
-        $success = 1;
+	function resetLocalErrorsAction()
+	{
+		$helper = $this->makeHelper();
+		$mageApp = $helper->getMageApp();
+		$request = $mageApp->getRequest();
+		$scope = $request->getParam('scope');
+		$scopeId = $request->getParam('scope_id');
+		$success = 1;
 
-        try {
-            $stores = $mageApp->getStores();
+		try {
+			$stores = $mageApp->getStores();
 
-            if ($scopeId == 0) {
-                foreach ($stores as $store) {
-                    $helper->resetErrors($store->getId());
-                }
-            }
+			if ($scopeId == 0) {
+				foreach ($stores as $store) {
+					$helper->resetErrors($store->getId());
+				}
+			}
 
-            $helper->resetErrors($scopeId, $scope);
-        } catch (Exception $e) {
-            $helper->logError($e->getMessage());
-            $success = 0;
-        }
+			$helper->resetErrors($scopeId, $scope);
+		} catch (Exception $e) {
+			$helper->logError($e->getMessage());
+			$success = 0;
+		}
 
-        $mageApp->getResponse()->setBody($success);
-    }
+		$mageApp->getResponse()->setBody($success);
+	}
 
-    function resendEcommerceDataAction()
-    {
-        $helper = $this->makeHelper();
-        $mageApp = $helper->getMageApp();
-        $request = $this->getRequest();
-        $filters = $request->getParam('filter');
-        $scope = $request->getParam('scope');
-        $scopeId = $request->getParam('scope_id');
-        $success = 0;
+	function resendEcommerceDataAction()
+	{
+		$helper = $this->makeHelper();
+		$mageApp = $helper->getMageApp();
+		$request = $this->getRequest();
+		$filters = $request->getParam('filter');
+		$scope = $request->getParam('scope');
+		$scopeId = $request->getParam('scope_id');
+		$success = 0;
 
-        if (is_array($filters) && empty($filters)) {
-            $this->addWarning($helper->__('At least one type of eCommerce data should be selected to Resend.'));
-            $success = $helper->__('Redirecting... ')
-                . '<script type="text/javascript">window.top.location.reload();</script>';
-        } else {
-            try {
-                $helper->resendMCEcommerceData($scopeId, $scope, $filters);
+		if (is_array($filters) && empty($filters)) {
+			$this->addWarning($helper->__('At least one type of eCommerce data should be selected to Resend.'));
+			$success = $helper->__('Redirecting... ')
+				. '<script type="text/javascript">window.top.location.reload();</script>';
+		} else {
+			try {
+				$helper->resendMCEcommerceData($scopeId, $scope, $filters);
 
-                $this->addSuccess($helper->__('Ecommerce data resent succesfully'));
-                $success = $helper->__('Redirecting... ')
-                    . '<script type="text/javascript">window.top.location.reload();</script>';
-            } catch (MailChimp_Error $e) {
-                $helper->logError($e->getFriendlyMessage());
-                $this->addError($e->getFriendlyMessage());
-            } catch (Exception $e) {
-                $helper->logError($e->getMessage());
-                $this->addError($e->getMessage());
-            }
-        }
+				$this->addSuccess($helper->__('Ecommerce data resent succesfully'));
+				$success = $helper->__('Redirecting... ')
+					. '<script type="text/javascript">window.top.location.reload();</script>';
+			} catch (MailChimp_Error $e) {
+				$helper->logError($e->getFriendlyMessage());
+				$this->addError($e->getFriendlyMessage());
+			} catch (Exception $e) {
+				$helper->logError($e->getMessage());
+				$this->addError($e->getMessage());
+			}
+		}
 
-        $mageApp->getResponse()->setBody($success);
-    }
+		$mageApp->getResponse()->setBody($success);
+	}
 
-    function createMergeFieldsAction()
-    {
-        $helper = $this->makeHelper();
-        $mageApp = $helper->getMageApp();
-        $request = $mageApp->getRequest();
-        $scope = $request->getParam('scope');
-        $scopeId = $request->getParam('scope_id');
-        $success = 0;
-        $subEnabled = $helper->isSubscriptionEnabled($scopeId, $scope);
+	function createMergeFieldsAction()
+	{
+		$helper = $this->makeHelper();
+		$mageApp = $helper->getMageApp();
+		$request = $mageApp->getRequest();
+		$scope = $request->getParam('scope');
+		$scopeId = $request->getParam('scope_id');
+		$success = 0;
+		$subEnabled = $helper->isSubscriptionEnabled($scopeId, $scope);
 
-        if ($subEnabled) {
-            $success = $helper->createMergeFields($scopeId, $scope);
-        }
+		if ($subEnabled) {
+			$success = $helper->createMergeFields($scopeId, $scope);
+		}
 
-        $mageApp->getResponse()->setBody($success);
-    }
+		$mageApp->getResponse()->setBody($success);
+	}
 
-    protected function _isAllowed()
-    {
-        switch ($this->getRequest()->getActionName()) {
-        case 'resetLocalErrors':
-        case 'renderresendecom':
-        case 'resendEcommerceData':
-        case 'createMergeFields':
-            $acl = 'system/config/mailchimp';
-            break;
-        }
+	protected function _isAllowed()
+	{
+		switch ($this->getRequest()->getActionName()) {
+		case 'resetLocalErrors':
+		case 'renderresendecom':
+		case 'resendEcommerceData':
+		case 'createMergeFields':
+			$acl = 'system/config/mailchimp';
+			break;
+		}
 
-        return $this->getAdminSession()->isAllowed($acl);
-    }
+		return $this->getAdminSession()->isAllowed($acl);
+	}
 
-    /**
-     * @return Ebizmarts_MailChimp_Helper_Data
-     */
-    protected function makeHelper()
-    {
-        return Mage::helper('mailchimp');
-    }
+	/**
+	 * @return Ebizmarts_MailChimp_Helper_Data
+	 */
+	protected function makeHelper()
+	{
+		return Mage::helper('mailchimp');
+	}
 
-    /**
-     * @return Mage_Adminhtml_Model_Session
-     */
-    protected function getAdminSession()
-    {
-        return Mage::getSingleton('admin/session');
-    }
+	/**
+	 * @return Mage_Adminhtml_Model_Session
+	 */
+	protected function getAdminSession()
+	{
+		return Mage::getSingleton('admin/session');
+	}
 
-    function addWarning($message)
-    {
-        Mage::getSingleton('core/session')->addWarning($message);
-    }
+	function addWarning($message)
+	{
+		Mage::getSingleton('core/session')->addWarning($message);
+	}
 
-    function addSuccess($message)
-    {
-        Mage::getSingleton('core/session')->addSuccess($message);
-    }
+	function addSuccess($message)
+	{
+		Mage::getSingleton('core/session')->addSuccess($message);
+	}
 
-    function addError($message)
-    {
-        Mage::getSingleton('core/session')->addError($message);
-    }
+	function addError($message)
+	{
+		Mage::getSingleton('core/session')->addError($message);
+	}
 }
