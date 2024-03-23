305ad305b634db68673d311e4be33bbbc7d5548c
Dmitrii Fediuk <dfediuk@gmail.com>
2024-03-23 10:13:11 +0100

    refactoring

--- app/code/local/Df/Framework/lib/db/conn.php
+++ app/code/local/Df/Framework/lib/db/conn.php
@@ -6,7 +6,7 @@ use Varien_Db_Adapter_Interface as IAdapter;
  * 2018-12-07
  * @return IAdapter|Mysql
  */
-function df_conn():IAdapter {static $r; return $r ? $r : $r = df_mage_r()->getConnection('write');}
+function df_conn():IAdapter {static $r; return $r ? $r : $r = df_db_resource()->getConnection('write');}
 
 /**
  * 2015-09-29
--- app/code/local/Df/Framework/lib/db/o.php
+++ app/code/local/Df/Framework/lib/db/o.php
@@ -5,4 +5,4 @@ use Mage_Core_Model_Resource as R;
  * 2018-12-07
  * @used-by df_conn()
  */
-function df_mage_r():R {return Mage::getSingleton('core/resource');}
+function df_db_resource():R {return Mage::getSingleton('core/resource');}
