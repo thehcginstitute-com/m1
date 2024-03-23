335a35059ee741ceecbe797711022eb6b74788f1
Dmitrii Fediuk <dfediuk@gmail.com>
2024-03-23 10:02:33 +0100

    https://github.com/thehcginstitute-com/m1/issues/525

--- app/code/local/Df/Framework/lib/db/fetch.php
+++ app/code/local/Df/Framework/lib/db/fetch.php
@@ -3,10 +3,6 @@
  * 2015-11-03
  * 2024-03-23 "Port `df_fetch_one()` from `mage2pro/core`": https://github.com/thehcginstitute-com/m1/issues/525
  * @used-by df_fetch_one_int()
- * @used-by \Alignet\Paymecheckout\Model\Client\Classic\Order\DataGetter::userCodePayme() (innomuebles.com, https://github.com/innomuebles/m2/issues/17)
- * @used-by \Dfe\CheckoutCom\Handler\Charge::paymentByTxnId()
- * @used-by \Dfe\Markdown\DbRecord::__construct()
- * @used-by \Inkifi\Consolidation\Processor::mcid()
  * @param string|string[] $cols
  * @param array(string => string) $compare
  * @return string|null|array(string => mixed)
