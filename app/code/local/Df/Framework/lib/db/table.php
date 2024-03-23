fced6872dbd023775321d36f560068ff571bde47
Dmitrii Fediuk <dfediuk@gmail.com>
2024-03-23 10:20:46 +0100

    refactoring

--- app/code/local/Df/Framework/lib/db/table.php
+++ app/code/local/Df/Framework/lib/db/table.php
@@ -4,18 +4,17 @@
  * и, глядя на реализацию @see Mage_Core_Model_Resource_Setup::getTable(),
  * которая выполняет кэширование для @see Mage_Core_Model_Resource::getTableName(),
  * я решил сделать аналогичную функцию, только доступную в произвольном контексте.
+ * @used-by df_db_column_add()
+ * @used-by df_db_column_describe()
+ * @used-by df_db_column_drop()
+ * @used-by df_db_column_exists()
+ * @used-by df_db_column_rename()
+ * @used-by df_db_drop_pk()
+ * @used-by df_db_from()
+ * @used-by df_next_increment()
+ * @used-by df_next_increment_set()
+ * @used-by df_table_delete()
+ * @used-by df_table_exists()
  * @param string|string[] $n
- * @return string
  */
-function df_table($n) {
-	static $cache; /** @var array(string => string) $cache */
-	/**
-	 * По аналогии с @see Mage_Core_Model_Resource_Setup::_getTableCacheName()
-	 * @var string $key
-	 */
-	$key = is_array($n) ? implode('_', $n) : $n;
-	if (!isset($cache[$key])) {
-		$cache[$key] = df_db_resource()->getTableName($n);
-	}
-	return $cache[$key];
-}
+function df_table($n):string {return dfcf(function($n) {return df_db_resource()->getTableName($n);}, [$n]);}
