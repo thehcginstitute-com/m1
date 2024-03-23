6665986f3f9e650d5f7af00ef8696e48ef36726b
Dmitrii Fediuk <dfediuk@gmail.com>
2024-03-23 11:13:15 +0100

    https://github.com/thehcginstitute-com/m1/issues/526

---
+++ app/code/local/Df/Framework/lib/db/main.php
@@ -0,0 +1,30 @@
+<?php
+use Varien_Db_Select as S;
+
+/**
+ * 2016-12-01
+ * 1) $cols could be:
+ * 1.1) a string to fetch a single column;
+ * 1.2) an array to fetch multiple columns.
+ * @see Zend_Db_Select::_tableCols()
+ *		if (!is_array($cols)) {
+ *			$cols = array($cols);
+ *		}
+ * https://github.com/zendframework/zf1/blob/release-1.12.16/library/Zend/Db/Select.php#L929-L931
+ * 2) The function always returns @see Select
+ * I added @see Zend_Db_Select to the PHPDoc return type declaration just for my IDE convenience.
+ * @used-by df_customer_att_pos_after()
+ * @used-by df_customer_is_new()
+ * @used-by df_fetch()
+ * @used-by df_fetch_col()
+ * @used-by df_fetch_col_max()
+ * @used-by df_fetch_one()
+ * @used-by df_trans_by_payment()
+ * @param string|Entity|array(string => string) $t
+ * @param string|string[] $cols [optional]
+ * @param string|null $schema [optional]
+ * @return S|Zend_Db_Select
+ */
+function df_db_from($t, $cols = '*', $schema = null) {return df_select()->from(
+	$t instanceof Entity ? $t->getEntityTable() : (is_array($t) ? $t : df_table($t)), $cols, $schema
+);}
