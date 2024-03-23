8ec4824859634502a717dfe6b2d0a73791c0ae46
Dmitrii Fediuk <dfediuk@gmail.com>
2024-03-23 09:20:46 +0100

    https://github.com/thehcginstitute-com/m1/issues/524

--- app/code/community/Ebizmarts/MailChimp/Model/Api/Carts.php
+++ app/code/community/Ebizmarts/MailChimp/Model/Api/Carts.php
@@ -432,7 +432,7 @@ class Ebizmarts_MailChimp_Model_Api_Carts extends Ebizmarts_MailChimp_Model_Api_
 			$sd = hcg_mc_syncd_get($pid, $isTypeProduct, $this->getMailchimpStoreId()); /** @var SyncD $sd */
 			if (($disabled = !$api->isProductEnabled($pid)) || $sd['mailchimp_sync_delta'] && !$sd['mailchimp_sync_error']) {
 				$lines[] = [
-					'id' => (string)++$count //id can not be 0 so we add 1 to $itemCount before setting the id
+					'id' => (string)++$count // id can not be 0 so we add 1 to $itemCount before setting the id
 					,'price' => $i->getRowTotal()
 					,'product_id' => $pid
 					,'product_variant_id' => $variantId
