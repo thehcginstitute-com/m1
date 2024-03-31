5130832678ed1968acb69599615a8f777f511d87
Dmitrii Fediuk <dfediuk@gmail.com>
2024-03-31 10:06:59 +0100

    https://github.com/thehcginstitute-com/m1/issues/533

--- app/code/community/IWD/OrderManager/Block/Adminhtml/Sales/Order/Address/Text.php
+++ app/code/community/IWD/OrderManager/Block/Adminhtml/Sales/Order/Address/Text.php
@@ -17,8 +17,11 @@ final class IWD_OrderManager_Block_Adminhtml_Sales_Order_Address_Text extends Ma
 	 * 2024-03-31 Dmitrii Fediuk https://upwork.com/fl/mage2pro
 	 * 1) "Refactor the `IWD_OrderManager` module": https://github.com/thehcginstitute-com/m1/issues/533
 	 * 2) It is set by @see IWD_OrderManager_Adminhtml_Sales_AddressController::getAddressTextAfterSave()
+	 * @used-by self::country()
+	 * @used-by self::region()
+	 * @used-by app/design/adminhtml/default/default/template/iwd/ordermanager/address/text.phtml
 	 * @return array(string => string)
-	 */		
+	 */
 	function address():array {return $this['address'];}
 
 	function country() {
