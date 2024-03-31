e902173b90f18843d44ee1945c19c64bb4c9e387
Dmitrii Fediuk <dfediuk@gmail.com>
2024-03-31 10:01:30 +0100

    https://github.com/thehcginstitute-com/m1/issues/533

--- app/code/community/IWD/OrderManager/Block/Adminhtml/Sales/Order/Address/Text.php
+++ app/code/community/IWD/OrderManager/Block/Adminhtml/Sales/Order/Address/Text.php
@@ -2,25 +2,25 @@
 
 class IWD_OrderManager_Block_Adminhtml_Sales_Order_Address_Text extends Mage_Adminhtml_Block_Widget
 {
-    public function __construct()
-    {
-        parent::__construct();
-        $this->setTemplate('iwd/ordermanager/address/text.phtml');
-    }
+	public function __construct()
+	{
+		parent::__construct();
+		$this->setTemplate('iwd/ordermanager/address/text.phtml');
+	}
 
-    public function getRegion()
-    {
-        $address = $this->getAddress();
+	public function getRegion()
+	{
+		$address = $this->getAddress();
 
-        return (!isset($address['region']) || empty($address['region']) ?
-            Mage::getModel('directory/region')->load($address['region_id'])->getName() :
-            $address['region']);
-    }
+		return (!isset($address['region']) || empty($address['region']) ?
+			Mage::getModel('directory/region')->load($address['region_id'])->getName() :
+			$address['region']);
+	}
 
-    public function getCountry()
-    {
-        $address = $this->getAddress();
+	public function getCountry()
+	{
+		$address = $this->getAddress();
 
-        return Mage::getModel('directory/country')->load($address['country_id'])->getName();
-    }
+		return Mage::getModel('directory/country')->load($address['country_id'])->getName();
+	}
 }
