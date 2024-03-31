533b8de149336f9e989be1482effc04aa7a8b920
Dmitrii Fediuk <dfediuk@gmail.com>
2024-03-31 10:02:43 +0100

    https://github.com/thehcginstitute-com/m1/issues/533

--- app/code/community/IWD/OrderManager/Block/Adminhtml/Sales/Order/Address/Text.php
+++ app/code/community/IWD/OrderManager/Block/Adminhtml/Sales/Order/Address/Text.php
@@ -9,16 +9,13 @@ final class IWD_OrderManager_Block_Adminhtml_Sales_Order_Address_Text extends Ma
 
 	function getRegion() {
 		$address = $this->getAddress();
-
 		return (!isset($address['region']) || empty($address['region']) ?
 			Mage::getModel('directory/region')->load($address['region_id'])->getName() :
 			$address['region']);
 	}
 
-	function getCountry()
-	{
+	function getCountry() {
 		$address = $this->getAddress();
-
 		return Mage::getModel('directory/country')->load($address['country_id'])->getName();
 	}
 }
