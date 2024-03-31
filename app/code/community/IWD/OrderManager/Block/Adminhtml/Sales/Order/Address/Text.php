bd8efd8b63478c4e422f99bd89510302384ca954
Dmitrii Fediuk <dfediuk@gmail.com>
2024-03-31 10:02:21 +0100

    https://github.com/thehcginstitute-com/m1/issues/533

--- app/code/community/IWD/OrderManager/Block/Adminhtml/Sales/Order/Address/Text.php
+++ app/code/community/IWD/OrderManager/Block/Adminhtml/Sales/Order/Address/Text.php
@@ -1,13 +1,13 @@
 <?php
-class IWD_OrderManager_Block_Adminhtml_Sales_Order_Address_Text extends Mage_Adminhtml_Block_Widget {
-	function __construct()
-	{
+# 2024-03-31 Dmitrii Fediuk https://upwork.com/fl/mage2pro
+# "Refactor the `IWD_OrderManager` module": https://github.com/thehcginstitute-com/m1/issues/533
+final class IWD_OrderManager_Block_Adminhtml_Sales_Order_Address_Text extends Mage_Adminhtml_Block_Widget {
+	function __construct() {
 		parent::__construct();
 		$this->setTemplate('iwd/ordermanager/address/text.phtml');
 	}
 
-	function getRegion()
-	{
+	function getRegion() {
 		$address = $this->getAddress();
 
 		return (!isset($address['region']) || empty($address['region']) ?
