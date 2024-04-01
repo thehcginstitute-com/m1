423d89ebb2084e24f525f505efb8a21c60231f2f
Dmitrii Fediuk <dfediuk@gmail.com>
2024-04-01 01:29:36 +0100

    https://github.com/thehcginstitute-com/m1/issues/541

--- app/code/local/HCG/Backend/lib/main.php
+++ app/code/local/HCG/Backend/lib/main.php
@@ -1,6 +1,13 @@
 <?php
 /**
  * 2024-04-01
+ * @param int|string|null $id [optional]
+ */
+function hcg_is_super_admin($id = 0):bool {return in_array((int)$id ?: df_backend_user_id(), hcg_super_admins());}
+
+/**
+ * 2024-04-01
  * "Restrict the access to bank card numbers in the backend": https://github.com/thehcginstitute-com/m1/issues/541
+ * @used-by hcg_is_super_admin()
  */
 function hcg_super_admins():array {return [5, 51];}
