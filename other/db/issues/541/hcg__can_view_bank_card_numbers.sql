c266a2f133278893f450fbb92ef01205cdcd2d9c
Dmitrii Fediuk <dfediuk@gmail.com>
2024-04-01 01:58:52 +0100

    https://github.com/thehcginstitute-com/m1/issues/541

--- other/db/issues/541/hcg__can_view_bank_card_numbers.sql
+++ other/db/issues/541/hcg__can_view_bank_card_numbers.sql
@@ -1,3 +1,5 @@
+-- 2024-04-01 Dmitrii Fediuk https://upwork.com/fl/mage2pro
+-- "Restrict the access to bank card numbers in the backend": https://github.com/thehcginstitute-com/m1/issues/541
 ALTER TABLE `admin_user`
 ADD `hcg__can_view_bank_card_numbers` SMALLINT(6) NOT NULL DEFAULT '0'
 COMMENT 'https://github.com/thehcginstitute-com/m1/issues/541'
