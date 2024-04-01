aecec858ed75ab76ad965e6d2b35882b3a6c6300
Dmitrii Fediuk <dfediuk@gmail.com>
2024-04-01 02:35:10 +0100

    https://github.com/thehcginstitute-com/m1/issues/142

--- app/code/community/INT/DisplayCvv/Helper/Data.php
+++ app/code/community/INT/DisplayCvv/Helper/Data.php
@@ -1,13 +1,8 @@
 <?php
-
-class INT_DisplayCvv_Helper_Data extends Mage_Core_Helper_Abstract
-{
-	function removeCVV($payement_quote_id)
-	{
+class INT_DisplayCvv_Helper_Data extends Mage_Core_Helper_Abstract {
+	function removeCVV($payement_quote_id) {
 		$connection = Mage::getSingleton('core/resource')->getConnection('core_read');
-
 		$connection->update("sales_flat_quote_payment",array("cc_cid_enc" => ''),"quote_id=$payement_quote_id");
 		echo $payement_quote_id;
 	}
-
 }
