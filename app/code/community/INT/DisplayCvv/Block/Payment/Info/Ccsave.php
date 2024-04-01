f90eaa661aa3b4db5e2d06a2377b12fc39e5bd04
Dmitrii Fediuk <dfediuk@gmail.com>
2024-04-01 02:12:43 +0100

    https://github.com/thehcginstitute-com/m1/issues/142

--- app/code/community/INT/DisplayCvv/Block/Payment/Info/Ccsave.php
+++ app/code/community/INT/DisplayCvv/Block/Payment/Info/Ccsave.php
@@ -77,8 +77,8 @@ class INT_DisplayCvv_Block_Payment_Info_Ccsave extends Mage_Payment_Block_Info_C
 			else {
 				$transport->addData(array(
 					Mage::helper('payment')->__('Expiration Date') =>
-						$this->_formatCardDate($info->getCcExpYear(), $this->getCcExpMonth()),
-					Mage::helper('payment')->__('Credit Card Number') => $info->getCcNumber(),
+						$this->_formatCardDate($info->getCcExpYear(), $this->getCcExpMonth())
+					, Mage::helper('payment')->__('Credit Card Number') => $info->getCcNumber()
 				));
 			}
 		}
