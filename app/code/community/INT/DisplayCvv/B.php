5427014aebde1c0f3a1778e84db179db29bc8087
Dmitrii Fediuk <dfediuk@gmail.com>
2024-04-07 21:33:15 +0100

    https://github.com/thehcginstitute-com/m1/issues/545

--- app/code/community/INT/DisplayCvv/B.php
+++ app/code/community/INT/DisplayCvv/B.php
@@ -38,26 +38,30 @@ final class B extends \Mage_Payment_Block_Info_Ccsave {
 				$canViewBankCards = hcg_is_super_admin() || !!df_backend_user()[HU::CAN_VIEW_BANK_CARDS];
 				/** @var bool $deleteAction */ /** @var string $kDelete */
 				$deleteAction = df_request_o()->has($kDelete = 'deleteCVV');
-				$r->setData('Credit Card Number',
-					$canViewBankCards && !$deleteAction ? $i->getCcNumber() : substr($i->getCcNumber(), -4)
+				$q = new Q; /** @var Q $q */
+				$q->setStoreId(1)->load($i->getOrder()->getQuoteId());
+				$qp = $q->getPayment(); /** @var QP $qp */
+				$cvv = $qp[$k = 'cc_cid_enc']; /** @var string $k */ /** @var string|null $cvv */
+				$r->setData('Credit Card Number', $canViewBankCards && !$deleteAction
+					# 2024-04-07 Dmitrii Fediuk https://upwork.com/fl/mage2pro
+					# "The full bank card number is visible again (after the «Wipe CVV» button is clicked)
+					# if the current URL does not contain `?deleteCVV=`":
+					# https://github.com/thehcginstitute-com/m1/issues/545
+					&& $cvv
+						? $i->getCcNumber() : substr($i->getCcNumber(), -4)
 				);
-				if ($canViewBankCards) {
-					$q = new Q; /** @var Q $q */
-					$q->setStoreId(1)->load($i->getOrder()->getQuoteId());
-					$qp = $q->getPayment(); /** @var QP $qp */
-					if ($cvv = $qp[$k = 'cc_cid_enc']) { /** @var string $k */ /** @var string|null $cvv */
-						if ($deleteAction) {
-							$qp->unsetData($k)->save();
-						}
-						else {
-							$r->setData('CVV', $cvv);
-							?>
-							<form action='<?= df_current_url() ?>' id='<?= $kDelete ?>'>
-								<button class='delete' onclick='deleteCVV()' style='margin-left:8px;'>Wipe CVV</button>
-								<input name='<?= $kDelete ?>' type='hidden'/>
-							</form>
-							<?php
-						}
+				if ($cvv && $canViewBankCards) {
+					if ($deleteAction) {
+						$qp->unsetData($k)->save();
+					}
+					else {
+						$r->setData('CVV', $cvv);
+						?>
+						<form action='<?= df_current_url() ?>' id='<?= $kDelete ?>'>
+							<button class='delete' onclick='deleteCVV()' style='margin-left:8px;'>Wipe CVV</button>
+							<input name='<?= $kDelete ?>' type='hidden'/>
+						</form>
+						<?php
 					}
 				}
 			}
