12b624f59b29823a7da47dcc713930247faecd7c
Dmitrii Fediuk <dfediuk@gmail.com>
2024-04-12 22:27:32 +0100

    https://github.com/thehcginstitute-com/m1/issues/556

--- app/code/core/Mage/Core/functions.php
+++ app/code/core/Mage/Core/functions.php
@@ -98,7 +98,9 @@ function mageFindClassFile($class)
 	# 2024-04-13 Dmitrii Fediuk https://upwork.com/fl/mage2pro
 	# "Implement a diagnostics of errors in `mageFindClassFile()`": https://github.com/thehcginstitute-com/m1/issues/556
 	if (!$found) {
-		df_log("Unable to find the class «{$class}».", null, ['get_include_path' => explode(PS, get_include_path())]);
+		df_log("Unable to find the file «{$classFile} for the class «{$class}».", null, [
+			'get_include_path' => explode(PS, get_include_path())
+		]);
 	}
     return $found;
 }
