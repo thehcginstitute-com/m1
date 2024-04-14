a0fc7453c3ba9cf0280ff2c1ac904a37eaea3b47
Dmitrii Fediuk <dfediuk@gmail.com>
2024-04-14 04:15:39 +0100

    https://github.com/thehcginstitute-com/m1/issues/524

--- app/code/community/Ebizmarts/MailChimp/Model/System/Config/Backend/Store.php
+++ app/code/community/Ebizmarts/MailChimp/Model/System/Config/Backend/Store.php
@@ -46,10 +46,7 @@ class Ebizmarts_MailChimp_Model_System_Config_Backend_Store extends Mage_Core_Mo
     /**
      * @return Ebizmarts_MailChimp_Helper_Data
      */
-    protected function makeHelper()
-    {
-        return Mage::helper('mailchimp');
-    }
+    protected function makeHelper() {return hcg_mc_h();}
 
     /**
      * @return Ebizmarts_MailChimp_Helper_Date
--- app/code/local/HCG/MailChimp/lib/h.php
+++ app/code/local/HCG/MailChimp/lib/h.php
@@ -44,7 +44,7 @@ use Ebizmarts_MailChimp_Helper_Data as H;
  * @used-by Ebizmarts_MailChimp_Model_System_Config_Backend_Ecommerce::makeHelper()
  * @used-by Ebizmarts_MailChimp_Model_System_Config_Backend_List::getMailchimpHelper()
  * @used-by Ebizmarts_MailChimp_Model_System_Config_Backend_Mapfield::_afterLoad()
- * @used-by STUB()
+ * @used-by Ebizmarts_MailChimp_Model_System_Config_Backend_Store::makeHelper()
  * @used-by STUB()
  * @used-by STUB()
  * @used-by STUB()
