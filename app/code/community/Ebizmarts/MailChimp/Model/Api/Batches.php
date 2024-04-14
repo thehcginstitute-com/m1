40108e30a34dc986ca631db156899630d0150d4b
Dmitrii Fediuk <dfediuk@gmail.com>
2024-04-14 15:38:06 +0100

    https://github.com/thehcginstitute-com/m1/issues/524

--- app/code/community/Ebizmarts/MailChimp/Model/Api/Batches.php
+++ app/code/community/Ebizmarts/MailChimp/Model/Api/Batches.php
@@ -600,7 +600,9 @@ class Ebizmarts_MailChimp_Model_Api_Batches {
 
 	/**
 	 * Send Subscribers batch on particular store view, return batch response.
-	 *
+	 * 2024-04-14 Dmitrii Fediuk https://upwork.com/fl/mage2pro
+	 * "Refactor the `Ebizmarts_MailChimp` module": https://github.com/thehcginstitute-com/m1/issues/524
+	 * @used-by self::handleSubscriberBatches()
 	 * @param  $storeId
 	 * @param  $limit
 	 * @return array|null
