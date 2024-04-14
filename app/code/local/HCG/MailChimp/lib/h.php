513f5aad7b7d80bc15804c1fae5ba8a589881ead
Dmitrii Fediuk <dfediuk@gmail.com>
2024-04-14 04:10:21 +0100

    https://github.com/thehcginstitute-com/m1/issues/524

--- app/code/local/HCG/MailChimp/lib/h.php
+++ app/code/local/HCG/MailChimp/lib/h.php
@@ -30,7 +30,7 @@ use Ebizmarts_MailChimp_Helper_Data as H;
  * @used-by Ebizmarts_MailChimp_Helper_Webhook::__construct()
  * @used-by Ebizmarts_MailChimp_Model_Adminhtml_Includetaxes_Comment::getCommentText()
  * @used-by Ebizmarts_MailChimp_Model_Adminhtml_Resendecommercedata_Comment::setMcHelper()
- * @used-by STUB()
+ * @used-by Ebizmarts_MailChimp_Model_Adminhtml_Resendsubscribers_Comment::setMcHelper()
  * @used-by STUB()
  * @used-by STUB()
  * @used-by STUB()
