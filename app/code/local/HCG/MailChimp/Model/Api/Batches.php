db58983e116ea3f99b5754e101f693b2a6990f67
Dmitrii Fediuk <dfediuk@gmail.com>
2024-04-14 05:42:54 +0100

    https://github.com/thehcginstitute-com/m1/issues/524

--- app/code/local/HCG/MailChimp/Model/Api/Batches.php
+++ app/code/local/HCG/MailChimp/Model/Api/Batches.php
@@ -31,12 +31,13 @@ final class Batches {
 				0,
 				true
 			);
-			$mE = new mE; /** @var mE $mE */
-			# 2024-03-17 Dmitrii Fediuk https://upwork.com/fl/mage2pro
-			# «Undefined index: type in app/code/community/Ebizmarts/MailChimp/Model/Api/Batches.php
-			# on line 836»: https://github.com/thehcginstitute-com/m1/issues/510
-			$mE->setType(dfa($response, 'type'));
-			$mE->setTitle($response['title']);
+			$mE = new mE([
+				'title' => $response['title']
+				# 2024-03-17 Dmitrii Fediuk https://upwork.com/fl/mage2pro
+				# «Undefined index: type in app/code/community/Ebizmarts/MailChimp/Model/Api/Batches.php
+				# on line 836»: https://github.com/thehcginstitute-com/m1/issues/510
+				,'type' => dfa($response, 'type')
+			]); /** @var mE $mE */
 			$mE->setStatus($i['status_code']);
 			$mE->setErrors($errorDetails);
 			$mE->setRegtype($type);
