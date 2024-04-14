214d7220918bf5c6742ac06c88b94b11812e69c6
Dmitrii Fediuk <dfediuk@gmail.com>
2024-04-14 04:13:05 +0100

    https://github.com/thehcginstitute-com/m1/issues/524

--- app/code/local/HCG/MailChimp/lib/h.php
+++ app/code/local/HCG/MailChimp/lib/h.php
@@ -37,7 +37,7 @@ use Ebizmarts_MailChimp_Helper_Data as H;
  * @used-by Ebizmarts_MailChimp_Model_Api_Subscribers_MailchimpTags::setMailChimpHelper()
  * @used-by Ebizmarts_MailChimp_Model_Api_Batches::__construct()
  * @used-by Ebizmarts_MailChimp_Model_Api_ItemSynchronizer::__construct()
- * @used-by STUB()
+ * @used-by Ebizmarts_MailChimp_Model_Api_Stores::makeHelper()
  * @used-by STUB()
  * @used-by STUB()
  * @used-by STUB()
