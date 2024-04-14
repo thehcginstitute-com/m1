a608389e8b2702042e42cd9b3ce486aeff644a38
Dmitrii Fediuk <dfediuk@gmail.com>
2024-04-14 05:46:13 +0100

    https://github.com/thehcginstitute-com/m1/issues/524

--- app/code/local/HCG/MailChimp/Model/Api/Batches.php
+++ app/code/local/HCG/MailChimp/Model/Api/Batches.php
@@ -32,18 +32,18 @@ final class Batches {
 				true
 			);
 			$mE = new mE([
-				'errors' => $errorDetails
+				'batch_id' => $batchId
+				,'errors' => $errorDetails
 				,'original_id' => $id
 				,'regtype' => $type
 				,'status' => $i['status_code']
+				,'store_id' => $store[1]
 				,'title' => $response['title']
 				# 2024-03-17 Dmitrii Fediuk https://upwork.com/fl/mage2pro
 				# «Undefined index: type in app/code/community/Ebizmarts/MailChimp/Model/Api/Batches.php
 				# on line 836»: https://github.com/thehcginstitute-com/m1/issues/510
 				,'type' => dfa($response, 'type')
 			]); /** @var mE $mE */
-			$mE->setBatchId($batchId);
-			$mE->setStoreId($store[1]);
 			if ($type != \Ebizmarts_MailChimp_Model_Config::IS_SUBSCRIBER) {
 				$mE->setMailchimpStoreId($mailchimpStoreId);
 			}
