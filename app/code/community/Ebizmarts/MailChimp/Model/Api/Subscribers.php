dd90a82346fb91a84b67b2d9b0ba5e06c948789e
Dmitrii Fediuk <dfediuk@gmail.com>
2024-04-14 15:57:40 +0100

    https://github.com/thehcginstitute-com/m1/issues/524

--- app/code/community/Ebizmarts/MailChimp/Model/Api/Subscribers.php
+++ app/code/community/Ebizmarts/MailChimp/Model/Api/Subscribers.php
@@ -50,6 +50,9 @@ class Ebizmarts_MailChimp_Model_Api_Subscribers
 	}
 
 	/**
+	 * 2024-04-14 Dmitrii Fediuk https://upwork.com/fl/mage2pro
+	 * "Refactor the `Ebizmarts_MailChimp` module": https://github.com/thehcginstitute-com/m1/issues/524
+	 * @used-by Ebizmarts_MailChimp_Model_Api_Batches::sendStoreSubscriberBatch()
 	 * @param $listId
 	 * @param $storeId
 	 * @param $limit
