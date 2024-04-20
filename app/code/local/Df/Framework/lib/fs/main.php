3cf9da8a95c1ff793e2334dff24dd491a378dc9c
Dmitrii Fediuk <dfediuk@gmail.com>
2024-04-20 21:39:27 +0100

    https://github.com/thehcginstitute-com/m1/issues/570

--- app/code/local/Df/Framework/lib/fs/main.php
+++ app/code/local/Df/Framework/lib/fs/main.php
@@ -1,5 +1,11 @@
 <?php
 use Df\Framework\Fs;
+use Varien_Io_File as F;
+
+/**
+ * 2024-04-20 "Port `df_fs_delete()` from `mage2pro/core`": https://github.com/thehcginstitute-com/m1/issues/570
+ */
+function df_fs_delete(string $p):void {F::rmdirRecursive(df_param_sne($p, 0));}
 
 /**
  * 2015-11-29
