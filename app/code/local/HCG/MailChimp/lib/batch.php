75b9c55fb096ffcfc029992def34c6e51dfd4393
Dmitrii Fediuk <dfediuk@gmail.com>
2024-04-20 21:04:36 +0100

    https://github.com/thehcginstitute-com/m1/issues/569

--- app/code/local/HCG/MailChimp/lib/batch.php
+++ app/code/local/HCG/MailChimp/lib/batch.php
@@ -1,6 +1,11 @@
 <?php
 use Ebizmarts_MailChimp_Model_Api_Batches as B;
 /**
+ * 2024-04-20 "Refactor `DS . 'mailchimp'`": https://github.com/thehcginstitute-com/m1/issues/569
+ */
+function hcg_mc_batches_dir():string {return Mage::getBaseDir('var') . DS . 'mailchimp';}
+
+/**
  * 2024-04-14
  * @used-by Ebizmarts_MailChimp_Helper_Migration::_migrateOrdersFrom116()
  * @used-by Ebizmarts_MailChimp_Model_Cron::syncEcommerceBatchData()
