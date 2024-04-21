6e35878a29f2f71bc60682febfef86046a13f82b
Dmitrii Fediuk <dfediuk@gmail.com>
2024-04-21 03:15:09 +0100

    https://github.com/thehcginstitute-com/m1/issues/573

--- app/code/community/Ebizmarts/MailChimp/sql/mailchimp_setup/mysql4-upgrade-1.1.6.3-1.1.6.4.php
+++
@@ -1,4 +0,0 @@
-<?php
-$installer = $this;
-hcg_mc_cfg_save(Ebizmarts_MailChimp_Model_Config::GENERAL_MIGRATE_FROM_116, 1);
-$installer->endSetup();
--- app/code/community/Ebizmarts/MailChimp/sql/mailchimp_setup/mysql4-upgrade-1.1.9.1-1.1.9.2.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$installer = $this;
-
-$installer->deleteConfigData(Ebizmarts_MailChimp_Model_Config::ECOMMERCE_MC_JS_URL);
-
-$installer->endSetup();
