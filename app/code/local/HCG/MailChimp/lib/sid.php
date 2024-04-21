08d2d6d22d79239ad1195b73bbd61448a11db77c
Dmitrii Fediuk <dfediuk@gmail.com>
2024-04-21 23:39:00 +0100

    https://github.com/thehcginstitute-com/m1/issues/574

--- app/code/local/HCG/MailChimp/lib/sid.php
+++ app/code/local/HCG/MailChimp/lib/sid.php
@@ -18,7 +18,7 @@ use Ebizmarts_MailChimp_Model_Config as Cfg;
  * @used-by Ebizmarts_MailChimp_Helper_Data::saveLastItemsSent()
  * @used-by Ebizmarts_MailChimp_Helper_Data::getLastCustomerSent()
  * @used-by Ebizmarts_MailChimp_Helper_Data::getLastProductSent()
- * @used-by STUB()
+ * @used-by Ebizmarts_MailChimp_Helper_Data::getLastOrderSent()
  * @used-by STUB()
  * @used-by STUB()
  * @used-by STUB()
