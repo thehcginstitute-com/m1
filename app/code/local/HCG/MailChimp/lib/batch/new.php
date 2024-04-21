525432c64391a6fbf57cdfe37bca0fdfe0936399
Dmitrii Fediuk <dfediuk@gmail.com>
2024-04-21 03:28:19 +0100

    https://github.com/thehcginstitute-com/m1/issues/573

--- app/code/local/HCG/MailChimp/lib/batch/new.php
+++ app/code/local/HCG/MailChimp/lib/batch/new.php
@@ -3,7 +3,6 @@ use Ebizmarts_MailChimp_Model_Api_Batches as B;
 /**
  * 2024-04-14
  * @used-by Ebizmarts_MailChimp_Adminhtml_MailchimperrorsController::downloadresponseAction()
- * @used-by Ebizmarts_MailChimp_Helper_Migration::_migrateOrdersFrom116()
  * @used-by Ebizmarts_MailChimp_Model_Cron::syncEcommerceBatchData()
  * @used-by Ebizmarts_MailChimp_Model_Cron::syncSubscriberBatchData()
  */
