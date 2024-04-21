d054e8485638a558a57bb4f686b1305a05e5e164
Dmitrii Fediuk <dfediuk@gmail.com>
2024-04-21 08:19:09 +0100

    https://github.com/thehcginstitute-com/m1/issues/572

--- app/code/community/Ebizmarts/MailChimp/Model/Api/Batches.php
+++ app/code/community/Ebizmarts/MailChimp/Model/Api/Batches.php
@@ -383,6 +383,8 @@ final class Ebizmarts_MailChimp_Model_Api_Batches {
 	}
 
 	/**
+	 * 2023-04-21 "Refactor `Ebizmarts_MailChimp_Model_Api_Batches`": https://github.com/thehcginstitute-com/m1/issues/572
+	 * @used-by self::getBatchResponse()
 	 * @param $batchId
 	 */
 	private function deleteBatchItems($batchId) {
