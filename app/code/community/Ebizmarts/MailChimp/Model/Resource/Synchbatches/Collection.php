0e9638845848c2447256d276892ea6bed382e981
Dmitrii Fediuk <dfediuk@gmail.com>
2024-04-22 06:05:41 +0100

    https://github.com/thehcginstitute-com/m1/issues/575

--- app/code/community/Ebizmarts/MailChimp/Model/Resource/Synchbatches/Collection.php
+++ app/code/community/Ebizmarts/MailChimp/Model/Resource/Synchbatches/Collection.php
@@ -13,14 +13,14 @@
 class Ebizmarts_MailChimp_Model_Resource_Synchbatches_Collection extends Mage_Core_Model_Resource_Db_Collection_Abstract
 {
 
-    /**
-     * Set resource type
-     *
-     * @return void
-     */
-    function _construct()
-    {
-        parent::_construct();
-        $this->_init('mailchimp/synchbatches');
-    }
+	/**
+	 * Set resource type
+	 *
+	 * @return void
+	 */
+	function _construct()
+	{
+		parent::_construct();
+		$this->_init('mailchimp/synchbatches');
+	}
 }
