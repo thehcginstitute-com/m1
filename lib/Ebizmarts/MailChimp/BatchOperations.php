8fd5a3b84def24f341c4bdfc5d8acfa714fd5f3e
Dmitrii Fediuk <dfediuk@gmail.com>
2024-04-22 05:47:17 +0100

    https://github.com/thehcginstitute-com/m1/issues/524

--- lib/Ebizmarts/MailChimp/BatchOperations.php
+++ lib/Ebizmarts/MailChimp/BatchOperations.php
@@ -12,73 +12,73 @@
  */
 class MailChimp_BatchOperations extends MailChimp_Abstract
 {
-    /**
-     * @param $operations
-     * @return mixed
-     * @throws MailChimp_Error
-     * @throws MailChimp_HttpError
-     */
-    public function add($operations)
-    {
-        return $this->_master->call('batches', $operations, Ebizmarts_MailChimp::POST, false);
-    }
+	/**
+	 * @param $operations
+	 * @return mixed
+	 * @throws MailChimp_Error
+	 * @throws MailChimp_HttpError
+	 */
+	public function add($operations)
+	{
+		return $this->_master->call('batches', $operations, Ebizmarts_MailChimp::POST, false);
+	}
 
-    /**
-     * @param $fields           A comma-separated list of fields to return. Reference parameters of sub-objects
-     *                          with dot notation.
-     * @param $excludeFields    A comma-separated list of fields to exclude. Reference parameters of sub-objects
-     *                          with dot notation.
-     * @return mixed
-     * @throws MailChimp_Error
-     * @throws MailChimp_HttpError
-     */
-    public function status(string $id, $fields = null, $excludeFields = null)
-    {
-        $_params = array();
+	/**
+	 * @param $fields           A comma-separated list of fields to return. Reference parameters of sub-objects
+	 *                          with dot notation.
+	 * @param $excludeFields    A comma-separated list of fields to exclude. Reference parameters of sub-objects
+	 *                          with dot notation.
+	 * @return mixed
+	 * @throws MailChimp_Error
+	 * @throws MailChimp_HttpError
+	 */
+	public function status(string $id, $fields = null, $excludeFields = null)
+	{
+		$_params = array();
 
-        if ($fields) {
-            $_params['fields'] = $fields;
-        }
+		if ($fields) {
+			$_params['fields'] = $fields;
+		}
 
-        if ($excludeFields) {
-            $_params['exclude_fields'] = $excludeFields;
-        }
+		if ($excludeFields) {
+			$_params['exclude_fields'] = $excludeFields;
+		}
 
-        return $this->_master->call('batches/' . $id, $_params, Ebizmarts_MailChimp::GET);
-    }
+		return $this->_master->call('batches/' . $id, $_params, Ebizmarts_MailChimp::GET);
+	}
 
-    /**
-     * @param $fields           A comma-separated list of fields to return. Reference parameters of sub-objects
-     *                              with dot notation.
-     * @param $excludeFields    A comma-separated list of fields to exclude. Reference parameters of sub-objects with
-     *                              dot notation.
-     * @param $count            The number of records to return.
-     * @param $offset           The number of records from a collection to skip. Iterating over large collections with
-     *                          this parameter can be slow.
-     * @return mixed
-     * @throws MailChimp_Error
-     * @throws MailChimp_HttpError
-     */
-    public function getAll($fields = null, $excludeFields = null, $count = null, $offset = null)
-    {
-        $_params = array();
+	/**
+	 * @param $fields           A comma-separated list of fields to return. Reference parameters of sub-objects
+	 *                              with dot notation.
+	 * @param $excludeFields    A comma-separated list of fields to exclude. Reference parameters of sub-objects with
+	 *                              dot notation.
+	 * @param $count            The number of records to return.
+	 * @param $offset           The number of records from a collection to skip. Iterating over large collections with
+	 *                          this parameter can be slow.
+	 * @return mixed
+	 * @throws MailChimp_Error
+	 * @throws MailChimp_HttpError
+	 */
+	public function getAll($fields = null, $excludeFields = null, $count = null, $offset = null)
+	{
+		$_params = array();
 
-        if ($fields) {
-            $_params['fields'] = $fields;
-        }
+		if ($fields) {
+			$_params['fields'] = $fields;
+		}
 
-        if ($excludeFields) {
-            $_params['exclude_fields'] = $excludeFields;
-        }
+		if ($excludeFields) {
+			$_params['exclude_fields'] = $excludeFields;
+		}
 
-        if ($count) {
-            $_params['count'] = $count;
-        }
+		if ($count) {
+			$_params['count'] = $count;
+		}
 
-        if ($offset) {
-            $_params['offset'] = $offset;
-        }
+		if ($offset) {
+			$_params['offset'] = $offset;
+		}
 
-        return $this->_master->call('batches', $_params, Ebizmarts_MailChimp::GET);
-    }
+		return $this->_master->call('batches', $_params, Ebizmarts_MailChimp::GET);
+	}
 }
