f4e823eb15f53ccd4e346ed23d636f14931cbe77
Dmitrii Fediuk <dfediuk@gmail.com>
2024-04-22 06:23:35 +0100

    https://github.com/thehcginstitute-com/m1/issues/575

--- app/code/community/Ebizmarts/MailChimp/Model/Resource/Synchbatches/Collection.php
+++ app/code/community/Ebizmarts/MailChimp/Model/Resource/Synchbatches/Collection.php
@@ -1,7 +1,8 @@
 <?php
 # 2024-04-22 Dmitrii Fediuk https://upwork.com/fl/mage2pro
 # "Refactor `Ebizmarts_MailChimp_Model_Synchbatches`": https://github.com/thehcginstitute-com/m1/issues/575
-class Ebizmarts_MailChimp_Model_Resource_Synchbatches_Collection extends Mage_Core_Model_Resource_Db_Collection_Abstract {
+final class Ebizmarts_MailChimp_Model_Resource_Synchbatches_Collection
+	extends Mage_Core_Model_Resource_Db_Collection_Abstract {
 	/**
 	 * 2024-04-22 Dmitrii Fediuk https://upwork.com/fl/mage2pro
 	 * "Refactor `Ebizmarts_MailChimp_Model_Synchbatches`": https://github.com/thehcginstitute-com/m1/issues/575
@@ -9,7 +10,7 @@ class Ebizmarts_MailChimp_Model_Resource_Synchbatches_Collection extends Mage_Co
 	 * @see \Mage_Core_Model_Resource_Db_Collection_Abstract::_construct()
 	 * @used-by Varien_Object::__construct()
 	 */
-	function _construct():void {
+	protected function _construct():void {
 		parent::_construct();
 		$this->_init('mailchimp/synchbatches');
 	}
