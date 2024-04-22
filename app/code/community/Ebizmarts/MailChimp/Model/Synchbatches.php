c838a22907833637e5a96e568b9a9cac3b43768d
Dmitrii Fediuk <dfediuk@gmail.com>
2024-04-22 05:59:31 +0100

    https://github.com/thehcginstitute-com/m1/issues/575

--- app/code/community/Ebizmarts/MailChimp/Model/Synchbatches.php
+++ app/code/community/Ebizmarts/MailChimp/Model/Synchbatches.php
@@ -3,13 +3,11 @@
 # "Refactor `Ebizmarts_MailChimp_Model_Synchbatches`": https://github.com/thehcginstitute-com/m1/issues/575
 class Ebizmarts_MailChimp_Model_Synchbatches extends Mage_Core_Model_Abstract {
     /**
-     * Initialize model
-     *
-     * @return void
+     * 2024-04-22 Dmitrii Fediuk https://upwork.com/fl/mage2pro
+	 * "Refactor `Ebizmarts_MailChimp_Model_Synchbatches`": https://github.com/thehcginstitute-com/m1/issues/575
+	 * @override
+	 * @see Varien_Object::_construct()
+	 * @used-by Varien_Object::__construct()
      */
-    function _construct()
-    {
-        parent::_construct();
-        $this->_init('mailchimp/synchbatches');
-    }
+    function _construct():void {$this->_init('mailchimp/synchbatches');}
 }
