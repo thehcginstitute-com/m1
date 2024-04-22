610565b1b74036d0310794b9cb0626126fcd08ba
Dmitrii Fediuk <dfediuk@gmail.com>
2024-04-22 06:01:52 +0100

    https://github.com/thehcginstitute-com/m1/issues/575

--- app/code/community/Ebizmarts/MailChimp/Model/Resource/Synchbatches.php
+++ app/code/community/Ebizmarts/MailChimp/Model/Resource/Synchbatches.php
@@ -1,16 +1,6 @@
 <?php
-
-/**
- * mc-magento Magento Component
- *
- * @category  Ebizmarts
- * @package   mc-magento
- * @author    Ebizmarts Team <info@ebizmarts.com>
- * @copyright Ebizmarts (http://ebizmarts.com)
- * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
- * @date:     2019-10-02 15:54
- * @file:     SychBatches.php
- */
+# 2024-04-22 Dmitrii Fediuk https://upwork.com/fl/mage2pro
+# "Refactor `Ebizmarts_MailChimp_Model_Synchbatches`": https://github.com/thehcginstitute-com/m1/issues/575
 class Ebizmarts_MailChimp_Model_Resource_SynchBatches extends Mage_Core_Model_Resource_Db_Abstract
 {
 
