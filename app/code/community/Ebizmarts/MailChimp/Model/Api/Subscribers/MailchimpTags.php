0d5c00b3a6bc66fe4ff4f02334eafb30b3303d8a
Dmitrii Fediuk <dfediuk@gmail.com>
2024-05-02 20:22:02 +0100

    https://github.com/thehcginstitute-com/m1/issues/589

--- app/code/community/Ebizmarts/MailChimp/Model/Api/Subscribers/MailchimpTags.php
+++ app/code/community/Ebizmarts/MailChimp/Model/Api/Subscribers/MailchimpTags.php
@@ -540,9 +540,9 @@ final class Ebizmarts_MailChimp_Model_Api_Subscribers_MailchimpTags {
 	 * @used-by self::STUB()
 	 * @used-by self::STUB()
 	 * @used-by self::STUB()
-	 * @param $value
+	 * @param $v
 	 */
-	private function addMailChimpTag(string $key, $value):void {$this->_mailChimpTags[$key] = $value;}
+	private function addMailChimpTag(string $k, $v):void {$this->_mailChimpTags[$k] = $v;}
 
 	/**
 	 * @param $customAtt
