0354e335749d801dda0acabc53c188570595ced8
Dmitrii Fediuk <dfediuk@gmail.com>
2024-05-04 20:27:12 +0100

    https://github.com/thehcginstitute-com/m1/issues/524

--- app/code/community/Ebizmarts/MailChimp/Model/Api/Stores.php
+++ app/code/community/Ebizmarts/MailChimp/Model/Api/Stores.php
@@ -281,14 +281,6 @@ class Ebizmarts_MailChimp_Model_Api_Stores
     protected function makeHelper() {return hcg_mc_h();}
 
     /**
-     * @return Ebizmarts_MailChimp_Helper_Date
-     */
-    protected function makeDateHelper()
-    {
-        return Mage::helper('mailchimp/date');
-    }
-
-    /**
      * @return Mage_Adminhtml_Model_Session
      */
     protected function getAdminSession()
