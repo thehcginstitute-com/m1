e8332ff8675d46dab91833395e33a8722f9a136a
Dmitrii Fediuk <dfediuk@gmail.com>
2024-05-04 22:47:16 +0100

    https://github.com/thehcginstitute-com/m1/issues/589

--- app/code/community/Ebizmarts/MailChimp/Model/Api/Subscribers/MailchimpTags.php
+++ app/code/community/Ebizmarts/MailChimp/Model/Api/Subscribers/MailchimpTags.php
@@ -812,6 +812,8 @@ final class Ebizmarts_MailChimp_Model_Api_Subscribers_MailchimpTags {
 	/**
 	 * 2024-05-05 Dmitrii Fediuk https://upwork.com/fl/mage2pro
 	 * "Refactor `Ebizmarts_MailChimp_Model_Api_Subscribers_MailchimpTags`": https://github.com/cabinetsbay/site/issues/589
+	 * @used-by self::p()
+	 * @used-by self::sub()
 	 * @var Sub
 	 */
 	private $_sub;
