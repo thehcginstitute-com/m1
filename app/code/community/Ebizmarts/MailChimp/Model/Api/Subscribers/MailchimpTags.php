133014b1a5558c1ec9df55f516c4598bdc6617f2
Dmitrii Fediuk <dfediuk@gmail.com>
2024-05-05 19:10:25 +0100

    https://github.com/thehcginstitute-com/m1/issues/589

--- app/code/community/Ebizmarts/MailChimp/Model/Api/Subscribers/MailchimpTags.php
+++ app/code/community/Ebizmarts/MailChimp/Model/Api/Subscribers/MailchimpTags.php
@@ -213,6 +213,7 @@ final class Ebizmarts_MailChimp_Model_Api_Subscribers_MailchimpTags {
 	 * 2024-05-05 Dmitrii Fediuk https://upwork.com/fl/mage2pro
 	 * "Refactor `Ebizmarts_MailChimp_Model_Api_Subscribers_MailchimpTags`": https://github.com/cabinetsbay/site/issues/589
 	 * @used-by self::buildCustomizedAttributes()
+	 * @return mixed
 	 */
 	private function customizedAttributes(string $a, string $key) {
 		$eventValue = null;
