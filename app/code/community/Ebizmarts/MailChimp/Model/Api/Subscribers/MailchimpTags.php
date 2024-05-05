107f3fb57cbeb5347f1b75cf629da749442c40bc
Dmitrii Fediuk <dfediuk@gmail.com>
2024-05-05 18:57:32 +0100

    https://github.com/thehcginstitute-com/m1/issues/589

--- app/code/community/Ebizmarts/MailChimp/Model/Api/Subscribers/MailchimpTags.php
+++ app/code/community/Ebizmarts/MailChimp/Model/Api/Subscribers/MailchimpTags.php
@@ -266,10 +266,9 @@ final class Ebizmarts_MailChimp_Model_Api_Subscribers_MailchimpTags {
 	});}
 
 	/**
-	 * @param $attributeCode
-	 * @param $key
-	 * @param $attribute
-	 * @return |null
+	 * 2024-05-05 Dmitrii Fediuk https://upwork.com/fl/mage2pro
+	 * "Refactor `Ebizmarts_MailChimp_Model_Api_Subscribers_MailchimpTags`": https://github.com/cabinetsbay/site/issues/589
+	 * @used-by self::STUB()
 	 */
 	private function customerAttributes($attributeCode, $key, $attribute) {
 		$customer = $this->customer();
