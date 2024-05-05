810b5c560638bf69ff77e7e53b0583e302e9f722
Dmitrii Fediuk <dfediuk@gmail.com>
2024-05-05 18:50:45 +0100

    https://github.com/thehcginstitute-com/m1/issues/589

--- app/code/community/Ebizmarts/MailChimp/Model/Api/Subscribers/MailchimpTags.php
+++ app/code/community/Ebizmarts/MailChimp/Model/Api/Subscribers/MailchimpTags.php
@@ -205,12 +205,11 @@ final class Ebizmarts_MailChimp_Model_Api_Subscribers_MailchimpTags {
 	private function set(string $k, $v):void {$this->_mailChimpTags[$k] = $v;}
 
 	/**
-	 * @param $customAtt
-	 * @param $key
-	 * @param $customer
+	 * 2024-05-05 Dmitrii Fediuk https://upwork.com/fl/mage2pro
+	 * "Refactor `Ebizmarts_MailChimp_Model_Api_Subscribers_MailchimpTags`": https://github.com/cabinetsbay/site/issues/589
+	 * @used-by self::customizedAttributes()
 	 */
-	private function addStateFromCustomizedAttribute($customAtt, $key, $customer):void
-	{
+	private function addStateFromCustomizedAttribute($customAtt, $key, $customer):void {
 		$address = $this->getAddressForCustomizedAttributes($customAtt, $customer);
 		if ($address) {
 			$state = $address->getRegion();
