5387ff2004ea8d86644ddca7b0ab01c64cc3ad3a
Dmitrii Fediuk <dfediuk@gmail.com>
2024-05-09 14:16:08 +0100

    https://github.com/thehcginstitute-com/m1/issues/524

--- app/code/local/HCG/MailChimp/lib/cfg/get.php
+++ app/code/local/HCG/MailChimp/lib/cfg/get.php
@@ -5,6 +5,5 @@ use Ebizmarts_MailChimp_Model_Config as C;
  * "Refactor the `Ebizmarts_MailChimp` module": https://github.com/thehcginstitute-com/m1/issues/524
  * @used-by Ebizmarts_MailChimp_Helper_Data::createMergeFields()
  * @used-by Ebizmarts_MailChimp_Model_Api_Subscribers_MailchimpTags::_p()
- * @used-by STUB()
  */
 function hcg_mc_cfg_fields() {return hcg_mc_h()->unserialize(Mage::getStoreConfig('mailchimp/general/map_fields'));}
