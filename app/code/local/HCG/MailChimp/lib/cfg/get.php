8d6328870db687bbeb51ff3add150337f637d5d7
Dmitrii Fediuk <dfediuk@gmail.com>
2024-05-10 10:17:56 +0100

    https://github.com/thehcginstitute-com/m1/issues/593

--- app/code/local/HCG/MailChimp/lib/cfg/get.php
+++ app/code/local/HCG/MailChimp/lib/cfg/get.php
@@ -1,7 +1,7 @@
 <?php
 use Ebizmarts_MailChimp_Model_Config as C;
 /**
- * 2024-05-08 Dmitrii Fediuk https://upwork.com/fl/mage2pro
+ * 2024-05-08
  * "Refactor the `Ebizmarts_MailChimp` module": https://github.com/thehcginstitute-com/m1/issues/524
  * @used-by Ebizmarts_MailChimp_Helper_Data::createMergeFields()
  * @used-by Ebizmarts_MailChimp_Model_Api_Subscribers_MailchimpTags::_p()
