bd4b894cc1f49a21888b22557f0f8009d41e2235
Dmitrii Fediuk <dfediuk@gmail.com>
2024-05-10 10:02:10 +0100

    https://github.com/thehcginstitute-com/m1/issues/592

--- app/code/local/Df/Config/lib/main.php
+++ app/code/local/Df/Config/lib/main.php
@@ -1,1 +1,6 @@
 <?php
+/**
+ * 2024-05-10 "Port `df_cfg()` from `mage2pro/core`": https://github.com/thehcginstitute-com/m1/issues/592
+ * @return mixed
+ */
+function df_cfg(string $k) {return Mage::getStoreConfig($k);}
