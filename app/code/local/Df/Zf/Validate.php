55036a1d5347b11b0f9a6688f0010b040a580015
Dmitrii Fediuk <dfediuk@gmail.com>
2024-05-14 05:24:45 +0100

    https://github.com/thehcginstitute-com/m1/issues/607

---
+++ app/code/local/Df/Zf/Validate.php
@@ -0,0 +1,47 @@
+<?php
+namespace Df\Zf;
+/**
+ * 2024-05-14 "Port `Df\Zf\Validate` from `mage2pro/core`": https://github.com/thehcginstitute-com/m1/issues/607
+ * @see \Df\Zf\Validate\StringT\IntT
+ * @see \Df\Zf\Validate\StringT\Iso2
+ * @see \Df\Zf\Validate\StringT\Parser
+ * @used-by \Df\Zf\Validate\StringT\IntT::s()
+ * @used-by \Df\Zf\Validate\StringT\Iso2::s()
+ * @used-by \Df\Zf\Validate\StringT\FloatT::s()
+ */
+abstract class Validate implements \Zend_Validate_Interface {
+	/**
+	 * @used-by self::message()
+	 * @see \Df\Zf\Validate\StringT\FloatT::expected()
+	 * @see \Df\Zf\Validate\StringT\IntT::expected()
+	 * @see \Df\Zf\Validate\StringT\Iso2::expected()
+	 */
+	abstract protected function expected():string;
+
+	/**
+	 * @override
+	 * @see \Zend_Validate_Interface::getMessages()
+	 * @return array(string => string)
+	 */
+	final function getMessages():array {return [__CLASS__ => $this->message()];}
+
+	/**
+	 * @used-by df_float()
+	 * @used-by df_int()
+	 * @used-by \Df\Zf\Validate::getMessages()
+	 */
+	final function message():string {$v = $this->v(); return is_null($v)
+		? "Got `NULL` instead of {$this->expected()}."
+		: sprintf("Unable to recognize the value «%s» of type «%s» as {$this->expected()}.", df_string_debug($v), gettype($v))
+	;}
+
+	/**
+	 * @used-by \Df\Zf\Validate\StringT\FloatT::isValid()
+	 * @used-by \Df\Zf\Validate\StringT\IntT::isValid()
+	 * @used-by \Df\Zf\Validate\StringT\Iso2::isValid()
+	 * @used-by \Df\Zf\Validate\StringT\Parser::isValid()
+	 * @param mixed $v [optional]
+	 * @return self|mixed
+	 */
+	final protected function v($v = DF_N) {return df_prop($this, $v);}
+}
