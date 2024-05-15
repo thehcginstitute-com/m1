26b43c8c79e188e6db5095e3d8fa72870119b39a
Dmitrii Fediuk <dfediuk@gmail.com>
2024-05-15 03:07:05 +0100

    https://github.com/thehcginstitute-com/m1/issues/609

--- app/code/local/Df/Customer/lib/attributes.php
+++ app/code/local/Df/Customer/lib/attributes.php
@@ -1,4 +1,8 @@
 <?php
-function df_customer_attributes() {
-
-}
+use Mage_Eav_Model_Entity_Attribute_Abstract as A;
+/**
+ * 2016-06-04
+ * 2024-05-15 "Port `df_customer_att()` from `mage2pro/core`": https://github.com/thehcginstitute-com/m1/issues/609
+ * @used-by df_customer_att_is_required()
+ */
+function df_customer_att(string $c):A {return df_eav_config()->getAttribute(df_eav_customer(), $c);}
