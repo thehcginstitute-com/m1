206071fdddaa053bafd5afd6569a79e943585318
Dmitrii Fediuk <dfediuk@gmail.com>
2024-05-16 01:09:20 +0100

    https://github.com/thehcginstitute-com/m1/issues/615

---
+++ app/code/local/Df/Customer/lib/address.php
@@ -0,0 +1,5 @@
+<?php
+/**
+ * 2024-05-16
+ */
+function df_assert_address_type(string $t):string {}
---
+++ app/code/local/Df/Sales/lib/order/address.php
@@ -0,0 +1,11 @@
+<?php
+use Mage_Sales_Model_Order as O;
+use Mage_Sales_Model_Order_Address as A;
+/**
+ * 2025-05-16
+ * @see Mage_Sales_Model_Order::getBillingAddress()
+ * @see \Mage_Sales_Model_Order::getShippingAddress()
+ */
+function df_oa(O $o, string $t):?A {
+
+}
