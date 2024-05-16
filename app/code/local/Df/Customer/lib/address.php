ba2fe592d1ef66539b5aeff8fa0e0ad0cde6eb17
Dmitrii Fediuk <dfediuk@gmail.com>
2024-05-16 01:10:49 +0100

    https://github.com/thehcginstitute-com/m1/issues/615

--- app/code/local/Df/Customer/lib/address.php
+++
@@ -1,5 +0,0 @@
-<?php
-/**
- * 2024-05-16
- */
-function df_assert_address_type(string $t):string {}
---
+++ app/code/local/Df/Customer/lib/address/assert.php
@@ -0,0 +1,15 @@
+<?php
+use Df\Core\Exception as DFE;
+use Mage_Customer_Model_Address_Abstract as AA;
+use Throwable as T;
+
+/**
+ * 2024-05-16
+ * 1) "Implement `df_assert_address_type()`": https://github.com/mage2pro/core/issues/372
+ * 2) "Port `df_assert_address_type()` from `mage2pro/core`": https://github.com/thehcginstitute-com/m1/issues/615
+ * @param string|T $m [optional]
+ * @throws DFE
+ */
+function df_assert_address_type(string $t, $m = null):string {return df_assert_in(
+	$t, [AA::TYPE_BILLING, AA::TYPE_SHIPPING], $m ?: "Invalid address type: «{$t}»."
+);}
