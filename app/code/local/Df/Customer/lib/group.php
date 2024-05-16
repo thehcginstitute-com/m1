9f1156e968b843c976fb32b27ba0acd859fabcd1
Dmitrii Fediuk <dfediuk@gmail.com>
2024-05-16 02:17:08 +0100

    https://github.com/thehcginstitute-com/m1/issues/617

--- app/code/local/Df/Customer/lib/group.php
+++ app/code/local/Df/Customer/lib/group.php
@@ -30,7 +30,9 @@ function df_customer_group_id():int {return (int)df_customer_session()->getCusto
 
 /**
  * 2020-02-06
- * 2024-05-16 "Port `df_customer_group_name()` from `mage2pro/core`": https://github.com/thehcginstitute-com/m1/issues/617
+ * 2024-05-16
+ * 1) "Port `df_customer_group_name()` from `mage2pro/core`": https://github.com/thehcginstitute-com/m1/issues/617
+ * 2) @see Mage_Adminhtml_Block_Customer_Edit_Tab_View::getGroupName())
  * @param C|G|int $v
  * @param F|bool|mixed string [optional]
  */
