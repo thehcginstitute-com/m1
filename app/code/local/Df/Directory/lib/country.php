ce8be86f93c2aefe7265a68b32f8cb6389aa8256
Dmitrii Fediuk <dfediuk@gmail.com>
2024-05-16 03:49:57 +0100

    https://github.com/thehcginstitute-com/m1/issues/589

--- app/code/local/Df/Directory/lib/country.php
+++ app/code/local/Df/Directory/lib/country.php
@@ -6,6 +6,7 @@ use Mage_Directory_Model_Country as C;
  * (or the default system locale) given: https://ru.wikipedia.org/wiki/ISO_3166-1
  * 2024-03-31 "Port `df_country_ctn()` from `mage2pro/core`": https://github.com/thehcginstitute-com/m1/issues/536
  * @used-by Ebizmarts_MailChimp_Model_Api_Subscribers_MailchimpTags::attOther() (https://github.com/cabinetsbay/site/issues/589)
+ * @used-by Ebizmarts_MailChimp_Model_Api_Subscribers_MailchimpTags::vAddress() (https://github.com/cabinetsbay/site/issues/589)
  * @used-by IWD_OrderManager_Adminhtml_Sales_AddressController::format() (https://github.com/thehcginstitute-com/m1/issues/533)
  */
 function df_country_ctn(string $iso2):string {
