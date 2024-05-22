db0ead6a47719e04154595cac7ab8a641a3c5bb2
Dmitrii Fediuk <dfediuk@gmail.com>
2024-05-23 00:40:57 +0100

    https://github.com/mage2pro/core/issues/375

--- app/code/local/Df/Core/lib/empty.php
+++ app/code/local/Df/Core/lib/empty.php
@@ -20,6 +20,7 @@ function df_es($v) {return '' === $v;}
  * 2017-04-26
  * @used-by df_caller_entry()
  * @used-by df_ci_get()
+ * @used-by df_error_create()
  * @used-by df_fe_fc()
  * @used-by df_fetch_one()
  * @used-by df_oi_add()
