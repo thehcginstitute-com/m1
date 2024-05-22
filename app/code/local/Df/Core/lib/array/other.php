b1017634d4353ed4b8be4aef41eee38ee518ce4e
Dmitrii Fediuk <dfediuk@gmail.com>
2024-05-23 00:40:21 +0100

    https://github.com/mage2pro/core/issues/375

--- app/code/local/Df/Core/lib/array/other.php
+++ app/code/local/Df/Core/lib/array/other.php
@@ -79,6 +79,7 @@ function dfa_group(array $a, $k = null):array {
 
 /**
  * 2021-01-29
+ * @used-by df_error_create()
  * @used-by dfa_try()
  * @used-by Ebizmarts_MailChimp_Model_Api_Subscribers_MailchimpTags::address() (https://github.com/cabinetsbay/site/issues/589)
  * @used-by IWD_OrderManager_Helper_Data::CheckTableEngine()
