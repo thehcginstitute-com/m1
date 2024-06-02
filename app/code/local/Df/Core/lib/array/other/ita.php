2d46b8d31645bf2def029bb99c5f7e9b9dc14b78
Dmitrii Fediuk <dfediuk@gmail.com>
2024-06-03 00:52:16 +0100

    refactoring

--- app/code/local/Df/Core/lib/array/other/ita.php
+++ app/code/local/Df/Core/lib/array/other/ita.php
@@ -2,8 +2,8 @@
 /**
  * 2015-02-11
  * Эта функция отличается от @see iterator_to_array() тем, что допускает в качестве параметра
- * не только @see \Traversable, но и массив.
- * @param \Traversable|array $t
+ * не только @see Traversable, но и массив.
+ * @param Traversable|array $t
  * @return array
  */
 function df_ita($t) {return is_array($t) ? $t : iterator_to_array($t);}
