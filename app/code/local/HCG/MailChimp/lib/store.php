629c2d07f079e499f11fe406ae413d689b9ab530
Dmitrii Fediuk <dfediuk@gmail.com>
2024-06-03 00:03:54 +0100

    https://github.com/thehcginstitute-com/m1/issues/524

--- app/code/local/HCG/MailChimp/lib/store.php
+++ app/code/local/HCG/MailChimp/lib/store.php
@@ -3,4 +3,7 @@
  * 2024-06-03
  * $mcListId is a string like «147da8cfd6».
  */
-function hcg_mc_stores(string $mcListId):array {return [];}
+function hcg_mc_stores(string $mcListId):array {return dfcf(function(string $mcListId):array {
+	$r = []; /** @var int[] $r */
+	return $r;
+}, [$mcListId]);}
