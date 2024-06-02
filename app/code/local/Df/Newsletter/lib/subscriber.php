a40e8fcad860c0d68d768b97325b00145fafca32
Dmitrii Fediuk <dfediuk@gmail.com>
2024-06-02 06:25:09 +0100

    https://github.com/thehcginstitute-com/m1/issues/627

--- app/code/local/Df/Newsletter/lib/subscriber.php
+++ app/code/local/Df/Newsletter/lib/subscriber.php
@@ -2,7 +2,9 @@
 use Mage_Newsletter_Model_Subscriber as S;
 use Mage_Newsletter_Model_Resource_Subscriber_Collection as C;
 /**
- * 2024-06-02 "Implement `df_subscriber()`": https://github.com/thehcginstitute-com/m1/issues/627
+ * 2024-06-02
+ * 1) "Implement `df_subscriber()`": https://github.com/thehcginstitute-com/m1/issues/627
+ * 2) https://3v4l.org/tIHdP
  * @used-by df_subscriber_c()
  * @used-by Ebizmarts_MailChimp_Block_Checkout_Subscribe::_toHtml()
  * @used-by Ebizmarts_MailChimp_Block_Checkout_Success_Groups::getInterest()
@@ -13,7 +15,10 @@ use Mage_Newsletter_Model_Resource_Subscriber_Collection as C;
  * @used-by Ebizmarts_MailChimp_Model_Api_Orders::GeneratePOSTPayload()
  * @used-by Ebizmarts_MailChimp_Model_Observer::createCreditmemo()
  */
-function df_subscriber():S {return Mage::getModel('newsletter/subscriber');}
+function df_subscriber(?string $email = ''):S {
+	$r = Mage::getModel('newsletter/subscriber'); /** @var S $r */
+	return !$email ? $r : $r->loadByEmail($email);
+}
 
 /**
  * 2024-06-02 "Implement `df_subscriber_c()`": https://github.com/thehcginstitute-com/m1/issues/626
