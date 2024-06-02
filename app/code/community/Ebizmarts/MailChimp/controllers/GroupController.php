bf6d1580938085e291190e7ec00614c4728a9eb0
Dmitrii Fediuk <dfediuk@gmail.com>
2024-06-02 10:50:36 +0100

    https://github.com/thehcginstitute-com/m1/issues/524

--- app/code/community/Ebizmarts/MailChimp/controllers/GroupController.php
+++ app/code/community/Ebizmarts/MailChimp/controllers/GroupController.php
@@ -14,96 +14,96 @@
 
 class Ebizmarts_MailChimp_GroupController extends Mage_Core_Controller_Front_Action
 {
-    function indexAction()
-    {
-        $helper = $this->getHelper();
-        $order = $this->getSessionLastRealOrder();
-        $session = $this->getCoreSession();
-        $interestGroup = $this->getInterestGroupModel();
-        $params = $this->getRequest()->getParams();
-        $storeId = $order->getStoreId();
-        $customerEmail = $order->getCustomerEmail();
-        $customerId = $order->getCustomerId();
-        $subscriber = df_subscriber($customerEmail);
-        try {
-            if (!$subscriber->getSubscriberId()) {
-                $subscriber->setSubscriberEmail($customerEmail);
-                $subscriber->setSubscriberFirstname($order->getCustomerFirstname());
-                $subscriber->setSubscriberLastname($order->getCustomerLastname());
-                $subscriber->subscribe($customerEmail);
-            }
+	function indexAction()
+	{
+		$helper = $this->getHelper();
+		$order = $this->getSessionLastRealOrder();
+		$session = $this->getCoreSession();
+		$interestGroup = $this->getInterestGroupModel();
+		$params = $this->getRequest()->getParams();
+		$storeId = $order->getStoreId();
+		$customerEmail = $order->getCustomerEmail();
+		$customerId = $order->getCustomerId();
+		$subscriber = df_subscriber($customerEmail);
+		try {
+			if (!$subscriber->getSubscriberId()) {
+				$subscriber->setSubscriberEmail($customerEmail);
+				$subscriber->setSubscriberFirstname($order->getCustomerFirstname());
+				$subscriber->setSubscriberLastname($order->getCustomerLastname());
+				$subscriber->subscribe($customerEmail);
+			}
 
-            $subscriberId = $subscriber->getSubscriberId();
-            $interestGroup->getByRelatedIdStoreId($customerId, $subscriberId, $storeId);
-            $encodedGroups = $helper->arrayEncode($params);
-            $interestGroup->setGroupdata($encodedGroups);
-            $interestGroup->setSubscriberId($subscriberId);
-            $interestGroup->setCustomerId($customerId);
-            $interestGroup->setStoreId($storeId);
-            $interestGroup->setUpdatedAt($this->getCurrentDateTime());
-            $interestGroup->save();
+			$subscriberId = $subscriber->getSubscriberId();
+			$interestGroup->getByRelatedIdStoreId($customerId, $subscriberId, $storeId);
+			$encodedGroups = $helper->arrayEncode($params);
+			$interestGroup->setGroupdata($encodedGroups);
+			$interestGroup->setSubscriberId($subscriberId);
+			$interestGroup->setCustomerId($customerId);
+			$interestGroup->setStoreId($storeId);
+			$interestGroup->setUpdatedAt($this->getCurrentDateTime());
+			$interestGroup->save();
 
-            $this->getApiSubscriber()->update($subscriber->getSubscriberEmail(), $storeId, '', 1);
+			$this->getApiSubscriber()->update($subscriber->getSubscriberEmail(), $storeId, '', 1);
 
-            $session->addSuccess($this->__('Thanks for sharing your interest with us.'));
-        } catch (Exception $e) {
-            $helper->logError($e->getMessage());
-            $session->addWarning(
-                $this->__(
-                    'Something went wrong with the interests subscription. '
-                    . 'Please go to the account subscription menu to subscriber to the interests successfully.'
-                )
-            );
-        }
+			$session->addSuccess($this->__('Thanks for sharing your interest with us.'));
+		} catch (Exception $e) {
+			$helper->logError($e->getMessage());
+			$session->addWarning(
+				$this->__(
+					'Something went wrong with the interests subscription. '
+					. 'Please go to the account subscription menu to subscriber to the interests successfully.'
+				)
+			);
+		}
 
-        $this->_redirect('/');
-    }
+		$this->_redirect('/');
+	}
 
-    /**
-     * @return Ebizmarts_MailChimp_Helper_Data|Mage_Core_Helper_Abstract
-     */
-    protected function getHelper($type='mailchimp')
-    {
-        return Mage::helper($type);
-    }
+	/**
+	 * @return Ebizmarts_MailChimp_Helper_Data|Mage_Core_Helper_Abstract
+	 */
+	protected function getHelper($type='mailchimp')
+	{
+		return Mage::helper($type);
+	}
 
-    /**
-     * @return Ebizmarts_MailChimp_Model_Api_Subscribers
-     */
-    protected function getApiSubscriber()
-    {
-        return Mage::getModel('mailchimp/api_subscribers');
-    }
+	/**
+	 * @return Ebizmarts_MailChimp_Model_Api_Subscribers
+	 */
+	protected function getApiSubscriber()
+	{
+		return Mage::getModel('mailchimp/api_subscribers');
+	}
 
-    /**
-     * @return Mage_Sales_Model_Order
-     */
-    protected function getSessionLastRealOrder()
-    {
-        return $this->getHelper()->getSessionLastRealOrder();
-    }
+	/**
+	 * @return Mage_Sales_Model_Order
+	 */
+	protected function getSessionLastRealOrder()
+	{
+		return $this->getHelper()->getSessionLastRealOrder();
+	}
 
-    /**
-     * @return Mage_Core_Model_Session
-     */
-    protected function getCoreSession()
-    {
-        return Mage::getSingleton('core/session');
-    }
+	/**
+	 * @return Mage_Core_Model_Session
+	 */
+	protected function getCoreSession()
+	{
+		return Mage::getSingleton('core/session');
+	}
 
-    /**
-     * @return Ebizmarts_MailChimp_Model_Interestgroup
-     */
-    protected function getInterestGroupModel()
-    {
-        return Mage::getModel('mailchimp/interestgroup');
-    }
+	/**
+	 * @return Ebizmarts_MailChimp_Model_Interestgroup
+	 */
+	protected function getInterestGroupModel()
+	{
+		return Mage::getModel('mailchimp/interestgroup');
+	}
 
-    /**
-     * @return string
-     */
-    protected function getCurrentDateTime()
-    {
-        return hcg_mc_h_date()->formatDate(null, 'd-m-Y H:i:s');
-    }
+	/**
+	 * @return string
+	 */
+	protected function getCurrentDateTime()
+	{
+		return hcg_mc_h_date()->formatDate(null, 'd-m-Y H:i:s');
+	}
 }
