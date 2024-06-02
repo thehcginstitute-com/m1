dac28b974e4f2bc783e5de32658cb39d32f1f59f
Dmitrii Fediuk <dfediuk@gmail.com>
2024-06-02 11:40:25 +0100

    https://github.com/thehcginstitute-com/m1/issues/634

--- app/code/local/Df/Customer/lib/other.php
+++ app/code/local/Df/Customer/lib/other.php
@@ -9,11 +9,11 @@ use Mage_Customer_Model_Customer as C;
  * @used-by df_customer()
  * @used-by df_customer_is_need_confirm()
  * @used-by vendor/inkifi/mediaclip-legacy/view/frontend/templates/savedproject.phtml
- * @param C|int|null $c [optional]
+ * @param C|int|null $v [optional]
  * @return int|null
  */
-function df_customer_id($c = null) {return !$c && !df_is_backend() ? df_customer_session()->getId() : (
-	$c instanceof C ? $c->getId() : $c
+function df_customer_id($v = null) {return !$v && !df_is_backend() ? df_customer_session()->getId() : (
+	$v instanceof C ? $v->getId() : $v
 );}
 
 /**
