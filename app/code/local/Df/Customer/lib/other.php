f54a1934a11060c2dd841615e75fb75d5621d908
Dmitrii Fediuk <dfediuk@gmail.com>
2024-06-02 11:47:06 +0100

    refactoring

--- app/code/local/Df/Customer/lib/other.php
+++ app/code/local/Df/Customer/lib/other.php
@@ -1,19 +1,4 @@
 <?php
-use Mage_Customer_Model_Customer as C;
-
-/**
- * 2016-12-04
- * 2024-03-17 "Port `df_customer_id()` from `mage2pro/core`": https://github.com/thehcginstitute-com/m1/issues/498
- * 2024-06-02 "Improve `df_customer_id()`": https://github.com/thehcginstitute-com/m1/issues/634
- * @used-by df_context()
- * @used-by df_customer_is_need_confirm()
- * @used-by vendor/inkifi/mediaclip-legacy/view/frontend/templates/savedproject.phtml
- * @param C|int|null $v [optional]
- */
-function df_customer_id($v = null):?int {return !$v && !df_is_backend() ? df_customer_session()->getId() : (
-	$v instanceof C ? $v->getId() : $v
-);}
-
 /**
  * 2024-03-03
  * "Implement the `df_customer_is_anon()` function": https://github.com/thehcginstitute-com/m1/issues/447
