b98d1d8cfe2a22ead4d4365b1edbbc0685175fcf
Dmitrii Fediuk <dfediuk@gmail.com>
2024-06-02 11:05:35 +0100

    https://github.com/thehcginstitute-com/m1/issues/632

--- app/code/local/Df/Customer/lib/get.php
+++ app/code/local/Df/Customer/lib/get.php
@@ -11,7 +11,8 @@ use Mage_Sales_Model_Order as O;
  * How to get a customer by his ID with the @see \Magento\Customer\Api\CustomerRepositoryInterface::getById()?
  * https://mage2.pro/t/1138
  * 2017-06-14 The $throw argument is not used for now.
- * 2024-05-16 "df_customer `STUB()` from `mage2pro/core`": https://github.com/thehcginstitute-com/m1/issues/612
+ * 2024-05-16 "Port `df_customer()` from `mage2pro/core`": https://github.com/thehcginstitute-com/m1/issues/612
+ * 2024-06-02 "Improve `df_customer()`": https://github.com/thehcginstitute-com/m1/issues/632
  * @used-by df_ci_get()
  * @used-by df_ci_get()
  * @used-by df_ci_save()
@@ -19,28 +20,28 @@ use Mage_Sales_Model_Order as O;
  * @used-by df_sentry_m()
  * @used-by hcg_mc_customer()
  * @used-by \HCG\MailChimp\Tags::c() (https://github.com/thehcginstitute-com/m1/issues/589)
- * @param string|int|C|Sub|null $c [optional]
+ * @param string|int|C|Sub|null $v [optional]
  * @param Closure|bool|mixed $onE [optional]
  * @return C|null
  */
-function df_customer($c = null, $onE = null) {return df_try(function() use($c) {return
+function df_customer($v = null, $onE = null) {return df_try(function() use($v) {return
 	/** @var int|string|null $id */
 	/**
 	 * 2016-08-22
 	 * I do not use @see \Magento\Customer\Model\Session::getCustomer()
 	 * because it does not use the customers repository, and loads a customer directly from the database.
 	 */
-	!$c ? (
+	!$v ? (
 		df_customer_session()->isLoggedIn()
 			? df_customer(df_customer_id())
 			: df_error('df_customer(): the argument is `null` and the visitor is anonymous.')
-	) : ($c instanceof C ? $c : (
+	) : ($v instanceof C ? $v : (
 		($id =
-			$c instanceof O ? $c->getCustomerId() : (
-				is_int($c) || is_string($c) ? $c : ($c instanceof Sub ? $c->getCustomerId() : null)
+			$v instanceof O ? $v->getCustomerId() : (
+				is_int($v) || is_string($v) ? $v : ($v instanceof Sub ? $v->getCustomerId() : null)
 			)
 		)
 			? Mage::getModel('customer/customer')->load($id)
-			: df_error("df_customer(): unable to detect the customer's ID from the argument of type `%s`.", df_type($c))
+			: df_error("df_customer(): unable to detect the customer's ID from the argument of type `%s`.", df_type($v))
 	))
 ;}, $onE);}
