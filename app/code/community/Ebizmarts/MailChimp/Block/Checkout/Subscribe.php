d80ae9c56af708d1c212be2e485737ae3d326d70
Dmitrii Fediuk <dfediuk@gmail.com>
2024-06-02 10:48:44 +0100

    https://github.com/thehcginstitute-com/m1/issues/627

--- app/code/community/Ebizmarts/MailChimp/Block/Checkout/Subscribe.php
+++ app/code/community/Ebizmarts/MailChimp/Block/Checkout/Subscribe.php
@@ -11,130 +11,130 @@
 class Ebizmarts_MailChimp_Block_Checkout_Subscribe extends Mage_Core_Block_Template
 {
 
-    protected $_lists = array();
-    protected $_info = array();
-    protected $_myLists = array();
-    protected $_generalList = array();
-    protected $_form;
-    protected $_api;
-    /**
-     * @var Ebizmarts_MailChimp_Helper_Data
-     */
-    protected $_helper;
-    protected $_storeId;
+	protected $_lists = array();
+	protected $_info = array();
+	protected $_myLists = array();
+	protected $_generalList = array();
+	protected $_form;
+	protected $_api;
+	/**
+	 * @var Ebizmarts_MailChimp_Helper_Data
+	 */
+	protected $_helper;
+	protected $_storeId;
 
-    function __construct()
-    {
-        parent::__construct();
-        $this->_helper = hcg_mc_h();
-        $this->_storeId = Mage::app()->getStore()->getId();
-    }
+	function __construct()
+	{
+		parent::__construct();
+		$this->_helper = hcg_mc_h();
+		$this->_storeId = Mage::app()->getStore()->getId();
+	}
 
-    /**
-     * @param $data
-     * @return string
-     */
-    function escapeQuote($data)
-    {
-        return $this->getHelper()->mcEscapeQuote($data);
-    }
+	/**
+	 * @param $data
+	 * @return string
+	 */
+	function escapeQuote($data)
+	{
+		return $this->getHelper()->mcEscapeQuote($data);
+	}
 
-    /**
-     * @return Ebizmarts_MailChimp_Helper_Data
-     */
-    function getHelper($type='')
-    {
-        return $this->_helper;
-    }
+	/**
+	 * @return Ebizmarts_MailChimp_Helper_Data
+	 */
+	function getHelper($type='')
+	{
+		return $this->_helper;
+	}
 
-    /**
-     * Render block HTML
-     *
-     * @return string
-     */
-    protected function _toHtml()
-    {
-        $helper = $this->_helper;
-        $storeId = $this->_storeId;
-        $alreadySubscribed = df_subscriber($this->getQuote())->isSubscribed();
-        if ($helper->isCheckoutSubscribeEnabled($storeId) && !$alreadySubscribed) {
-            return parent::_toHtml();
-        } else {
-            return '';
-        }
-    }
+	/**
+	 * Render block HTML
+	 *
+	 * @return string
+	 */
+	protected function _toHtml()
+	{
+		$helper = $this->_helper;
+		$storeId = $this->_storeId;
+		$alreadySubscribed = df_subscriber($this->getQuote())->isSubscribed();
+		if ($helper->isCheckoutSubscribeEnabled($storeId) && !$alreadySubscribed) {
+			return parent::_toHtml();
+		} else {
+			return '';
+		}
+	}
 
-    /**
-     * Retrieve current quote object from session
-     *
-     * @return Mage_Sales_Model_Quote
-     */
-    protected function getQuote()
-    {
-        return Mage::getSingleton('checkout/session')
-            ->getQuote();
-    }
+	/**
+	 * Retrieve current quote object from session
+	 *
+	 * @return Mage_Sales_Model_Quote
+	 */
+	protected function getQuote()
+	{
+		return Mage::getSingleton('checkout/session')
+			->getQuote();
+	}
 
-    protected function getCurrentCheckoutSubscribeValue()
-    {
-        return $this->_helper->getCheckoutSubscribeValue($this->_storeId);
-    }
+	protected function getCurrentCheckoutSubscribeValue()
+	{
+		return $this->_helper->getCheckoutSubscribeValue($this->_storeId);
+	}
 
-    protected function isForceHidden($currentValue = null)
-    {
-        if (!$currentValue) {
-            $currentValue = $this->getCurrentCheckoutSubscribeValue();
-        }
+	protected function isForceHidden($currentValue = null)
+	{
+		if (!$currentValue) {
+			$currentValue = $this->getCurrentCheckoutSubscribeValue();
+		}
 
-        return ($currentValue == Ebizmarts_MailChimp_Model_System_Config_Source_Checkoutsubscribe::FORCE_HIDDEN);
-    }
+		return ($currentValue == Ebizmarts_MailChimp_Model_System_Config_Source_Checkoutsubscribe::FORCE_HIDDEN);
+	}
 
-    protected function isForceVisible($currentValue)
-    {
-        return ($currentValue == Ebizmarts_MailChimp_Model_System_Config_Source_Checkoutsubscribe::FORCE_VISIBLE);
-    }
+	protected function isForceVisible($currentValue)
+	{
+		return ($currentValue == Ebizmarts_MailChimp_Model_System_Config_Source_Checkoutsubscribe::FORCE_VISIBLE);
+	}
 
-    protected function isCheckedByDefault($currentValue)
-    {
-        return ($currentValue == Ebizmarts_MailChimp_Model_System_Config_Source_Checkoutsubscribe::CHECKED_BY_DEFAULT);
-    }
+	protected function isCheckedByDefault($currentValue)
+	{
+		return ($currentValue == Ebizmarts_MailChimp_Model_System_Config_Source_Checkoutsubscribe::CHECKED_BY_DEFAULT);
+	}
 
-    function isForceEnabled()
-    {
-        $currentValue = $this->getCurrentCheckoutSubscribeValue();
-        if ($this->isForceHidden($currentValue) || $this->isForceVisible($currentValue)) {
-            return true;
-        }
+	function isForceEnabled()
+	{
+		$currentValue = $this->getCurrentCheckoutSubscribeValue();
+		if ($this->isForceHidden($currentValue) || $this->isForceVisible($currentValue)) {
+			return true;
+		}
 
-        return false;
-    }
+		return false;
+	}
 
-    function isChecked()
-    {
-        $currentValue = $this->getCurrentCheckoutSubscribeValue();
-        if ($this->isCheckedByDefault($currentValue) || $this->isForceVisible($currentValue)) {
-            return true;
-        }
+	function isChecked()
+	{
+		$currentValue = $this->getCurrentCheckoutSubscribeValue();
+		if ($this->isCheckedByDefault($currentValue) || $this->isForceVisible($currentValue)) {
+			return true;
+		}
 
-        return false;
-    }
+		return false;
+	}
 
-    function addToPostOnLoad()
-    {
-        return ($this->isChecked() || $this->isForceHidden());
-    }
+	function addToPostOnLoad()
+	{
+		return ($this->isChecked() || $this->isForceHidden());
+	}
 
-    /**
-     * Get list data from MC
-     *
-     * @return array
-     */
-    function getGeneralList()
-    {
-        $storeId = $this->_storeId;
-        $helper = $this->_helper;
-        $listId = $helper->getGeneralList($storeId);
+	/**
+	 * Get list data from MC
+	 *
+	 * @return array
+	 */
+	function getGeneralList()
+	{
+		$storeId = $this->_storeId;
+		$helper = $this->_helper;
+		$listId = $helper->getGeneralList($storeId);
 
-        return $listId;
-    }
+		return $listId;
+	}
 }
