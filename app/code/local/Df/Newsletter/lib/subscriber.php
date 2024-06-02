ca0429a1f01f82c167e010c9fdd8bb415e6d609d
Dmitrii Fediuk <dfediuk@gmail.com>
2024-06-02 06:02:37 +0100

    https://github.com/thehcginstitute-com/m1/issues/627

--- app/code/local/Df/Newsletter/lib/subscriber.php
+++ app/code/local/Df/Newsletter/lib/subscriber.php
@@ -1,7 +1,14 @@
 <?php
+use Mage_Newsletter_Model_Subscriber as S;
 use Mage_Newsletter_Model_Resource_Subscriber_Collection as C;
+/**
+ * 2024-06-02 "Implement `df_subscriber()`": https://github.com/thehcginstitute-com/m1/issues/627
+ * @used-by df_subscriber_c()
+ */
+function df_subscriber():S {return Mage::getModel('newsletter/subscriber');}
+
 /**
  * 2024-06-02 "Implement `df_subscriber_c()`": https://github.com/thehcginstitute-com/m1/issues/626
  * @used-by Glew_Service_Model_Types_Subscribers::load()
  */
-function df_subscriber_c():C {return new C;}
+function df_subscriber_c():C {return df_subscriber()->getCollection();}
