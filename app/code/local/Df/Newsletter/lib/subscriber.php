8ee01cc54e74479dcaccd98dd6e0203b6a967918
Dmitrii Fediuk <dfediuk@gmail.com>
2024-06-02 06:13:13 +0100

    https://github.com/thehcginstitute-com/m1/issues/627

--- app/code/local/Df/Newsletter/lib/subscriber.php
+++ app/code/local/Df/Newsletter/lib/subscriber.php
@@ -5,19 +5,13 @@ use Mage_Newsletter_Model_Resource_Subscriber_Collection as C;
  * 2024-06-02 "Implement `df_subscriber()`": https://github.com/thehcginstitute-com/m1/issues/627
  * @used-by df_subscriber_c()
  * @used-by Ebizmarts_MailChimp_Block_Checkout_Subscribe::_toHtml()
- * @used-by Ebizmarts_MailChimp_Block_Popup_Emailcatcher::_handleCookie()
- * @used-by Ebizmarts_MailChimp_Model_Api_Customers::createBatchJson()
  * @used-by Ebizmarts_MailChimp_Block_Checkout_Success_Groups::getInterest()
  * @used-by Ebizmarts_MailChimp_Block_Customer_Newsletter_Index::getInterest()
+ * @used-by Ebizmarts_MailChimp_Block_Popup_Emailcatcher::_handleCookie()
  * @used-by Ebizmarts_MailChimp_GroupController::indexAction()
+ * @used-by Ebizmarts_MailChimp_Model_Api_Customers::createBatchJson()
  * @used-by Ebizmarts_MailChimp_Model_Api_Orders::GeneratePOSTPayload()
  * @used-by Ebizmarts_MailChimp_Model_Observer::createCreditmemo()
- * @used-by STUB()
- * @used-by STUB()
- * @used-by STUB()
- * @used-by STUB()
- * @used-by STUB()
- * @used-by STUB()
  */
 function df_subscriber():S {return Mage::getModel('newsletter/subscriber');}
 
