fcdcb76f09c681ad361868b2b33d7822cb3bb4d2
Dmitrii Fediuk <dfediuk@gmail.com>
2024-06-02 05:58:12 +0100

    refactoring

--- app/code/community/Glew/Service/Model/Types/Subscribers.php
+++ app/code/community/Glew/Service/Model/Types/Subscribers.php
@@ -2,35 +2,35 @@
 
 class Glew_Service_Model_Types_Subscribers
 {
-    public $subscribers = array();
-    private $pageNum;
+	public $subscribers = array();
+	private $pageNum;
 
-    public function load($pageSize, $pageNum, $sortDir, $filterBy, $id)
-    {
-        $helper = Mage::helper('glew');
-        $this->pageNum = $pageNum;
-        if ($id) {
-            $subscribers = df_subscriber_c()->addFieldToFilter('main_table.subscriber_id', $id);
-        }
+	public function load($pageSize, $pageNum, $sortDir, $filterBy, $id)
+	{
+		$helper = Mage::helper('glew');
+		$this->pageNum = $pageNum;
+		if ($id) {
+			$subscribers = df_subscriber_c()->addFieldToFilter('main_table.subscriber_id', $id);
+		}
 		else {
-            $subscribers = df_subscriber_c();
-        }
-        $subscribers->addFilter('store_id', 'store_id = '.$helper->getStore()->getStoreId(), 'string');
-        $subscribers->setOrder('subscriber_id', $sortDir);
-        $subscribers->setCurPage($pageNum);
-        $subscribers->setPageSize($pageSize);
+			$subscribers = df_subscriber_c();
+		}
+		$subscribers->addFilter('store_id', 'store_id = '.$helper->getStore()->getStoreId(), 'string');
+		$subscribers->setOrder('subscriber_id', $sortDir);
+		$subscribers->setCurPage($pageNum);
+		$subscribers->setPageSize($pageSize);
 
-        if ($subscribers->getLastPageNumber() < $pageNum) {
-            return $this;
-        }
+		if ($subscribers->getLastPageNumber() < $pageNum) {
+			return $this;
+		}
 
-        foreach ($subscribers as $subscriber) {
-            $model = Mage::getModel('glew/types_subscriber')->parse($subscriber);
-            if ($model) {
-                $this->subscribers[] = $model;
-            }
-        }
+		foreach ($subscribers as $subscriber) {
+			$model = Mage::getModel('glew/types_subscriber')->parse($subscriber);
+			if ($model) {
+				$this->subscribers[] = $model;
+			}
+		}
 
-        return $this;
-    }
+		return $this;
+	}
 }
