721eda2f38ae8eb64b1af84362aa4a3d51628798
Dmitrii Fediuk <dfediuk@gmail.com>
2024-06-03 02:17:09 +0100

    https://github.com/mage2pro/core/issues/403

--- app/code/local/Df/Core/lib/array/map.php
+++ app/code/local/Df/Core/lib/array/map.php
@@ -1,7 +1,23 @@
 <?php
-// Глобальные константы появились в PHP 5.3.
-// http://www.codingforums.com/php/303927-unexpected-t_const-php-version-5-2-17-a.html#post1363452
+use Df\Core\Exception as DFE;
+
+/**
+ * PHP supports global constants since 5.3:
+ * http://www.codingforums.com/php/303927-unexpected-t_const-php-version-5-2-17-a.html#post1363452
+ * @used-by df_find()
+ * @used-by df_map()
+ */
 const DF_AFTER = 1;
+
+/**
+ * PHP supports global constants since 5.3:
+ * http://www.codingforums.com/php/303927-unexpected-t_const-php-version-5-2-17-a.html#post1363452
+ * @used-by df_find()
+ * @used-by df_map()
+ * @used-by df_map_k()
+ * @used-by df_map_kr()
+ * @used-by \Df\Payment\Method::amountFactor()
+ */
 const DF_BEFORE = -1;
 
 /**
@@ -106,13 +122,23 @@ function df_map($a1, $a2, $pAppend = [], $pPrepend = [], int $keyPosition = 0, b
 function df_map_k($a1, $a2):array {return df_map($a1, $a2, [], [], DF_BEFORE);}
 
 /**
- * 2016-11-08
- * Функция принимает аргументы в любом порядке.
- * @param callable|array(int|string => mixed)|array[]\Traversable $a1
- * @param callable|array(int|string => mixed)|array[]|\Traversable $a2
+ * 2016-11-08 Функция принимает аргументы в любом порядке.
+ * 2024-06-03
+ * 1.1) "Use the `iterable` type": https://github.com/mage2pro/core/issues/403
+ * 1.2) `iterable` is supported by PHP ≥ 7.1: https://3v4l.org/qNX1j
+ * 1.3) https://php.net/manual/en/language.types.iterable.php
+ * 2) We still can not use «Union Types» (e.g. `callable|iterable`) because they require PHP ≥ 8 (we need to support PHP ≥ 7.1):
+ * 2.1) https://php.watch/versions/8.0/union-types
+ * 2.2) https://3v4l.org/AOWmO
+ * @used-by dfak_transform()
+ * @used-by \Df\Config\Source\Block::map()
+ * @used-by \Df\Core\Text\Regex::getErrorCodeMap()
+ * @param callable|iterable $a1
+ * @param callable|iterable $a2
  * @return array(int|string => mixed)
+ * @throws DFE
  */
-function df_map_kr($a1, $a2) {return df_map($a1, $a2, [], [], DF_BEFORE, true);}
+function df_map_kr($a1, $a2):array {return df_map($a1, $a2, [], [], DF_BEFORE, true);}
 
 /**
  * 2016-11-08
