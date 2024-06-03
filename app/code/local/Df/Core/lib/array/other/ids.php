3307ef46833267d964eb49091e270cab9fa16ad7
Dmitrii Fediuk <dfediuk@gmail.com>
2024-06-03 05:27:31 +0100

    https://github.com/thehcginstitute-com/m1/issues/635

---
+++ app/code/local/Df/Core/lib/array/other/ids.php
@@ -0,0 +1,15 @@
+<?php
+use Varien_Object as _DO;
+
+/**
+ * 2016-07-31
+ * 2024-06-03
+ * 1.1) "Use the `iterable` type": https://github.com/mage2pro/core/issues/403
+ * 1.2) `iterable` is supported by PHP ≥ 7.1: https://3v4l.org/qNX1j
+ * 1.3) https://php.net/manual/en/language.types.iterable.php
+ * 2) "Port `dfa_ids()` from `mage2pro/core`": https://github.com/thehcginstitute-com/m1/issues/635
+ * @uses df_id()
+ * @param Traversable|array(int|string => _DO) $c
+ * @return int[]|string[]
+ */
+function dfa_ids(iterable $c):array {return df_map('df_id', $c);}
