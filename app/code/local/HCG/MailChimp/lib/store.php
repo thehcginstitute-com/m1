3c0278177c15edb564fc6d19e6fb46d14621b0b5
Dmitrii Fediuk <dfediuk@gmail.com>
2024-06-05 19:33:49 +0100

    refactoring

--- app/code/local/HCG/MailChimp/lib/store.php
+++ app/code/local/HCG/MailChimp/lib/store.php
@@ -2,8 +2,7 @@
 use Ebizmarts_MailChimp_Model_Config as Cfg;
 use Mage_Core_Model_Store as S;
 /**
- * 2024-06-03
- * $mcListId is a string like «147da8cfd6».
+ * 2024-06-03 $mcListId is a string like «147da8cfd6».
  */
 function hcg_mc_stores(string $mcListId):array {return dfcf(function(string $mcListId):array {return df_find(
 	function(S $s) use($mcListId):bool {return
