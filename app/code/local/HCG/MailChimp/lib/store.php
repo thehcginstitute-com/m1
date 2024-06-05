685c12b983b8054d99e50afa42efceacbb22abfd
Dmitrii Fediuk <dfediuk@gmail.com>
2024-06-05 19:45:34 +0100

    https://github.com/thehcginstitute-com/m1/issues/524

--- app/code/local/HCG/MailChimp/lib/store.php
+++ app/code/local/HCG/MailChimp/lib/store.php
@@ -3,6 +3,7 @@ use Ebizmarts_MailChimp_Model_Config as Cfg;
 use Mage_Core_Model_Store as S;
 /**
  * 2024-06-03 $mcListId is a string like «147da8cfd6».
+ * @used-by hcg_mc_sub()
  * @return int[]
  */
 function hcg_mc_stores(string $mcListId):array {return dfcf(function(string $mcListId):array {return dfa_ids(df_find(
