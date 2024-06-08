d5ae31700b4bb892565da2fdb0bc19f21520c804
Dmitrii Fediuk <dfediuk@gmail.com>
2024-06-08 14:23:43 +0100

    https://github.com/thehcginstitute-com/m1/issues/640

--- app/code/local/Df/Config/lib/o.php
+++ app/code/local/Df/Config/lib/o.php
@@ -1,6 +1,7 @@
 <?php
+use Mage_Core_Model_Resource_Config_Data_Collection as C;
 /**
  * 2024-06-08
  * "Implenent `df_config_c()`": https://github.com/thehcginstitute-com/m1/issues/640
  */
-function df_config_c() {return Mage::getResourceModel('core/config_data_collection');}
+function df_config_c():C {return Mage::getResourceModel('core/config_data_collection');}
