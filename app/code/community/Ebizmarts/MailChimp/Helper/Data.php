7c886cd4019a45efd823f21ab27d3dfa7c862ba4
Dmitrii Fediuk <dfediuk@gmail.com>
2024-06-08 15:15:10 +0100

    https://github.com/thehcginstitute-com/m1/issues/641

--- app/code/community/Ebizmarts/MailChimp/Helper/Data.php
+++ app/code/community/Ebizmarts/MailChimp/Helper/Data.php
@@ -1,4 +1,5 @@
 <?php
+use HCG\MailChimp\Cfg as hCfg;
 # 2024-06-08
 # Transfer the configuration code from `Ebizmarts_MailChimp_Helper_Data`
 # to a dedicated class (`HCG\MailChimp\Cfg`) and `hcg_mc_cfg_*` functions: https://github.com/thehcginstitute-com/m1/issues/641
