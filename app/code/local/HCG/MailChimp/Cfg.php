77e8e057d5ed3ccc8a4079955b367e55613f9fee
Dmitrii Fediuk <dfediuk@gmail.com>
2024-06-08 14:51:45 +0100

    https://github.com/thehcginstitute-com/m1/issues/641

--- app/code/local/HCG/MailChimp/Cfg.php
+++ app/code/local/HCG/MailChimp/Cfg.php
@@ -6,8 +6,9 @@ use Mage_Core_Model_Config_Data as C;
 # to a dedicated class (`HCG\MailChimp\Cfg`) and `hcg_mc_cfg_*` functions: https://github.com/thehcginstitute-com/m1/issues/641
 final class Cfg {
 	/**
-	 * 2024-06-08 Dmitrii Fediuk https://upwork.com/fl/mage2pro
-	 * "Refactor the `Ebizmarts_MailChimp` module": https://github.com/thehcginstitute-com/m1/issues/524
+	 * 2024-06-08
+	 * Transfer the configuration code from `Ebizmarts_MailChimp_Helper_Data`
+	 * to a dedicated class (`HCG\MailChimp\Cfg`) and `hcg_mc_cfg_*` functions: https://github.com/thehcginstitute-com/m1/issues/641
 	 * @used-by hcg_mc_cfg_scope()
 	 */
 	static function isExtraEntry(C $c, string $s, int $sid, int $websiteId):bool {
