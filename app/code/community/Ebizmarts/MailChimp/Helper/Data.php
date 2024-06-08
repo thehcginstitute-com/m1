ffa2ad1eb5358b377407975018ac5bdf2c18fcfe
Dmitrii Fediuk <dfediuk@gmail.com>
2024-06-08 14:46:04 +0100

    https://github.com/thehcginstitute-com/m1/issues/641

--- app/code/community/Ebizmarts/MailChimp/Helper/Data.php
+++ app/code/community/Ebizmarts/MailChimp/Helper/Data.php
@@ -2601,15 +2601,13 @@ class Ebizmarts_MailChimp_Helper_Data extends Mage_Core_Helper_Abstract {
 	}
 
 	/**
-	 * @param $config
-	 * @param $scope
-	 * @param $scopeId
-	 * @return bool
+	 * 2024-06-08 Dmitrii Fediuk https://upwork.com/fl/mage2pro
+	 * "Refactor the `Ebizmarts_MailChimp` module": https://github.com/thehcginstitute-com/m1/issues/524
+	 * @used-by HCG\MailChimp\Cfg::isExtraEntry() (https://github.com/thehcginstitute-com/m1/issues/641)
 	 */
-	protected function isDifferentStoreView($config, $scope, $scopeId)
-	{
-		return $config->getScope() == 'stores' && $scope == 'stores' && $scopeId != $config->getScopeId();
-	}
+	function isDifferentStoreView($config, $scope, $scopeId) {return
+		$config->getScope() == 'stores' && $scope == 'stores' && $scopeId != $config->getScopeId()
+	;}
 
 	/**
 	 * 2024-06-08 Dmitrii Fediuk https://upwork.com/fl/mage2pro
