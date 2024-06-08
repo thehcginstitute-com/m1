7f5566f96e1d6830f435971918c27fe3b7794b2e
Dmitrii Fediuk <dfediuk@gmail.com>
2024-06-08 14:41:08 +0100

    https://github.com/thehcginstitute-com/m1/issues/641

--- app/code/community/Ebizmarts/MailChimp/Helper/Data.php
+++ app/code/community/Ebizmarts/MailChimp/Helper/Data.php
@@ -1,4 +1,7 @@
 <?php
+# 2024-06-08
+# Transfer the configuration code from `Ebizmarts_MailChimp_Helper_Data`
+# to a dedicated class (`HCG\MailChimp\Cfg`) and `hcg_mc_cfg_*` functions: https://github.com/thehcginstitute-com/m1/issues/641
 class Ebizmarts_MailChimp_Helper_Data extends Mage_Core_Helper_Abstract {
 	const DEFAULT_SIZE = '0';
 	const SMALL_SIZE = '1';
--- app/code/local/HCG/MailChimp/Cfg.php
+++ app/code/local/HCG/MailChimp/Cfg.php
@@ -3,4 +3,6 @@ namespace HCG\MailChimp;
 # 2024-06-08
 # Transfer the configuration code from `Ebizmarts_MailChimp_Helper_Data`
 # to a dedicated class (`HCG\MailChimp\Cfg`) and `hcg_mc_cfg_*` functions: https://github.com/thehcginstitute-com/m1/issues/641
-final class Cfg {}
+final class Cfg {
+
+}
