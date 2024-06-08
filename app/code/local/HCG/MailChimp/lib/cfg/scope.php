ec6efa898013f21f7d75e59d9b5aa14406a85fc9
Dmitrii Fediuk <dfediuk@gmail.com>
2024-06-08 15:03:11 +0100

    https://github.com/thehcginstitute-com/m1/issues/641

--- app/code/local/HCG/MailChimp/lib/cfg/scope.php
+++ app/code/local/HCG/MailChimp/lib/cfg/scope.php
@@ -31,7 +31,7 @@ function hcg_mc_cfg_scope(string $path, int $scopeId, string $scope = 'stores'):
 				$r = ['scope_id' => $c->getScopeId(), 'scope' => $c->getScope()];
 				break;
 			case F::SCOPE_WEBSITES:
-				if (!$r || $r['scope'] == 'default') {
+				if (!$r || F::SCOPE_DEFAULT === $r['scope']) {
 					$r = ['scope_id' => $c->getScopeId(), 'scope' => $c->getScope()];
 				}
 				break;
