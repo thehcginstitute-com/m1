3401df5d0c1ab2b3927a69610d9e38ed46cf9074
Dmitrii Fediuk <dfediuk@gmail.com>
2024-06-15 12:05:04 +0100

    refactoring

--- app/code/local/Df/Core/Boot.php
+++ app/code/local/Df/Core/Boot.php
@@ -87,8 +87,7 @@ class Boot {
 		};
 		$iterate = function(string $base, array $mm) use($requireFiles):void  {
 			foreach ($mm as $m) {/** @var string $m */
-				# 2016-11-23
-				# It gets rid of the ['..', '.'] and the root files (non-directories).
+				# 2016-11-23 It gets rid of the ['..', '.'] and the root files (non-directories).
 				/** @var string $baseM */
 				if (ctype_upper($m[0]) && is_dir($baseM = $base . $m)) {
 					/** @var string $libDir */
