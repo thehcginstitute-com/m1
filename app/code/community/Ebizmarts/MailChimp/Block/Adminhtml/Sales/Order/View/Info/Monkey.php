e5a5370d4d9776b4323d4ee4bdea71640e863586
Dmitrii Fediuk <dfediuk@gmail.com>
2024-08-31 22:38:40 +0100

    https://github.com/thehcginstitute-com/m1/issues/668

--- app/code/community/Ebizmarts/MailChimp/Block/Adminhtml/Sales/Order/View/Info/Monkey.php
+++ app/code/community/Ebizmarts/MailChimp/Block/Adminhtml/Sales/Order/View/Info/Monkey.php
@@ -1,125 +1,117 @@
 <?php
-
-/**
- * mc-magento Magento Component
- *
- * @category  Ebizmarts
- * @package   mc-magento
- * @author    Ebizmarts Team <info@ebizmarts.com>
- * @copyright Ebizmarts (http://ebizmarts.com)
- * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
- * @date:     12/8/16 2:04 PM
- * @file:     Monkey.php
- */
+# 2024-09-01 Dmitrii Fediuk https://upwork.com/fl/mage2pro
+# 1) "Refactor the `Ebizmarts_MailChimp` module": https://github.com/cabinetsbay/site/issues/524
+# 2) "Improve the «Yaay! Recovered by Mailchimp's campaign» block of the backend order screen":
+# https://github.com/thehcginstitute-com/m1/issues/668
 class Ebizmarts_MailChimp_Block_Adminhtml_Sales_Order_View_Info_Monkey extends Mage_Core_Block_Template
 {
 
-    /**
-     * @var string $campaignName
-     */
-    protected $_campaignName = null;
+	/**
+	 * @var string $campaignName
+	 */
+	protected $_campaignName = null;
 
-    /**
-     * @var Mage_Sales_Model_Order $order
-     */
-    protected $_order = null;
+	/**
+	 * @var Mage_Sales_Model_Order $order
+	 */
+	protected $_order = null;
 
-    /**
-     * @return bool
-     */
-    function isReferred()
-    {
-        $order = $this->getCurrentOrder();
-        $ret = false;
-        if ($order->getMailchimpAbandonedcartFlag() || $order->getMailchimpCampaignId()) {
-            $ret = true;
-        }
+	/**
+	 * @return bool
+	 */
+	function isReferred()
+	{
+		$order = $this->getCurrentOrder();
+		$ret = false;
+		if ($order->getMailchimpAbandonedcartFlag() || $order->getMailchimpCampaignId()) {
+			$ret = true;
+		}
 
-        return $ret;
-    }
+		return $ret;
+	}
 
-    /**
-     * @return string
-     */
-    function getCampaignId()
-    {
-        $order = $this->getCurrentOrder();
-        return $order->getMailchimpCampaignId();
-    }
+	/**
+	 * @return string
+	 */
+	function getCampaignId()
+	{
+		$order = $this->getCurrentOrder();
+		return $order->getMailchimpCampaignId();
+	}
 
-    /**
-     * @return string
-     */
-    function getCampaignName()
-    {
-        if (!$this->_campaignName) {
-            $campaignId = $this->getCampaignId();
-            $order = $this->getCurrentOrder();
-            $storeId = $order->getStoreId();
-            $helper = $this->getMailChimpHelper();
+	/**
+	 * @return string
+	 */
+	function getCampaignName()
+	{
+		if (!$this->_campaignName) {
+			$campaignId = $this->getCampaignId();
+			$order = $this->getCurrentOrder();
+			$storeId = $order->getStoreId();
+			$helper = $this->getMailChimpHelper();
 
-            if ($helper->isEcomSyncDataEnabled($storeId)) {
-                $this->_campaignName = $helper->getMailChimpCampaignNameById($campaignId, $storeId);
-            }
-        }
+			if ($helper->isEcomSyncDataEnabled($storeId)) {
+				$this->_campaignName = $helper->getMailChimpCampaignNameById($campaignId, $storeId);
+			}
+		}
 
-        return $this->_campaignName;
-    }
+		return $this->_campaignName;
+	}
 
-    /**
-     * @param $data
-     * @return string
-     */
-    function escapeQuote($data)
-    {
-        return $this->getMailChimpHelper()->mcEscapeQuote($data);
-    }
+	/**
+	 * @param $data
+	 * @return string
+	 */
+	function escapeQuote($data)
+	{
+		return $this->getMailChimpHelper()->mcEscapeQuote($data);
+	}
 
-    /**
-     * @return Ebizmarts_MailChimp_Helper_Data
-     */
-    function getMailChimpHelper() {return hcg_mc_h();}
+	/**
+	 * @return Ebizmarts_MailChimp_Helper_Data
+	 */
+	function getMailChimpHelper() {return hcg_mc_h();}
 
-    /**
-     * @return Mage_Sales_Model_Order
-     */
-    protected function getCurrentOrder()
-    {
-        if (!$this->_order) {
-            $this->_order = Mage::registry('current_order');
-        }
+	/**
+	 * @return Mage_Sales_Model_Order
+	 */
+	protected function getCurrentOrder()
+	{
+		if (!$this->_order) {
+			$this->_order = Mage::registry('current_order');
+		}
 
-        return $this->_order;
-    }
+		return $this->_order;
+	}
 
-    /**
-     * Return true if campaign data is available with the current api and list selected.
-     *
-     * @return bool
-     */
-    function isDataAvailable()
-    {
-        $dataAvailable = false;
-        $campaignName = $this->getCampaignName();
+	/**
+	 * Return true if campaign data is available with the current api and list selected.
+	 *
+	 * @return bool
+	 */
+	function isDataAvailable()
+	{
+		$dataAvailable = false;
+		$campaignName = $this->getCampaignName();
 
-        if ($campaignName) {
-            $dataAvailable = true;
-        }
+		if ($campaignName) {
+			$dataAvailable = true;
+		}
 
-        return $dataAvailable;
-    }
+		return $dataAvailable;
+	}
 
-    /**
-     * @return string
-     * @throws Mage_Core_Model_Store_Exception
-     */
-    function getStoreCodeFromOrder()
-    {
-        $helper = $this->getMailChimpHelper();
-        $order = $this->getCurrentOrder();
-        $storeId = $order->getStoreId();
-        $storeCode = $helper->getMageApp()->getStore($storeId)->getCode();
+	/**
+	 * @return string
+	 * @throws Mage_Core_Model_Store_Exception
+	 */
+	function getStoreCodeFromOrder()
+	{
+		$helper = $this->getMailChimpHelper();
+		$order = $this->getCurrentOrder();
+		$storeId = $order->getStoreId();
+		$storeCode = $helper->getMageApp()->getStore($storeId)->getCode();
 
-        return $storeCode;
-    }
+		return $storeCode;
+	}
 }
