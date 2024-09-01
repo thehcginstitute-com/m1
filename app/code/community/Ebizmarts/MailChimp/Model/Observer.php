378c276b250d553d28643e6b8fadf9bab9c1e117
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-01 01:41:53 +0100

    https://github.com/thehcginstitute-com/m1/issues/668

--- app/code/community/Ebizmarts/MailChimp/Model/Observer.php
+++ app/code/community/Ebizmarts/MailChimp/Model/Observer.php
@@ -544,10 +544,13 @@ class Ebizmarts_MailChimp_Model_Observer {
 	 *		</core_block_abstract_to_html_after>
 	 * https://github.com/thehcginstitute-com/m1/blob/2024-09-01/app/code/community/Ebizmarts/MailChimp/etc/config.xml#L319-L327
 	 * 4)
+	 *		if (self::$_transportObject === null) {
+	 *			self::$_transportObject = new Varien_Object();
+	 *		}
 	 *		self::$_transportObject->setHtml($html);
 	 *		Mage::dispatchEvent('core_block_abstract_to_html_after', ['block' => $this, 'transport' => self::$_transportObject]);
 	 *		$html = self::$_transportObject->getHtml();
-	 * https://github.com/thehcginstitute-com/m1/blob/2024-09-01/app/code/core/Mage/Core/Block/Abstract.php#L949-L954
+	 * https://github.com/thehcginstitute-com/m1/blob/2024-09-01/app/code/core/Mage/Core/Block/Abstract.php#L946-L954
 	 * @used-by Mage_Core_Model_App::_callObserverMethod()
 	 */
 	function addOrderViewMonkey(Ob $ob):Ob {
