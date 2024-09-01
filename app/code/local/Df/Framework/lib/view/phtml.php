a72450b000b0800674cd3fa37a050b1f09ac1b55
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-01 18:01:08 +0100

    refactoring

--- app/code/local/Df/Framework/lib/view/phtml.php
+++ app/code/local/Df/Framework/lib/view/phtml.php
@@ -6,3 +6,10 @@
  * @used-by \Df\Qa\Trace\Frame::isPHTML()
  */
 function df_is_phtml(string $f):bool {return df_ends_with($f, '.phtml');}
+
+/**
+ * 2023-08-01
+ * @used-by df_block()
+ * @used-by df_phtml_exists()
+ */
+function df_phtml_add_ext(string $f):string {return df_file_ext_add($f, 'phtml');}
