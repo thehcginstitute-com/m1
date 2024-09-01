59fc85ce62b9cdcfbd02be24d2b61d32c44a2acc
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-01 18:19:10 +0100

    refactoring

--- app/code/local/Df/Framework/lib/fs/main.php
+++ app/code/local/Df/Framework/lib/fs/main.php
@@ -1,13 +1,5 @@
 <?php
 use Df\Framework\Fs;
-use Varien_Io_File as F;
-
-/**
- * 2024-04-20 "Port `df_fs_delete()` from `mage2pro/core`": https://github.com/thehcginstitute-com/m1/issues/570
- * @used-by hcg_mc_batch_delete() (https://github.com/thehcginstitute-com/m1/issues/569)
- */
-function df_fs_delete(string $p):void {F::rmdirRecursive(df_param_sne($p, 0));}
-
 /**
  * 2015-11-29
  * 2018-07-06 The `$append` parameter has been added.
