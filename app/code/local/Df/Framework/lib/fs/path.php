04ef8db8c2dd5392dbff73a44fc06d682eff7349
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-01 18:14:49 +0100

    refactoring

--- app/code/local/Df/Framework/lib/fs/path.php
+++ app/code/local/Df/Framework/lib/fs/path.php
@@ -1,22 +1,5 @@
 <?php
 /**
- * Заменяет все сиволы пути на /
- * 2021-12-17 https://3v4l.org/8iP17
- * @see df_path_n_real()
- * @used-by df_path_rel_g()
- * @used-by df_bt_s()
- * @used-by df_class_file()
- * @used-by df_explode_path()
- * @used-by df_file_name()
- * @used-by df_path_is_internal()
- * @used-by df_path_relative()
- * @used-by df_product_image_url()
- * @param string $p
- * @return string
- */
-function df_path_n($p) {return str_replace(['\/', '\\'], '/', $p);}
-
-/**
  * 2015-12-06 It trims the ending «/».
  * @used-by df_module_name_by_path()
  * @used-by \Df\Qa\Failure\Error::preface()
