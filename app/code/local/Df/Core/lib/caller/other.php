9b5eb92505867d616638cd9d973f9b58e1b30f45
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-01 21:03:55 +0100

    refactoring

--- app/code/local/Df/Core/lib/caller/other.php
+++ app/code/local/Df/Core/lib/caller/other.php
@@ -14,14 +14,10 @@ function df_caller_c(int $o = 0):string {return df_first(df_explode_method(df_ca
  * 2017-01-12
  * The df_caller_ff() implementation: https://github.com/mage2pro/core/blob/6.7.3/Core/lib/caller.php#L113-L123
  * 2020-07-08 The function's new implementation is from the previous df_caller_ff() function.
- * @used-by df_log_e()
- * @used-by df_log_l()
  * @used-by df_oqi_amount()
  * @used-by df_prop()
- * @param int $o [optional]
- * @return string
  */
-function df_caller_f($o = 0) {return df_caller_entry(++$o)['function'];}
+function df_caller_f(int $o = 0):string {return df_caller_entry(++$o)['function'];}
 
 /**
  * 2016-08-10
