d92b3554accd68efecb5bc4f2fef3b7e4b9f1868
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-01 17:20:24 +0100

    refactoring

--- app/code/local/Df/Core/lib/http/main.php
+++ app/code/local/Df/Core/lib/http/main.php
@@ -3,8 +3,8 @@
  * 2024-01-09 "Port `df_request()`": https://github.com/thehcginstitute-com/m1/issues/141
  * @used-by Ebizmarts_MailChimp_Block_Adminhtml_Customer_Edit_Tab_Mailchimp::interests() (https://github.com/thehcginstitute-com/m1/issues/579)
  * @used-by Ebizmarts_MailChimp_Model_Observer::addCustomerTab() (https://github.com/thehcginstitute-com/m1/issues/524)
- * @used-by app/code/community/INT/DisplayCvv/Block/Payment/Info/Ccsave.php
  * @used-by HCG\MailChimp\Observer\AddTabToCustomer::p() (https://github.com/thehcginstitute-com/m1/issues/580)
+ * @used-by INT\DisplayCvv\B::_prepareSpecificInformation() (https://github.com/thehcginstitute-com/m1/issues/142)
  * @used-by app/design/adminhtml/default/default/template/sales/order/view/info.phtml (https://github.com/thehcginstitute-com/m1/issues/669)
  * @param string|string[] $k [optional]
  * @param string|null|callable $d [optional]
