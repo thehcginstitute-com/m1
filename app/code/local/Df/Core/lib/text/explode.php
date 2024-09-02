16c1807a0b91e7ffd2344ff9f94d2eefb7ad4e4d
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-02 01:37:51 +0100

    refactoring

--- app/code/local/Df/Core/lib/text/explode.php
+++ app/code/local/Df/Core/lib/text/explode.php
@@ -1,18 +1,16 @@
 <?php
 /**
  * 2016-03-25 «charge.dispute.funds_reinstated» => [charge, dispute, funds, reinstated]
+ * @used-by df_explode_class()
+ * @used-by df_fe_name_short()
  * @param string[] $delimiters
- * @param string $s
  * @return string[]
  */
-function df_explode_multiple(array $delimiters, $s) {
+function df_explode_multiple(array $delimiters, string $s):array {
 	$main = array_shift($delimiters); /** @var string $main */
-	/**
-	 * 2016-03-25
-	 * «If search is an array and replace is a string,
-	 * then this replacement string is used for every value of search.»
-	 * http://php.net/manual/function.str-replace.php
-	 */
+	# 2016-03-25
+	# «If `search` is an array and `replace` is a string, then this replacement string is used for every value of `search`.»
+	# https://php.net/manual/function.str-replace.php
 	return explode($main, str_replace($delimiters, $main, $s));
 }
 
