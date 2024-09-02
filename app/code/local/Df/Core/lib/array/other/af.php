5c3d053a78ca5c67547b2917f1b5825b24420b1f
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-02 01:40:54 +0100

    https://github.com/thehcginstitute-com/m1/issues/652

--- app/code/local/Df/Core/lib/array/other/af.php
+++ app/code/local/Df/Core/lib/array/other/af.php
@@ -1,6 +1,7 @@
 <?php
 /**
- * 2017-02-18 [array|callable, array|callable] => [array, callable]
+ * 2017-02-18 [iterable|callable, iterable|callable] => [iterable, callable]
+ * @used-by df_call_a()
  * @used-by df_filter_f()
  * @used-by df_find()
  * @used-by df_map()
@@ -17,20 +18,24 @@ function dfaf($a, $b):array {
 	# 1) https://php.watch/versions/8.2/partially-supported-callable-deprecation
 	# 2.1) `is_callable([__CLASS__, 'f'])` for a private `f` is allowed: https://3v4l.org/ctZJG
 	# 2.2) `array_map([__CLASS__, 'f'], [1, 2, 3])` for a private `f` is allowed too: https://3v4l.org/29Zim
-	$ca = is_callable($a); /** @var bool $ca */
-	$cb = is_callable($b); /** @var bool $ca */
-	if (!$ca || !$cb) {
-		df_assert($ca || $cb);
-		$r = $ca ? [df_assert_iterable($b), $a] : [df_assert_iterable($a), $b];
+	# 2024-06-11 "Improve `dfaf()`": https://github.com/mage2pro/core/issues/421
+	$assert = function(bool $cond, string $m) use($a, $b):void {df_assert($cond, "dfaf(): $m.", ['a' => $a, 'b' => $b]);};
+	/** @var bool $ca */ /** @var bool $cb *//** @var bool $ia */ /** @var bool $ib */
+	[$ca, $cb, $ia, $ib] = [is_callable($a), is_callable($b), is_iterable($a), is_iterable($b)];
+	if ($ca && $cb) {
+		$assert($ia xor $ib, !$ia ? 'none of arguments are `iterable`': 'both arguments are `callable` and `iterable`');
+		$r = $ia ? [$a, $b] : [$b, $a];
 	}
 	else {
-		$ta = is_iterable($a); /** @var bool $ta */
-		$tb = is_iterable($b); /** @var bool $tb */
-		if ($ta && $tb) {
-			df_error('dfaf(): both arguments are callable and traversable: %s and %s.', df_type($a), df_type($b));
+		$assert($ca || $cb, 'none of arguments are callable');
+		if ($ca) {
+			$assert($ib, '$a is `callable`, and $b is not `callable`, so $b must be `iterable`');
+			$r = [$b, $a];
 		}
-		df_assert($ta || $tb);
-		$r = $ta ? [$a, $b] : [$b, $a];
+		else {
+			$assert($ia, '$b is `callable`, and $a is not `callable`, so $a must be `iterable`');
+			$r = [$a, $b];
+		}
 	}
 	return $r;
 }
