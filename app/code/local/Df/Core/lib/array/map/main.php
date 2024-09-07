99d5d0912d364fcf1c837d82cc96f5cf0051f9da
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-07 01:28:27 +0100

    refactoring

--- app/code/local/Df/Core/lib/array/map/main.php
+++ app/code/local/Df/Core/lib/array/map/main.php
@@ -16,7 +16,6 @@ const DF_AFTER = 1;
  * @used-by df_map()
  * @used-by df_map_k()
  * @used-by df_map_kr()
- * @used-by \Df\Payment\Method::amountFactor()
  */
 const DF_BEFORE = -1;
 
@@ -98,70 +97,3 @@ function df_map($a1, $a2, $pAppend = [], $pPrepend = [], int $keyPosition = 0, b
 	}
 	return $r;
 }
-
-/**
- * 2016-08-09 Функция принимает аргументы в любом порядке.
- * 2024-06-03
- * 1.1) "Use the `iterable` type": https://github.com/mage2pro/core/issues/403
- * 1.2) `iterable` is supported by PHP ≥ 7.1: https://3v4l.org/qNX1j
- * 1.3) https://php.net/manual/en/language.types.iterable.php
- * 2) We still can not use «Union Types» (e.g. `callable|iterable`) because they require PHP ≥ 8 (we need to support PHP ≥ 7.1):
- * 2.1) https://php.watch/versions/8.0/union-types
- * 2.2) https://3v4l.org/AOWmO
- * @used-by df_kv()
- * @used-by df_kv_table()
- * @used-by df_modules_my()
- * @used-by dfe_modules_log()
- * @used-by dfp_methods()
- * @used-by \Df\Payment\ConfigProvider::configOptions()
- * @used-by \Df\Qa\Dumper::dumpArrayElements()
- * @used-by \Df\Qa\Trace\Formatter::p()
- * @used-by \Df\Sentry\Client::send()
- * @param callable|iterable $a1
- * @param callable|iterable $a2
- * @return array(int|string => mixed)
- */
-function df_map_k($a1, $a2):array {return df_map($a1, $a2, [], [], DF_BEFORE);}
-
-/**
- * 2016-11-08 Функция принимает аргументы в любом порядке.
- * 2024-06-03
- * 1.1) "Use the `iterable` type": https://github.com/mage2pro/core/issues/403
- * 1.2) `iterable` is supported by PHP ≥ 7.1: https://3v4l.org/qNX1j
- * 1.3) https://php.net/manual/en/language.types.iterable.php
- * 2) We still can not use «Union Types» (e.g. `callable|iterable`) because they require PHP ≥ 8 (we need to support PHP ≥ 7.1):
- * 2.1) https://php.watch/versions/8.0/union-types
- * 2.2) https://3v4l.org/AOWmO
- * @used-by dfak_transform()
- * @used-by \Df\Config\Source\Block::map()
- * @used-by \Df\Core\Text\Regex::getErrorCodeMap()
- * @param callable|iterable $a1
- * @param callable|iterable $a2
- * @return array(int|string => mixed)
- * @throws DFE
- */
-function df_map_kr($a1, $a2):array {return df_map($a1, $a2, [], [], DF_BEFORE, true);}
-
-/**
- * 2016-11-08 Функция принимает аргументы в любом порядке.
- * 2024-06-03
- * 1.1) "Use the `iterable` type": https://github.com/mage2pro/core/issues/403
- * 1.2) `iterable` is supported by PHP ≥ 7.1: https://3v4l.org/qNX1j
- * 1.3) https://php.net/manual/en/language.types.iterable.php
- * 2) We still can not use «Union Types» (e.g. `callable|iterable`) because they require PHP ≥ 8 (we need to support PHP ≥ 7.1):
- * 2.1) https://php.watch/versions/8.0/union-types
- * 2.2) https://3v4l.org/AOWmO
- * @used-by df_category_children_map()
- * @used-by df_modules_my()
- * @used-by df_parse_colon()
- * @used-by dfe_packages()
- * @used-by dfe_portal_stripe_customers()
- * @used-by \Df\Config\Source\Block::map()
- * @used-by \Df\Directory\Model\ResourceModel\Country\Collection::mapFrom3To2()
- * @used-by \Df\Directory\Model\ResourceModel\Country\Collection::mapFromCodeToName()
- * @used-by \Df\Framework\Form\Element\Multiselect::getElementHtml()
- * @param callable|iterable $a1
- * @param callable|iterable $a2
- * @return array(int|string => mixed)
- */
-function df_map_r($a1, $a2):array {return df_map($a1, $a2, [], [], 0, true);}
