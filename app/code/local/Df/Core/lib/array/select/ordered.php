d932b995605067ab2bf0cf5f9dd0f97a0463d04f
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-07 03:56:43 +0100

    refactoring

--- app/code/local/Df/Core/lib/array/select/ordered.php
+++ app/code/local/Df/Core/lib/array/select/ordered.php
@@ -37,14 +37,3 @@ function dfa_select_ordered($a, array $k) {
 	$resultWithGarbage = dfa_merge_numeric($resultKeys, df_ita($a));
 	return array_intersect_key($resultWithGarbage, $resultKeys);
 }
-
-/**
- * 2019-01-28
- * @used-by Glew_Service_ModuleController::_initRequest()
- * 2024-03-05
- * 1) https://3v4l.org/C3qrh
- * 2) The previous code (`dfa_seq`): https://github.com/mage2pro/core/blob/10.6.9/Core/lib/array/main.php#L214-L231
- * @param array(int|string => mixed) $a
- * @return mixed|null
- */
-function dfa_try(array $a, string ...$k) {return df_first(dfa($a, $k));}
