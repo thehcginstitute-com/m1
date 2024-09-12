8dd7408fd122e65fb43288d1ab9d0373de81c6ed
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-12 17:32:46 +0100

    https://github.com/thehcginstitute-com/m1/issues/680

--- app/code/core/Mage/Customer/Block/Account/Navigation.php
+++ app/code/core/Mage/Customer/Block/Account/Navigation.php
@@ -33,14 +33,11 @@ class Mage_Customer_Block_Account_Navigation extends Mage_Core_Block_Template
     protected $_activeLink = false;
 
     /**
-     * @param string $name
-     * @param string $path
-     * @param string $label
-     * @param array $urlParams
-     * @return $this
+     * 2024-09-12 Dmitrii Fediuk https://upwork.com/fl/mage2pro
+	 * "The names of arguments in `<action method="<methodName>">` calls should match the `methodName`'s arguments":
+	 * https://github.com/thehcginstitute-com/m1/issues/680
      */
-    function addLink($name, $path, $label, $urlParams = [])
-    {
+    function addLink(string $name, string $path, string $label, array $urlParams = []):self {
         $this->_links[$name] = new Varien_Object([
             'name' => $name,
             'path' => $path,
