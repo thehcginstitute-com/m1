2bb5b1034b8d00f2a78894f6a3a8caecd159177b
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-14 15:55:47 +0100

    refactoring

--- app/code/core/Mage/Adminhtml/Block/Page/Head.php
+++ app/code/core/Mage/Adminhtml/Block/Page/Head.php
@@ -5,32 +5,32 @@
 # https://github.com/thehcginstitute-com/m1/issues/583
 class Mage_Adminhtml_Block_Page_Head extends HCG_Page_Block_Html_Head
 {
-    /**
-     * @return string
-     */
-    protected function _getUrlModelClass()
-    {
-        return 'adminhtml/url';
-    }
+	/**
+	 * @return string
+	 */
+	protected function _getUrlModelClass()
+	{
+		return 'adminhtml/url';
+	}
 
-    /**
-     * Retrieve Session Form Key
-     *
-     * @return string
-     */
-    function getFormKey()
-    {
-        return Mage::getSingleton('core/session')->getFormKey();
-    }
+	/**
+	 * Retrieve Session Form Key
+	 *
+	 * @return string
+	 */
+	function getFormKey()
+	{
+		return Mage::getSingleton('core/session')->getFormKey();
+	}
 
-    /**
-     * Retrieve Timeout Delay from Config
-     *
-     * @return int
-     * @since 19.4.18 / 20.0.16
-     */
-    function getLoadingTimeout()
-    {
-        return (int)Mage::getStoreConfig('admin/design/loading_timeout');
-    }
+	/**
+	 * Retrieve Timeout Delay from Config
+	 *
+	 * @return int
+	 * @since 19.4.18 / 20.0.16
+	 */
+	function getLoadingTimeout()
+	{
+		return (int)Mage::getStoreConfig('admin/design/loading_timeout');
+	}
 }
