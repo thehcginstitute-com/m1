c149f65bd021ffe9e0f6e131d874e096cc8ba9b1
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-14 15:02:53 +0100

    https://github.com/thehcginstitute-com/m1/issues/680

--- app/code/core/Mage/Adminhtml/Block/Widget/Breadcrumbs.php
+++ app/code/core/Mage/Adminhtml/Block/Widget/Breadcrumbs.php
@@ -39,6 +39,10 @@ class Mage_Adminhtml_Block_Widget_Breadcrumbs extends Mage_Adminhtml_Block_Templ
 	}
 
 	/**
+     * 2024-09-14 Dmitrii Fediuk https://upwork.com/fl/mage2pro
+	 * "The names of arguments in `<action method="<methodName>">` calls should match the `methodName`'s arguments":
+	 * https://github.com/thehcginstitute-com/m1/issues/680
+	 * @used-by self::__construct()
 	 * @param string $label
 	 * @param string|null $title
 	 * @param string|null $url
