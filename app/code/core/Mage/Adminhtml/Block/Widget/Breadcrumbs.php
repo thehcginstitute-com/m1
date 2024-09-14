4044b918734bef2011eb947bb1fa3306146c083f
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-14 15:03:16 +0100

    https://github.com/thehcginstitute-com/m1/issues/680

--- app/code/core/Mage/Adminhtml/Block/Widget/Breadcrumbs.php
+++ app/code/core/Mage/Adminhtml/Block/Widget/Breadcrumbs.php
@@ -43,6 +43,7 @@ class Mage_Adminhtml_Block_Widget_Breadcrumbs extends Mage_Adminhtml_Block_Templ
 	 * "The names of arguments in `<action method="<methodName>">` calls should match the `methodName`'s arguments":
 	 * https://github.com/thehcginstitute-com/m1/issues/680
 	 * @used-by self::__construct()
+	 * @used-by Mage_Adminhtml_Block_Widget::_addBreadcrumb()
 	 * @param string $label
 	 * @param string|null $title
 	 * @param string|null $url
