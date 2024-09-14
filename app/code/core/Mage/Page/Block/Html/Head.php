a4c551a3ad3a6a53334db61bd60e9bdc80f276eb
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-14 15:56:32 +0100

    refactoring

--- app/code/core/Mage/Page/Block/Html/Head.php
+++ app/code/core/Mage/Page/Block/Html/Head.php
@@ -1,25 +1,5 @@
 <?php
 /**
- * OpenMage
- *
- * This source file is subject to the Open Software License (OSL 3.0)
- * that is bundled with this package in the file LICENSE.txt.
- * It is also available at https://opensource.org/license/osl-3-0-php
- *
- * @category   Mage
- * @package    Mage_Page
- * @copyright  Copyright (c) 2006-2020 Magento, Inc. (https://www.magento.com)
- * @copyright  Copyright (c) 2019-2023 The OpenMage Contributors (https://www.openmage.org)
- * @license    https://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
- */
-
-/**
- * Html page block
- *
- * @category   Mage
- * @package    Mage_Page
- * @author     Magento Core Team <core@magentocommerce.com>
- *
  * @method $this setCanLoadCalendarJs(bool $value)
  * @method $this setDescription(string $value)
  * @method $this setKeywords(string $value)
