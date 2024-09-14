a3cb2b1f8124ff2afe41eccc0a18b9dd697bd056
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-14 15:46:16 +0100

    refactoring

--- app/code/core/Mage/Cms/Block/Block.php
+++ app/code/core/Mage/Cms/Block/Block.php
@@ -25,62 +25,62 @@
  */
 class Mage_Cms_Block_Block extends Mage_Core_Block_Abstract
 {
-    /**
-     * Initialize cache
-     */
-    protected function _construct()
-    {
-        /*
-        * setting cache to save the cms block
-        */
-        $this->setCacheTags([Mage_Cms_Model_Block::CACHE_TAG]);
-        $this->setCacheLifetime(false);
-    }
+	/**
+	 * Initialize cache
+	 */
+	protected function _construct()
+	{
+		/*
+		* setting cache to save the cms block
+		*/
+		$this->setCacheTags([Mage_Cms_Model_Block::CACHE_TAG]);
+		$this->setCacheLifetime(false);
+	}
 
-    /**
-     * Prepare Content HTML
-     *
-     * @return string
-     * @throws Mage_Core_Model_Store_Exception
-     * @throws Exception
-     */
-    protected function _toHtml()
-    {
-        $blockId = $this->getBlockId();
-        $html = '';
-        if ($blockId) {
-            $block = Mage::getModel('cms/block')
-                ->setStoreId(Mage::app()->getStore()->getId())
-                ->load($blockId);
-            if ($block->getIsActive()) {
-                /** @var Mage_Cms_Helper_Data $helper */
-                $helper = Mage::helper('cms');
-                $processor = $helper->getBlockTemplateProcessor();
-                $html = $processor->filter($block->getContent());
-                $this->addModelTags($block);
-            }
-        }
-        return $html;
-    }
+	/**
+	 * Prepare Content HTML
+	 *
+	 * @return string
+	 * @throws Mage_Core_Model_Store_Exception
+	 * @throws Exception
+	 */
+	protected function _toHtml()
+	{
+		$blockId = $this->getBlockId();
+		$html = '';
+		if ($blockId) {
+			$block = Mage::getModel('cms/block')
+				->setStoreId(Mage::app()->getStore()->getId())
+				->load($blockId);
+			if ($block->getIsActive()) {
+				/** @var Mage_Cms_Helper_Data $helper */
+				$helper = Mage::helper('cms');
+				$processor = $helper->getBlockTemplateProcessor();
+				$html = $processor->filter($block->getContent());
+				$this->addModelTags($block);
+			}
+		}
+		return $html;
+	}
 
-    /**
-     * Retrieve values of properties that unambiguously identify unique content
-     *
-     * @return array
-     * @throws Mage_Core_Model_Store_Exception
-     */
-    function getCacheKeyInfo()
-    {
-        $blockId = $this->getBlockId();
-        if ($blockId) {
-            $result = [
-                'CMS_BLOCK',
-                $blockId,
-                Mage::app()->getStore()->getCode(),
-            ];
-        } else {
-            $result = parent::getCacheKeyInfo();
-        }
-        return $result;
-    }
+	/**
+	 * Retrieve values of properties that unambiguously identify unique content
+	 *
+	 * @return array
+	 * @throws Mage_Core_Model_Store_Exception
+	 */
+	function getCacheKeyInfo()
+	{
+		$blockId = $this->getBlockId();
+		if ($blockId) {
+			$result = [
+				'CMS_BLOCK',
+				$blockId,
+				Mage::app()->getStore()->getCode(),
+			];
+		} else {
+			$result = parent::getCacheKeyInfo();
+		}
+		return $result;
+	}
 }
