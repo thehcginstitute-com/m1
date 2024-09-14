6502f51fce056a8e6a89a12858fc8824a2933526
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-14 06:30:11 +0100

    https://github.com/thehcginstitute-com/m1/issues/681

--- app/code/core/Mage/Adminhtml/Block/Catalog/Product/Edit/Tab/Ajax/Serializer.php
+++ app/code/core/Mage/Adminhtml/Block/Catalog/Product/Edit/Tab/Ajax/Serializer.php
@@ -20,39 +20,39 @@
  */
 class Mage_Adminhtml_Block_Catalog_Product_Edit_Tab_Ajax_Serializer extends Mage_Core_Block_Template
 {
-    function _construct()
-    {
-        parent::_construct();
-        $this->setTemplate('catalog/product/edit/serializer.phtml');
-        return $this;
-    }
+	function _construct()
+	{
+		parent::_construct();
+		$this->setTemplate('catalog/product/edit/serializer.phtml');
+		return $this;
+	}
 
-    function getProductsJSON()
-    {
-        $result = [];
-        if ($this->getProducts()) {
-            $isEntityId = $this->getIsEntityId();
-            foreach ($this->getProducts() as $product) {
-                $id = $isEntityId ? $product->getEntityId() : $product->getId();
-                $result[$id] = $product->toArray(['qty', 'position']);
-            }
-        }
-        return $result ? Zend_Json::encode($result) : '{}';
-    }
+	function getProductsJSON()
+	{
+		$result = [];
+		if ($this->getProducts()) {
+			$isEntityId = $this->getIsEntityId();
+			foreach ($this->getProducts() as $product) {
+				$id = $isEntityId ? $product->getEntityId() : $product->getId();
+				$result[$id] = $product->toArray(['qty', 'position']);
+			}
+		}
+		return $result ? Zend_Json::encode($result) : '{}';
+	}
 
-    /**
-     * Initialize grid block under the "Related Products", "Up-sells", "Cross-sells" sections
-     *
-     * @param string $blockName
-     * @param string $getProductFunction
-     * @param string $inputName
-     */
-    function initSerializerBlock($blockName, $getProductFunction, $inputName)
-    {
-        if ($block = $this->getLayout()->getBlock($blockName)) {
-            $this->setGridBlock($block)
-                ->setProducts(Mage::registry('current_product')->$getProductFunction())
-                ->setInputElementName($inputName);
-        }
-    }
+	/**
+	 * Initialize grid block under the "Related Products", "Up-sells", "Cross-sells" sections
+	 *
+	 * @param string $blockName
+	 * @param string $getProductFunction
+	 * @param string $inputName
+	 */
+	function initSerializerBlock($blockName, $getProductFunction, $inputName)
+	{
+		if ($block = $this->getLayout()->getBlock($blockName)) {
+			$this->setGridBlock($block)
+				->setProducts(Mage::registry('current_product')->$getProductFunction())
+				->setInputElementName($inputName);
+		}
+	}
 }
--- app/code/core/Mage/Adminhtml/Block/Widget/Grid/Serializer.php
+++ app/code/core/Mage/Adminhtml/Block/Widget/Grid/Serializer.php
@@ -20,96 +20,96 @@
  */
 class Mage_Adminhtml_Block_Widget_Grid_Serializer extends Mage_Core_Block_Template
 {
-    /**
-     * Store grid input names to serialize
-     *
-     * @var array
-     */
-    private $_inputsToSerialize = [];
+	/**
+	 * Store grid input names to serialize
+	 *
+	 * @var array
+	 */
+	private $_inputsToSerialize = [];
 
-    /**
-     * Set serializer template
-     *
-     * @return $this
-     */
-    function _construct()
-    {
-        parent::_construct();
-        $this->setTemplate('widget/grid/serializer.phtml');
-        return $this;
-    }
+	/**
+	 * Set serializer template
+	 *
+	 * @return $this
+	 */
+	function _construct()
+	{
+		parent::_construct();
+		$this->setTemplate('widget/grid/serializer.phtml');
+		return $this;
+	}
 
-    /**
-     * Register grid column input name to serialize
-     *
-     * @param array|string $names
-     */
-    function addColumnInputName($names)
-    {
-        if (is_array($names)) {
-            foreach ($names as $name) {
-                $this->addColumnInputName($name);
-            }
-        } else {
-            if (!in_array($names, $this->_inputsToSerialize)) {
-                $this->_inputsToSerialize[] = $names;
-            }
-        }
-    }
+	/**
+	 * Register grid column input name to serialize
+	 *
+	 * @param array|string $names
+	 */
+	function addColumnInputName($names)
+	{
+		if (is_array($names)) {
+			foreach ($names as $name) {
+				$this->addColumnInputName($name);
+			}
+		} else {
+			if (!in_array($names, $this->_inputsToSerialize)) {
+				$this->_inputsToSerialize[] = $names;
+			}
+		}
+	}
 
-    /**
-     * Get grid column input names to serialize
-     *
-     * @param bool $asJSON
-     * @return array|string
-     */
-    function getColumnInputNames($asJSON = false)
-    {
-        if ($asJSON) {
-            return Mage::helper('core')->jsonEncode($this->_inputsToSerialize);
-        }
-        return $this->_inputsToSerialize;
-    }
+	/**
+	 * Get grid column input names to serialize
+	 *
+	 * @param bool $asJSON
+	 * @return array|string
+	 */
+	function getColumnInputNames($asJSON = false)
+	{
+		if ($asJSON) {
+			return Mage::helper('core')->jsonEncode($this->_inputsToSerialize);
+		}
+		return $this->_inputsToSerialize;
+	}
 
-    /**
-     * Get object data as JSON
-     *
-     * @return string
-     */
-    function getDataAsJSON()
-    {
-        $result = [];
-        if ($serializeData = $this->getSerializeData()) {
-            $result = $serializeData;
-        } elseif (!empty($this->_inputsToSerialize)) {
-            return '{}';
-        }
-        return Mage::helper('core')->jsonEncode($result);
-    }
+	/**
+	 * Get object data as JSON
+	 *
+	 * @return string
+	 */
+	function getDataAsJSON()
+	{
+		$result = [];
+		if ($serializeData = $this->getSerializeData()) {
+			$result = $serializeData;
+		} elseif (!empty($this->_inputsToSerialize)) {
+			return '{}';
+		}
+		return Mage::helper('core')->jsonEncode($result);
+	}
 
-    /**
-     * Initialize grid block
-     *
-     * Get grid block from layout by specified block name
-     * Get serialize data to manage it (called specified method, that return data to manage)
-     * Also use reload param name for saving grid checked boxes states
-     *
-     *
-     * @param Mage_Adminhtml_Block_Widget_Grid | string $grid grid object or grid block name
-     * @param string $callback block method  to retrieve data to serialize
-     * @param string $hiddenInputName hidden input name where serialized data will be store
-     * @param string $reloadParamName name of request parametr that will be used to save setted data while reload grid
-     */
-    function initSerializerBlock($grid, $callback, $hiddenInputName, $reloadParamName = 'entityCollection')
-    {
-        if (is_string($grid)) {
-            $grid = $this->getLayout()->getBlock($grid);
-        }
-        if ($grid instanceof Mage_Adminhtml_Block_Widget_Grid) {
-            $this->setGridBlock($grid)
-                 ->setInputElementName($hiddenInputName)
-                 ->setReloadParamName($reloadParamName)
-                 ->setSerializeData($grid->$callback());
-        }
-    }
+	/**
+	 * Initialize grid block
+	 *
+	 * Get grid block from layout by specified block name
+	 * Get serialize data to manage it (called specified method, that return data to manage)
+	 * Also use reload param name for saving grid checked boxes states
+	 *
+	 *
+	 * @param Mage_Adminhtml_Block_Widget_Grid | string $grid grid object or grid block name
+	 * @param string $callback block method  to retrieve data to serialize
+	 * @param string $hiddenInputName hidden input name where serialized data will be store
+	 * @param string $reloadParamName name of request parametr that will be used to save setted data while reload grid
+	 */
+	function initSerializerBlock($grid, $callback, $hiddenInputName, $reloadParamName = 'entityCollection')
+	{
+		if (is_string($grid)) {
+			$grid = $this->getLayout()->getBlock($grid);
+		}
+		if ($grid instanceof Mage_Adminhtml_Block_Widget_Grid) {
+			$this->setGridBlock($grid)
+				 ->setInputElementName($hiddenInputName)
+				 ->setReloadParamName($reloadParamName)
+				 ->setSerializeData($grid->$callback());
+		}
+	}
 }
