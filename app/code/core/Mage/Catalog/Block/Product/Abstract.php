fe279f7de9c9ab58d34804e086f2a8065f6b1f2c
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-15 23:04:19 +0100

    refactoring

--- app/code/core/Mage/Catalog/Block/Product/Abstract.php
+++ app/code/core/Mage/Catalog/Block/Product/Abstract.php
@@ -254,7 +254,7 @@ abstract class Mage_Catalog_Block_Product_Abstract extends Mage_Core_Block_Templ
 
 	/**
 	 * Adding customized price template for product type
-     * 2024-09-13 Dmitrii Fediuk https://upwork.com/fl/mage2pro
+	 * 2024-09-13 Dmitrii Fediuk https://upwork.com/fl/mage2pro
 	 * "The names of arguments in `<action method="<methodName>">` calls should match the `methodName`'s arguments":
 	 * https://github.com/thehcginstitute-com/m1/issues/680
 	 */
