0fd16f9a18c550998f2cf5b8f6960a8466f9e6da
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-16 00:15:55 +0100

    https://github.com/thehcginstitute-com/m1/issues/680

--- app/code/core/Mage/Page/Block/Html/Wrapper.php
+++ app/code/core/Mage/Page/Block/Html/Wrapper.php
@@ -1,8 +1,6 @@
 <?php
 /**
  * A generic wrapper block that renders its children and supports a few parameters of the wrapper HTML-element
- * @method bool hasElementId()
- * @method string getElementId()
  * @method bool hasMayBeInvisible()
  * @method bool hasOtherParams()
  * @method string getOtherParams()
@@ -35,11 +33,10 @@ class Mage_Page_Block_Html_Wrapper extends Mage_Core_Block_Abstract {
 		if ($this->_isInvisible()) {
 			return $html;
 		}
-		$id          = $this->hasElementId() ? sprintf(' id="%s"', $this->getElementId()) : '';
 		$otherParams = $this->hasOtherParams() ? ' ' . $this->getOtherParams() : '';
 		return sprintf('<%1$s%2$s%3$s%4$s>%5$s</%1$s>',
 			$this->getElementTagName()
-			,$id
+			,($id = $this[self::$ELEMENT_ID]) ? " id='{$id}'" : ''
 			,($с = $this[self::$ELEMENT_CLASS]) ? " class='{$с}'" : ''
 			,$otherParams
 			,$html
@@ -58,11 +55,24 @@ class Mage_Page_Block_Html_Wrapper extends Mage_Core_Block_Abstract {
 
 	/**
 	 * 2024-09-16 Dmitrii Fediuk https://upwork.com/fl/mage2pro
+	 */
+	final function setElementId(string $v):void {$this[self::$ELEMENT_ID] = $v;}
+
+	/**
+	 * 2024-09-16 Dmitrii Fediuk https://upwork.com/fl/mage2pro
 	 * @used-by self::_toHtml()
 	 * @used-by self::setElementClass()
 	 * @const string
 	 */
 	private static $ELEMENT_CLASS = 'element_class';
+
+	/**
+	 * 2024-09-16 Dmitrii Fediuk https://upwork.com/fl/mage2pro
+	 * @used-by self::_toHtml()
+	 * @used-by self::setElementId()
+	 * @const string
+	 */
+	private static $ELEMENT_ID = 'element_id';
 
 	/**
 	 * Wrapper element tag name getter
