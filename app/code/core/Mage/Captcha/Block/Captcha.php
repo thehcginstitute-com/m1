b94612215135ccd6e48bca92d4f1e249e8ba035c
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-16 00:37:42 +0100

    refactoring

--- app/code/core/Mage/Captcha/Block/Captcha.php
+++ app/code/core/Mage/Captcha/Block/Captcha.php
@@ -1,29 +1,24 @@
 <?php
-/**
- * OpenMage
- *
- * This source file is subject to the Open Software License (OSL 3.0)
- * that is bundled with this package in the file LICENSE.txt.
- * It is also available at https://opensource.org/license/osl-3-0-php
- *
- * @category   Mage
- * @package    Mage_Captcha
- * @copyright  Copyright (c) 2006-2020 Magento, Inc. (https://www.magento.com)
- * @copyright  Copyright (c) 2020-2022 The OpenMage Contributors (https://www.openmage.org)
- * @license    https://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
- */
+class Mage_Captcha_Block_Captcha extends Mage_Core_Block_Template {
+	/**
+	 * 2024-09-16 Dmitrii Fediuk https://upwork.com/fl/mage2pro
+	 * @used-by self::_toHtml()
+	 */
+	final function getFormId():?string {return $this[self::$FORM_ID];}
 
-/**
- * Captcha block
- *
- * @category   Core
- * @package    Mage_Captcha
- * @author     Magento Core Team <core@magentocommerce.com>
- *
- * @method string getFormId()
- */
-class Mage_Captcha_Block_Captcha extends Mage_Core_Block_Template
-{
+	/**
+	 * 2024-09-16 Dmitrii Fediuk https://upwork.com/fl/mage2pro
+	 */
+	final function setFormId(string $v):void {$this[self::$FORM_ID] = $v;}
+
+	/**
+	 * 2024-09-16 Dmitrii Fediuk https://upwork.com/fl/mage2pro
+	 * @used-by self::getFormId()
+	 * @used-by self::setFormId()
+	 * @const string
+	 */
+	private static $FORM_ID = 'form_id';
+
 	/**
 	 * Renders captcha HTML (if required)
 	 *
