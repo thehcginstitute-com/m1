73d7f1820156c76571c2a4171d90b54fc043c904
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-15 23:22:03 +0100

    refactoring

--- app/code/core/Mage/Adminhtml/Block/Dashboard/Grid.php
+++ app/code/core/Mage/Adminhtml/Block/Dashboard/Grid.php
@@ -22,14 +22,14 @@
  */
 class Mage_Adminhtml_Block_Dashboard_Grid extends Mage_Adminhtml_Block_Widget_Grid
 {
-    /**
-     * Setting default for every grid on dashboard
-     *
-     */
-    function __construct()
-    {
-        parent::__construct();
-        $this->setTemplate('dashboard/grid.phtml');
-        $this->setDefaultLimit(5);
-    }
+	/**
+	 * Setting default for every grid on dashboard
+	 *
+	 */
+	function __construct()
+	{
+		parent::__construct();
+		$this->setTemplate('dashboard/grid.phtml');
+		$this->setDefaultLimit(5);
+	}
 }
