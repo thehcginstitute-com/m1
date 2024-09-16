3f190870824e8abb140cf1daec11437eeea5fa9e
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-16 02:37:18 +0100

    refactoring

--- app/code/core/Mage/Catalog/Block/Seo/Sitemap/Tree/Category.php
+++ app/code/core/Mage/Catalog/Block/Seo/Sitemap/Tree/Category.php
@@ -1,25 +1,4 @@
 <?php
-/**
- * OpenMage
- *
- * This source file is subject to the Open Software License (OSL 3.0)
- * that is bundled with this package in the file LICENSE.txt.
- * It is also available at https://opensource.org/license/osl-3-0-php
- *
- * @category   Mage
- * @package    Mage_Catalog
- * @copyright  Copyright (c) 2006-2020 Magento, Inc. (https://www.magento.com)
- * @copyright  Copyright (c) 2019-2022 The OpenMage Contributors (https://www.openmage.org)
- * @license    https://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
- */
-
-/**
- * SEO tree Categories Sitemap block
- *
- * @category   Mage
- * @package    Mage_Catalog
- * @author     Magento Core Team <core@magentocommerce.com>
- */
 class Mage_Catalog_Block_Seo_Sitemap_Tree_Category extends Mage_Catalog_Block_Seo_Sitemap_Category
 {
 	public const XML_PATH_LINES_PER_PAGE = 'catalog/sitemap/lines_perpage';
