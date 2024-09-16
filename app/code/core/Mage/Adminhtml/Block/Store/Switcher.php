3bbe019a583fe359ce20329e6c3c2f9de63279c0
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-17 00:08:51 +0100

    refactoring

--- app/code/core/Mage/Adminhtml/Block/Store/Switcher.php
+++ app/code/core/Mage/Adminhtml/Block/Store/Switcher.php
@@ -1,27 +1,6 @@
 <?php
-/**
- * OpenMage
- *
- * This source file is subject to the Open Software License (OSL 3.0)
- * that is bundled with this package in the file LICENSE.txt.
- * It is also available at https://opensource.org/license/osl-3-0-php
- *
- * @category   Mage
- * @package    Mage_Adminhtml
- * @copyright  Copyright (c) 2006-2020 Magento, Inc. (https://www.magento.com)
- * @copyright  Copyright (c) 2018-2022 The OpenMage Contributors (https://www.openmage.org)
- * @license    https://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
- */
-
-/**
- * Store switcher block
- *
- * @category   Mage
- * @package    Mage_Adminhtml
- * @author     Magento Core Team <core@magentocommerce.com>
- */
-class Mage_Adminhtml_Block_Store_Switcher extends Mage_Adminhtml_Block_Template
-{
+# 2024-09-17 Dmitrii Fediuk https://upwork.com/fl/mage2pro	
+class Mage_Adminhtml_Block_Store_Switcher extends Mage_Adminhtml_Block_Template {
 	/**
 	 * @var array
 	 */
