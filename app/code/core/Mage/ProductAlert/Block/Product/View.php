674177f7c8269490802217c4be41633a3a4b90d2
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-16 01:14:27 +0100

    refactoring

--- app/code/core/Mage/ProductAlert/Block/Product/View.php
+++ app/code/core/Mage/ProductAlert/Block/Product/View.php
@@ -24,71 +24,71 @@
  */
 class Mage_ProductAlert_Block_Product_View extends Mage_Core_Block_Template
 {
-    /**
-     * Current product instance
-     *
-     * @var Mage_Catalog_Model_Product
-     */
-    protected $_product = null;
+	/**
+	 * Current product instance
+	 *
+	 * @var Mage_Catalog_Model_Product
+	 */
+	protected $_product = null;
 
-    /**
-     * Helper instance
-     *
-     * @var Mage_ProductAlert_Helper_Data|null
-     */
-    protected $_helper = null;
+	/**
+	 * Helper instance
+	 *
+	 * @var Mage_ProductAlert_Helper_Data|null
+	 */
+	protected $_helper = null;
 
-    /**
-     * Check whether the stock alert data can be shown and prepare related data
-     */
-    function prepareStockAlertData()
-    {
-        if (!$this->_getHelper()->isStockAlertAllowed() || !$this->_product || $this->_product->isAvailable()) {
-            $this->setTemplate('');
-            return;
-        }
-        $this->setSignupUrl($this->_getHelper()->getSaveUrl('stock'));
-    }
+	/**
+	 * Check whether the stock alert data can be shown and prepare related data
+	 */
+	function prepareStockAlertData()
+	{
+		if (!$this->_getHelper()->isStockAlertAllowed() || !$this->_product || $this->_product->isAvailable()) {
+			$this->setTemplate('');
+			return;
+		}
+		$this->setSignupUrl($this->_getHelper()->getSaveUrl('stock'));
+	}
 
-    /**
-     * Check whether the price alert data can be shown and prepare related data
-     */
-    function preparePriceAlertData()
-    {
-        if (!$this->_getHelper()->isPriceAlertAllowed()
-            || !$this->_product || $this->_product->getCanShowPrice() === false
-        ) {
-            $this->setTemplate('');
-            return;
-        }
-        $this->setSignupUrl($this->_getHelper()->getSaveUrl('price'));
-    }
+	/**
+	 * Check whether the price alert data can be shown and prepare related data
+	 */
+	function preparePriceAlertData()
+	{
+		if (!$this->_getHelper()->isPriceAlertAllowed()
+			|| !$this->_product || $this->_product->getCanShowPrice() === false
+		) {
+			$this->setTemplate('');
+			return;
+		}
+		$this->setSignupUrl($this->_getHelper()->getSaveUrl('price'));
+	}
 
-    /**
-     * Get current product instance
-     *
-     * @inheritDoc
-     */
-    protected function _prepareLayout()
-    {
-        $product = Mage::registry('current_product');
-        if ($product && $product->getId()) {
-            $this->_product = $product;
-        }
+	/**
+	 * Get current product instance
+	 *
+	 * @inheritDoc
+	 */
+	protected function _prepareLayout()
+	{
+		$product = Mage::registry('current_product');
+		if ($product && $product->getId()) {
+			$this->_product = $product;
+		}
 
-        return parent::_prepareLayout();
-    }
+		return parent::_prepareLayout();
+	}
 
-    /**
-     * Retrieve helper instance
-     *
-     * @return Mage_ProductAlert_Helper_Data
-     */
-    protected function _getHelper()
-    {
-        if (is_null($this->_helper)) {
-            $this->_helper = Mage::helper('productalert');
-        }
-        return $this->_helper;
-    }
+	/**
+	 * Retrieve helper instance
+	 *
+	 * @return Mage_ProductAlert_Helper_Data
+	 */
+	protected function _getHelper()
+	{
+		if (is_null($this->_helper)) {
+			$this->_helper = Mage::helper('productalert');
+		}
+		return $this->_helper;
+	}
 }
