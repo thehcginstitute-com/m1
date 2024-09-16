104e27ab6a6ee6a061db15bfeb0abae3dd5e507b
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-16 02:36:36 +0100

    refactoring

--- app/code/core/Mage/Catalog/Block/Seo/Sitemap/Product.php
+++ app/code/core/Mage/Catalog/Block/Seo/Sitemap/Product.php
@@ -24,38 +24,38 @@
  */
 class Mage_Catalog_Block_Seo_Sitemap_Product extends Mage_Catalog_Block_Seo_Sitemap_Abstract
 {
-    /**
-     * Initialize products collection
-     *
-     * @return $this
-     */
-    protected function _prepareLayout()
-    {
-        /** @var Mage_Catalog_Model_Resource_Product_Collection $collection */
-        $collection = Mage::getModel('catalog/product')->getCollection();
-        $collection
-            ->addAttributeToSelect('name')
-            ->addAttributeToSelect('url_key')
-            ->addStoreFilter()
-            ->addAttributeToFilter('status', [
-                'in' => Mage::getSingleton('catalog/product_status')->getVisibleStatusIds()
-            ]);
-        Mage::getSingleton('catalog/product_visibility')->addVisibleInCatalogFilterToCollection($collection);
+	/**
+	 * Initialize products collection
+	 *
+	 * @return $this
+	 */
+	protected function _prepareLayout()
+	{
+		/** @var Mage_Catalog_Model_Resource_Product_Collection $collection */
+		$collection = Mage::getModel('catalog/product')->getCollection();
+		$collection
+			->addAttributeToSelect('name')
+			->addAttributeToSelect('url_key')
+			->addStoreFilter()
+			->addAttributeToFilter('status', [
+				'in' => Mage::getSingleton('catalog/product_status')->getVisibleStatusIds()
+			]);
+		Mage::getSingleton('catalog/product_visibility')->addVisibleInCatalogFilterToCollection($collection);
 
-        $this->setCollection($collection);
-        return $this;
-    }
+		$this->setCollection($collection);
+		return $this;
+	}
 
-    /**
-     * Get item URL
-     *
-     * @param Mage_Catalog_Model_Product $product
-     * @return string
-     */
-    function getItemUrl($product)
-    {
-        $helper = Mage::helper('catalog/product');
-        /** @var Mage_Catalog_Helper_Product $helper */
-        return $helper->getProductUrl($product);
-    }
+	/**
+	 * Get item URL
+	 *
+	 * @param Mage_Catalog_Model_Product $product
+	 * @return string
+	 */
+	function getItemUrl($product)
+	{
+		$helper = Mage::helper('catalog/product');
+		/** @var Mage_Catalog_Helper_Product $helper */
+		return $helper->getProductUrl($product);
+	}
 }
