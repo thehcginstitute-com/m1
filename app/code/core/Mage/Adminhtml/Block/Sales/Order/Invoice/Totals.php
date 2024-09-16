c0f4c25d9f5a999de1c381d60f61112594ad79c1
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-16 16:22:41 +0100

    refactoring

--- app/code/core/Mage/Adminhtml/Block/Sales/Order/Invoice/Totals.php
+++ app/code/core/Mage/Adminhtml/Block/Sales/Order/Invoice/Totals.php
@@ -1,56 +1,35 @@
 <?php
-/**
- * OpenMage
- *
- * This source file is subject to the Open Software License (OSL 3.0)
- * that is bundled with this package in the file LICENSE.txt.
- * It is also available at https://opensource.org/license/osl-3-0-php
- *
- * @category   Mage
- * @package    Mage_Adminhtml
- * @copyright  Copyright (c) 2006-2020 Magento, Inc. (https://www.magento.com)
- * @copyright  Copyright (c) 2021 The OpenMage Contributors (https://www.openmage.org)
- * @license    https://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
- */
+# 2024-09-16 Dmitrii Fediuk https://upwork.com/fl/mage2pro
+class Mage_Adminhtml_Block_Sales_Order_Invoice_Totals extends Mage_Adminhtml_Block_Sales_Totals {
+	protected $_invoice = null;
 
-/**
- * Adminhtml order invoice totals block
- *
- * @category   Mage
- * @package    Mage_Adminhtml
- * @author     Magento Core Team <core@magentocommerce.com>
- */
-class Mage_Adminhtml_Block_Sales_Order_Invoice_Totals extends Mage_Adminhtml_Block_Sales_Totals
-{
-    protected $_invoice = null;
+	function getInvoice()
+	{
+		if ($this->_invoice === null) {
+			if ($this->hasData('invoice')) {
+				$this->_invoice = $this->_getData('invoice');
+			} elseif (Mage::registry('current_invoice')) {
+				$this->_invoice = Mage::registry('current_invoice');
+			} elseif ($this->getParentBlock()->getInvoice()) {
+				$this->_invoice = $this->getParentBlock()->getInvoice();
+			}
+		}
+		return $this->_invoice;
+	}
 
-    function getInvoice()
-    {
-        if ($this->_invoice === null) {
-            if ($this->hasData('invoice')) {
-                $this->_invoice = $this->_getData('invoice');
-            } elseif (Mage::registry('current_invoice')) {
-                $this->_invoice = Mage::registry('current_invoice');
-            } elseif ($this->getParentBlock()->getInvoice()) {
-                $this->_invoice = $this->getParentBlock()->getInvoice();
-            }
-        }
-        return $this->_invoice;
-    }
+	function getSource()
+	{
+		return $this->getInvoice();
+	}
 
-    function getSource()
-    {
-        return $this->getInvoice();
-    }
-
-    /**
-     * Initialize order totals array
-     *
-     * @return Mage_Sales_Block_Order_Totals
-     */
-    protected function _initTotals()
-    {
-        parent::_initTotals();
-        return $this;
-    }
+	/**
+	 * Initialize order totals array
+	 *
+	 * @return Mage_Sales_Block_Order_Totals
+	 */
+	protected function _initTotals()
+	{
+		parent::_initTotals();
+		return $this;
+	}
 }
