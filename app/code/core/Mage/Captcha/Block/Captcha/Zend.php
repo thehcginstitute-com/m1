8207f73a3e22fe15acc8efd816a6b390860ae145
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-16 01:24:45 +0100

    refactoring

--- app/code/core/Mage/Captcha/Block/Captcha/Zend.php
+++ app/code/core/Mage/Captcha/Block/Captcha/Zend.php
@@ -25,57 +25,57 @@
  */
 class Mage_Captcha_Block_Captcha_Zend extends Mage_Core_Block_Template
 {
-    protected $_template = 'captcha/zend.phtml';
+	protected $_template = 'captcha/zend.phtml';
 
-    /**
-     * @var string
-     */
-    protected $_captcha;
+	/**
+	 * @var string
+	 */
+	protected $_captcha;
 
-    /**
-     * Returns template path
-     *
-     * @return string
-     */
-    function getTemplate()
-    {
-        return $this->getIsAjax() ? '' : $this->_template;
-    }
+	/**
+	 * Returns template path
+	 *
+	 * @return string
+	 */
+	function getTemplate()
+	{
+		return $this->getIsAjax() ? '' : $this->_template;
+	}
 
-    /**
-     * Returns URL to controller action which returns new captcha image
-     *
-     * @return string
-     */
-    function getRefreshUrl()
-    {
-        return Mage::getUrl(
-            Mage::app()->getStore()->isAdmin() ? 'adminhtml/refresh/refresh' : 'captcha/refresh',
-            ['_secure' => Mage::app()->getStore()->isCurrentlySecure()]
-        );
-    }
+	/**
+	 * Returns URL to controller action which returns new captcha image
+	 *
+	 * @return string
+	 */
+	function getRefreshUrl()
+	{
+		return Mage::getUrl(
+			Mage::app()->getStore()->isAdmin() ? 'adminhtml/refresh/refresh' : 'captcha/refresh',
+			['_secure' => Mage::app()->getStore()->isCurrentlySecure()]
+		);
+	}
 
-    /**
-     * Renders captcha HTML (if required)
-     *
-     * @return string
-     */
-    protected function _toHtml()
-    {
-        if (Mage::helper('captcha')->isEnabled() && $this->getCaptchaModel()->isRequired()) {
-            $this->getCaptchaModel()->generate();
-            return parent::_toHtml();
-        }
-        return '';
-    }
+	/**
+	 * Renders captcha HTML (if required)
+	 *
+	 * @return string
+	 */
+	protected function _toHtml()
+	{
+		if (Mage::helper('captcha')->isEnabled() && $this->getCaptchaModel()->isRequired()) {
+			$this->getCaptchaModel()->generate();
+			return parent::_toHtml();
+		}
+		return '';
+	}
 
-    /**
-     * Returns captcha model
-     *
-     * @return Mage_Captcha_Model_Interface
-     */
-    function getCaptchaModel()
-    {
-        return Mage::helper('captcha')->getCaptcha($this->getFormId());
-    }
+	/**
+	 * Returns captcha model
+	 *
+	 * @return Mage_Captcha_Model_Interface
+	 */
+	function getCaptchaModel()
+	{
+		return Mage::helper('captcha')->getCaptcha($this->getFormId());
+	}
 }
