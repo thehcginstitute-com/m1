d02e4ff5e2eec119fa27d8802a6aff68fe8d4800
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-16 01:05:54 +0100

    refactoring

--- app/code/core/Mage/Page/Block/Html.php
+++ app/code/core/Mage/Page/Block/Html.php
@@ -118,6 +118,7 @@ class Mage_Page_Block_Html extends Mage_Core_Block_Template
      * 2024-09-16 Dmitrii Fediuk https://upwork.com/fl/mage2pro
 	 * "The names of arguments in `<action method="<methodName>">` calls should match the `methodName`'s arguments":
 	 * https://github.com/thehcginstitute-com/m1/issues/680
+	 * @used-by Mage_Customer_Block_Account::__construct()
 	 */
 	final function setHeaderTitle(string $v):void {$this->_title = $v;}
 
