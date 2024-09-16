73a73aca594a087bdc4ab4cb8621940e05a36d70
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-16 01:07:59 +0100

    refactoring

--- app/code/core/Mage/Sales/Block/Order/History.php
+++ app/code/core/Mage/Sales/Block/Order/History.php
@@ -1,101 +1,75 @@
 <?php
 /**
- * OpenMage
- *
- * This source file is subject to the Open Software License (OSL 3.0)
- * that is bundled with this package in the file LICENSE.txt.
- * It is also available at https://opensource.org/license/osl-3-0-php
- *
- * @category   Mage
- * @package    Mage_Sales
- * @copyright  Copyright (c) 2006-2020 Magento, Inc. (https://www.magento.com)
- * @copyright  Copyright (c) 2020-2022 The OpenMage Contributors (https://www.openmage.org)
- * @license    https://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
- */
-
-/**
- * Sales order history block
- *
- * @category   Mage
- * @package    Mage_Sales
- * @author     Magento Core Team <core@magentocommerce.com>
- *
  * @method Mage_Sales_Model_Resource_Order_Collection getOrders()
  * @method $this setOrders(Mage_Sales_Model_Resource_Order_Collection $value)
  */
-class Mage_Sales_Block_Order_History extends Mage_Core_Block_Template
-{
-    function __construct()
-    {
-        parent::__construct();
-        $this->setTemplate('sales/order/history.phtml');
+class Mage_Sales_Block_Order_History extends Mage_Core_Block_Template {
+	function __construct() {
+		parent::__construct();
+		$this->setTemplate('sales/order/history.phtml');
+		$orders = Mage::getResourceModel('sales/order_collection')
+			->addFieldToSelect('*')
+			->addFieldToFilter('customer_id', Mage::getSingleton('customer/session')->getCustomer()->getId())
+			->addFieldToFilter('state', ['in' => Mage::getSingleton('sales/order_config')->getVisibleOnFrontStates()])
+			->setOrder('created_at', 'desc')
+		;
+		$this->setOrders($orders);
+		Mage::app()->getFrontController()->getAction()->getLayout()->getBlock('root')->setHeaderTitle('My Orders');
+	}
 
-        $orders = Mage::getResourceModel('sales/order_collection')
-            ->addFieldToSelect('*')
-            ->addFieldToFilter('customer_id', Mage::getSingleton('customer/session')->getCustomer()->getId())
-            ->addFieldToFilter('state', ['in' => Mage::getSingleton('sales/order_config')->getVisibleOnFrontStates()])
-            ->setOrder('created_at', 'desc')
-        ;
+	/**
+	 * @inheritDoc
+	 */
+	protected function _prepareLayout() {
+		parent::_prepareLayout();
+		$pager = $this->getLayout()->createBlock('page/html_pager', 'sales.order.history.pager')
+			->setCollection($this->getOrders())
+		;
+		$this->setChild('pager', $pager);
+		$this->getOrders()->load();
+		return $this;
+	}
 
-        $this->setOrders($orders);
+	/**
+	 * @return string
+	 */
+	function getPagerHtml()
+	{
+		return $this->getChildHtml('pager');
+	}
 
-        Mage::app()->getFrontController()->getAction()->getLayout()->getBlock('root')->setHeaderTitle(Mage::helper('sales')->__('My Orders'));
-    }
+	/**
+	 * @param Mage_Sales_Model_Order $order
+	 * @return string
+	 */
+	function getViewUrl($order)
+	{
+		return $this->getUrl('*/*/view', ['order_id' => $order->getId()]);
+	}
 
-    /**
-     * @inheritDoc
-     */
-    protected function _prepareLayout()
-    {
-        parent::_prepareLayout();
+	/**
+	 * @param Mage_Sales_Model_Order $order
+	 * @return string
+	 */
+	function getTrackUrl($order)
+	{
+		return $this->getUrl('*/*/track', ['order_id' => $order->getId()]);
+	}
 
-        $pager = $this->getLayout()->createBlock('page/html_pager', 'sales.order.history.pager')
-            ->setCollection($this->getOrders());
-        $this->setChild('pager', $pager);
-        $this->getOrders()->load();
-        return $this;
-    }
+	/**
+	 * @param Mage_Sales_Model_Order $order
+	 * @return string
+	 */
+	function getReorderUrl($order)
+	{
+		return $this->getUrl('*/*/reorder', ['order_id' => $order->getId()]);
+	}
 
-    /**
-     * @return string
-     */
-    function getPagerHtml()
-    {
-        return $this->getChildHtml('pager');
-    }
-
-    /**
-     * @param Mage_Sales_Model_Order $order
-     * @return string
-     */
-    function getViewUrl($order)
-    {
-        return $this->getUrl('*/*/view', ['order_id' => $order->getId()]);
-    }
-
-    /**
-     * @param Mage_Sales_Model_Order $order
-     * @return string
-     */
-    function getTrackUrl($order)
-    {
-        return $this->getUrl('*/*/track', ['order_id' => $order->getId()]);
-    }
-
-    /**
-     * @param Mage_Sales_Model_Order $order
-     * @return string
-     */
-    function getReorderUrl($order)
-    {
-        return $this->getUrl('*/*/reorder', ['order_id' => $order->getId()]);
-    }
-
-    /**
-     * @return string
-     */
-    function getBackUrl()
-    {
-        return $this->getUrl('customer/account/');
-    }
+	/**
+	 * @return string
+	 */
+	function getBackUrl()
+	{
+		return $this->getUrl('customer/account/');
+	}
 }
