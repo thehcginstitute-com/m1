ab7a151143bb858a5ff1d4c00df78dc444351ded
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-16 16:22:04 +0100

    refactoring

--- app/code/core/Mage/Adminhtml/Block/Sales/Order/Totals.php
+++ app/code/core/Mage/Adminhtml/Block/Sales/Order/Totals.php
@@ -1,27 +1,8 @@
 <?php
 /**
- * OpenMage
- *
- * This source file is subject to the Open Software License (OSL 3.0)
- * that is bundled with this package in the file LICENSE.txt.
- * It is also available at https://opensource.org/license/osl-3-0-php
- *
- * @category   Mage
- * @package    Mage_Adminhtml
- * @copyright  Copyright (c) 2006-2020 Magento, Inc. (https://www.magento.com)
- * @copyright  Copyright (c) 2022 The OpenMage Contributors (https://www.openmage.org)
- * @license    https://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ * 2024-09-16 Dmitrii Fediuk https://upwork.com/fl/mage2pro
  */
-
-/**
- * Adminhtml order totals block
- *
- * @category   Mage
- * @package    Mage_Adminhtml
- * @author     Magento Core Team <core@magentocommerce.com>
- */
-class Mage_Adminhtml_Block_Sales_Order_Totals extends Mage_Adminhtml_Block_Sales_Totals //Mage_Adminhtml_Block_Sales_Order_Abstract
-{
+class Mage_Adminhtml_Block_Sales_Order_Totals extends Mage_Adminhtml_Block_Sales_Totals {
 	/**
 	 * Initialize order totals array
 	 *
