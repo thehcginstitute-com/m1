e60788ff535e4b1681f314750eaad507dffd5494
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-16 01:05:00 +0100

    refactoring

--- app/code/core/Mage/Customer/Block/Account.php
+++ app/code/core/Mage/Customer/Block/Account.php
@@ -1,26 +1,5 @@
 <?php
-/**
- * OpenMage
- *
- * This source file is subject to the Open Software License (OSL 3.0)
- * that is bundled with this package in the file LICENSE.txt.
- * It is also available at https://opensource.org/license/osl-3-0-php
- *
- * @category   Mage
- * @package    Mage_Customer
- * @copyright  Copyright (c) 2006-2020 Magento, Inc. (https://www.magento.com)
- * @license    https://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
- */
-
-/**
- * Customer login block
- *
- * @category   Mage
- * @package    Mage_Customer
- * @author     Magento Core Team <core@magentocommerce.com>
- */
-class Mage_Customer_Block_Account extends Mage_Core_Block_Template
-{
+class Mage_Customer_Block_Account extends Mage_Core_Block_Template {
 	function __construct()
 	{
 		parent::__construct();
