58c7f077a7a134ccdd7c4cb1bcd5b8eb0df1c340
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-16 01:04:48 +0100

    refactoring

--- app/code/core/Mage/Customer/Block/Account.php
+++ app/code/core/Mage/Customer/Block/Account.php
@@ -21,10 +21,10 @@
  */
 class Mage_Customer_Block_Account extends Mage_Core_Block_Template
 {
-    function __construct()
-    {
-        parent::__construct();
-        $this->setTemplate('customer/account.phtml');
-        Mage::app()->getFrontController()->getAction()->getLayout()->getBlock('root')->setHeaderTitle(Mage::helper('customer')->__('My Account'));
-    }
+	function __construct()
+	{
+		parent::__construct();
+		$this->setTemplate('customer/account.phtml');
+		Mage::app()->getFrontController()->getAction()->getLayout()->getBlock('root')->setHeaderTitle(Mage::helper('customer')->__('My Account'));
+	}
 }
