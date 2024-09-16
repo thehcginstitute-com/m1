69130c0330f75b9312e58dd83a94694e34a630bc
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-16 23:23:50 +0100

    https://github.com/thehcginstitute-com/m1/issues/680

--- app/code/core/Mage/Page/Block/Html/Wrapper.php
+++ app/code/core/Mage/Page/Block/Html/Wrapper.php
@@ -98,6 +98,9 @@ class Mage_Page_Block_Html_Wrapper extends Mage_Core_Block_Abstract {
 
 	/**
 	 * 2024-09-17 Dmitrii Fediuk https://upwork.com/fl/mage2pro
+	 * @used-by https://github.com/thehcginstitute-com/m1/blob/2024-09-17--3/app/design/frontend/default/mobileshoppe/layout/checkout.xml#L361
+	 * @used-by https://github.com/thehcginstitute-com/m1/blob/2024-09-17--3/app/design/frontend/default/mobileshoppe/layout/newsletter.xml#L31
+	 * @used-by https://github.com/thehcginstitute-com/m1/blob/2024-09-17--3/app/design/frontend/default/mobileshoppe/layout/review.xml#L86
 	 */
 	final function markAsPotentiallyInvisible():void {$this->_potentiallyInvisible = true;}
 
