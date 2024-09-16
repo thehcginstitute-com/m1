d44083d2d317297b29b7ed8fd81104c74c0ed39e
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-16 01:43:37 +0100

    https://github.com/thehcginstitute-com/m1/issues/680

--- app/code/core/Mage/Captcha/Block/Captcha.php
+++ app/code/core/Mage/Captcha/Block/Captcha.php
@@ -40,10 +40,32 @@ class Mage_Captcha_Block_Captcha extends Mage_Core_Block_Abstract {
 
 	/**
 	 * 2024-09-16 Dmitrii Fediuk https://upwork.com/fl/mage2pro
+	 * @used-by https://github.com/thehcginstitute-com/m1/blob/2024-09-16--8/app/design/adminhtml/default/default/layout/captcha.xml#L12
+	 * @used-by https://github.com/thehcginstitute-com/m1/blob/2024-09-16--8/app/design/adminhtml/default/default/layout/captcha.xml#L21
+	 * @used-by https://github.com/thehcginstitute-com/m1/blob/2024-09-16--8/app/design/frontend/base/default/layout/captcha.xml#L16
+	 * @used-by https://github.com/thehcginstitute-com/m1/blob/2024-09-16--8/app/design/frontend/base/default/layout/captcha.xml#L30
+	 * @used-by https://github.com/thehcginstitute-com/m1/blob/2024-09-16--8/app/design/frontend/base/default/layout/captcha.xml#L44
+	 * @used-by https://github.com/thehcginstitute-com/m1/blob/2024-09-16--8/app/design/frontend/base/default/layout/captcha.xml#L58
+	 * @used-by https://github.com/thehcginstitute-com/m1/blob/2024-09-16--8/app/design/frontend/base/default/layout/captcha.xml#L70
+	 * @used-by https://github.com/thehcginstitute-com/m1/blob/2024-09-16--8/app/design/frontend/base/default/layout/captcha.xml#L78
+	 * @used-by https://github.com/thehcginstitute-com/m1/blob/2024-09-16--8/app/design/frontend/base/default/layout/captcha.xml#L92
+	 * @used-by https://github.com/thehcginstitute-com/m1/blob/2024-09-16--8/app/design/frontend/base/default/layout/captcha.xml#L106
+	 */
+	final function setImgWidth(int $v):void {$this[self::IMG_WIDTH] = $v;}
+
+	/**
+	 * 2024-09-16 Dmitrii Fediuk https://upwork.com/fl/mage2pro
 	 * @used-by self::setImgHeight()
 	 * @used-by Mage_Captcha_Block_Captcha_Zend::getImgHeight()
 	 */
 	const IMG_HEIGHT = 'img_height';
+
+	/**
+	 * 2024-09-16 Dmitrii Fediuk https://upwork.com/fl/mage2pro
+	 * @used-by self::setImgWidth()
+	 * @used-by Mage_Captcha_Block_Captcha_Zend::getImgWidth()
+	 */
+	const IMG_WIDTH = 'img_width';
 
 	/**
 	 * 2024-09-16 Dmitrii Fediuk https://upwork.com/fl/mage2pro
