9031469d6ac553241ce8d23cf72caa5edfd61de3
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-16 16:32:17 +0100

    refactoring

--- app/code/core/Mage/Tax/Block/Sales/Order/Tax.php
+++ app/code/core/Mage/Tax/Block/Sales/Order/Tax.php
@@ -1,319 +1,303 @@
 <?php
-/**
- * OpenMage
- *
- * This source file is subject to the Open Software License (OSL 3.0)
- * that is bundled with this package in the file LICENSE.txt.
- * It is also available at https://opensource.org/license/osl-3-0-php
- *
- * @category   Mage
- * @package    Mage_Tax
- * @copyright  Copyright (c) 2006-2020 Magento, Inc. (https://www.magento.com)
- * @copyright  Copyright (c) 2019-2022 The OpenMage Contributors (https://www.openmage.org)
- * @license    https://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
- */
-
-/**
- * Tax totals modification block. Can be used just as subblock of Mage_Sales_Block_Order_Totals
- */
+# 2024-09-16 Dmitrii Fediuk https://upwork.com/fl/mage2pro
 class Mage_Tax_Block_Sales_Order_Tax extends Mage_Core_Block_Template
 {
-    /**
-     * Tax configuration model
-     *
-     * @var Mage_Tax_Model_Config
-     */
-    protected $_config;
+	/**
+	 * Tax configuration model
+	 *
+	 * @var Mage_Tax_Model_Config
+	 */
+	protected $_config;
 
-    /**
-     * @var Mage_Sales_Model_Order
-     */
-    protected $_order;
+	/**
+	 * @var Mage_Sales_Model_Order
+	 */
+	protected $_order;
 
-    /**
-     * @var Mage_Sales_Model_Order_Invoice
-     */
-    protected $_source;
+	/**
+	 * @var Mage_Sales_Model_Order_Invoice
+	 */
+	protected $_source;
 
-    /**
-     * Initialize configuration object
-     */
-    protected function _construct()
-    {
-        $this->_config = Mage::getSingleton('tax/config');
-    }
+	/**
+	 * Initialize configuration object
+	 */
+	protected function _construct()
+	{
+		$this->_config = Mage::getSingleton('tax/config');
+	}
 
-    /**
-     * Check if we nedd display full tax total info
-     *
-     * @return bool
-     */
-    function displayFullSummary()
-    {
-        return $this->_config->displaySalesFullSummary($this->getOrder()->getStore());
-    }
+	/**
+	 * Check if we nedd display full tax total info
+	 *
+	 * @return bool
+	 */
+	function displayFullSummary()
+	{
+		return $this->_config->displaySalesFullSummary($this->getOrder()->getStore());
+	}
 
-    /**
-     * Get data (totals) source model
-     *
-     * @return Varien_Object
-     */
-    function getSource()
-    {
-        return $this->_source;
-    }
+	/**
+	 * Get data (totals) source model
+	 *
+	 * @return Varien_Object
+	 */
+	function getSource()
+	{
+		return $this->_source;
+	}
 
-    /**
-     * Initialize all order totals relates with tax
-     *
-     * @return $this
-     */
-    function initTotals()
-    {
-        /** @var Mage_Adminhtml_Block_Sales_Order_Invoice_Totals $parent */
-        $parent = $this->getParentBlock();
-        $this->_order   = $parent->getOrder();
-        $this->_source  = $parent->getSource();
+	/**
+	 * Initialize all order totals relates with tax
+	 *
+	 * @return $this
+	 */
+	function initTotals()
+	{
+		/** @var Mage_Adminhtml_Block_Sales_Order_Invoice_Totals $parent */
+		$parent = $this->getParentBlock();
+		$this->_order   = $parent->getOrder();
+		$this->_source  = $parent->getSource();
 
-        $store = $this->getStore();
-        $allowTax = ($this->_source->getTaxAmount() > 0) || ($this->_config->displaySalesZeroTax($store));
-        $grandTotal = (float) $this->_source->getGrandTotal();
-        if (!$grandTotal || ($allowTax && !$this->_config->displaySalesTaxWithGrandTotal($store))) {
-            $this->_addTax();
-        }
+		$store = $this->getStore();
+		$allowTax = ($this->_source->getTaxAmount() > 0) || ($this->_config->displaySalesZeroTax($store));
+		$grandTotal = (float) $this->_source->getGrandTotal();
+		if (!$grandTotal || ($allowTax && !$this->_config->displaySalesTaxWithGrandTotal($store))) {
+			$this->_addTax();
+		}
 
-        $this->_initSubtotal();
-        $this->_initShipping();
-        $this->_initDiscount();
-        $this->_initGrandTotal();
-        return $this;
-    }
+		$this->_initSubtotal();
+		$this->_initShipping();
+		$this->_initDiscount();
+		$this->_initGrandTotal();
+		return $this;
+	}
 
-    /**
-     * Add tax total string
-     *
-     * @param string $after
-     * @return $this
-     */
-    protected function _addTax($after = 'discount')
-    {
-        $taxTotal = new Varien_Object([
-            'code'      => 'tax',
-            'block_name' => $this->getNameInLayout()
-        ]);
-        $this->getParentBlock()->addTotal($taxTotal, $after);
-        return $this;
-    }
+	/**
+	 * Add tax total string
+	 *
+	 * @param string $after
+	 * @return $this
+	 */
+	protected function _addTax($after = 'discount')
+	{
+		$taxTotal = new Varien_Object([
+			'code'      => 'tax',
+			'block_name' => $this->getNameInLayout()
+		]);
+		$this->getParentBlock()->addTotal($taxTotal, $after);
+		return $this;
+	}
 
-    /**
-     * Get order store object
-     *
-     * @return Mage_Core_Model_Store
-     */
-    function getStore()
-    {
-        return $this->_order->getStore();
-    }
+	/**
+	 * Get order store object
+	 *
+	 * @return Mage_Core_Model_Store
+	 */
+	function getStore()
+	{
+		return $this->_order->getStore();
+	}
 
-    /**
-     * @return $this
-     */
-    protected function _initSubtotal()
-    {
-        $store  = $this->getStore();
-        $parent = $this->getParentBlock();
-        $subtotal = $parent->getTotal('subtotal');
-        if (!$subtotal) {
-            return $this;
-        }
-        if ($this->_config->displaySalesSubtotalBoth($store)) {
-            $subtotal       = (float) $this->_source->getSubtotal();
-            $baseSubtotal   = (float) $this->_source->getBaseSubtotal();
-            $subtotalIncl   = (float) $this->_source->getSubtotalInclTax();
-            $baseSubtotalIncl = (float) $this->_source->getBaseSubtotalInclTax();
+	/**
+	 * @return $this
+	 */
+	protected function _initSubtotal()
+	{
+		$store  = $this->getStore();
+		$parent = $this->getParentBlock();
+		$subtotal = $parent->getTotal('subtotal');
+		if (!$subtotal) {
+			return $this;
+		}
+		if ($this->_config->displaySalesSubtotalBoth($store)) {
+			$subtotal       = (float) $this->_source->getSubtotal();
+			$baseSubtotal   = (float) $this->_source->getBaseSubtotal();
+			$subtotalIncl   = (float) $this->_source->getSubtotalInclTax();
+			$baseSubtotalIncl = (float) $this->_source->getBaseSubtotalInclTax();
 
-            if (!$subtotalIncl || !$baseSubtotalIncl) {
-                //Calculate the subtotal if not set
-                $subtotalIncl = $subtotal + $this->_source->getTaxAmount()
-                    - $this->_source->getShippingTaxAmount();
-                $baseSubtotalIncl = $baseSubtotal + $this->_source->getBaseTaxAmount()
-                    - $this->_source->getBaseShippingTaxAmount();
+			if (!$subtotalIncl || !$baseSubtotalIncl) {
+				//Calculate the subtotal if not set
+				$subtotalIncl = $subtotal + $this->_source->getTaxAmount()
+					- $this->_source->getShippingTaxAmount();
+				$baseSubtotalIncl = $baseSubtotal + $this->_source->getBaseTaxAmount()
+					- $this->_source->getBaseShippingTaxAmount();
 
-                if ($this->_source instanceof Mage_Sales_Model_Order) {
-                    //Adjust the discount amounts for the base and well as the weee to display the right totals
-                    foreach ($this->_source->getAllItems() as $item) {
+				if ($this->_source instanceof Mage_Sales_Model_Order) {
+					//Adjust the discount amounts for the base and well as the weee to display the right totals
+					foreach ($this->_source->getAllItems() as $item) {
 						# 2024-02-05 Dmitrii Fediuk https://upwork.com/fl/mage2pro
 						# "Delete the unused `Mage_Weee` module": https://github.com/thehcginstitute-com/m1/issues/377
-                        $subtotalIncl += $item->getHiddenTaxAmount();
-                        $baseSubtotalIncl += $item->getBaseHiddenTaxAmount();
-                    }
-                }
-            }
+						$subtotalIncl += $item->getHiddenTaxAmount();
+						$baseSubtotalIncl += $item->getBaseHiddenTaxAmount();
+					}
+				}
+			}
 
-            $subtotalIncl = max(0, $subtotalIncl);
-            $baseSubtotalIncl = max(0, $baseSubtotalIncl);
-            $totalExcl = new Varien_Object([
-                'code'      => 'subtotal_excl',
-                'value'     => $subtotal,
-                'base_value' => $baseSubtotal,
-                'label'     => $this->__('Subtotal (Excl.Tax)')
-            ]);
-            $totalIncl = new Varien_Object([
-                'code'      => 'subtotal_incl',
-                'value'     => $subtotalIncl,
-                'base_value' => $baseSubtotalIncl,
-                'label'     => $this->__('Subtotal (Incl.Tax)')
-            ]);
-            $parent->addTotal($totalExcl, 'subtotal');
-            $parent->addTotal($totalIncl, 'subtotal_excl');
-            $parent->removeTotal('subtotal');
-        } elseif ($this->_config->displaySalesSubtotalInclTax($store)) {
-            $subtotalIncl   = (float) $this->_source->getSubtotalInclTax();
-            $baseSubtotalIncl = (float) $this->_source->getBaseSubtotalInclTax();
+			$subtotalIncl = max(0, $subtotalIncl);
+			$baseSubtotalIncl = max(0, $baseSubtotalIncl);
+			$totalExcl = new Varien_Object([
+				'code'      => 'subtotal_excl',
+				'value'     => $subtotal,
+				'base_value' => $baseSubtotal,
+				'label'     => $this->__('Subtotal (Excl.Tax)')
+			]);
+			$totalIncl = new Varien_Object([
+				'code'      => 'subtotal_incl',
+				'value'     => $subtotalIncl,
+				'base_value' => $baseSubtotalIncl,
+				'label'     => $this->__('Subtotal (Incl.Tax)')
+			]);
+			$parent->addTotal($totalExcl, 'subtotal');
+			$parent->addTotal($totalIncl, 'subtotal_excl');
+			$parent->removeTotal('subtotal');
+		} elseif ($this->_config->displaySalesSubtotalInclTax($store)) {
+			$subtotalIncl   = (float) $this->_source->getSubtotalInclTax();
+			$baseSubtotalIncl = (float) $this->_source->getBaseSubtotalInclTax();
 
-            if (!$subtotalIncl) {
-                $subtotalIncl = $this->_source->getSubtotal()
-                    + $this->_source->getTaxAmount()
-                    - $this->_source->getShippingTaxAmount();
-            }
-            if (!$baseSubtotalIncl) {
-                $baseSubtotalIncl = $this->_source->getBaseSubtotal()
-                    + $this->_source->getBaseTaxAmount()
-                    - $this->_source->getBaseShippingTaxAmount();
-            }
+			if (!$subtotalIncl) {
+				$subtotalIncl = $this->_source->getSubtotal()
+					+ $this->_source->getTaxAmount()
+					- $this->_source->getShippingTaxAmount();
+			}
+			if (!$baseSubtotalIncl) {
+				$baseSubtotalIncl = $this->_source->getBaseSubtotal()
+					+ $this->_source->getBaseTaxAmount()
+					- $this->_source->getBaseShippingTaxAmount();
+			}
 
-            $total = $parent->getTotal('subtotal');
-            if ($total) {
-                $total->setValue(max(0, $subtotalIncl));
-                $total->setBaseValue(max(0, $baseSubtotalIncl));
-            }
-        }
-        return $this;
-    }
+			$total = $parent->getTotal('subtotal');
+			if ($total) {
+				$total->setValue(max(0, $subtotalIncl));
+				$total->setBaseValue(max(0, $baseSubtotalIncl));
+			}
+		}
+		return $this;
+	}
 
-    /**
-     * @return $this
-     */
-    protected function _initShipping()
-    {
-        $store  = $this->getStore();
-        $parent = $this->getParentBlock();
-        $shipping = $parent->getTotal('shipping');
-        if (!$shipping) {
-            return $this;
-        }
+	/**
+	 * @return $this
+	 */
+	protected function _initShipping()
+	{
+		$store  = $this->getStore();
+		$parent = $this->getParentBlock();
+		$shipping = $parent->getTotal('shipping');
+		if (!$shipping) {
+			return $this;
+		}
 
-        if ($this->_config->displaySalesShippingBoth($store)) {
-            $shipping           = (float) $this->_source->getShippingAmount();
-            $baseShipping       = (float) $this->_source->getBaseShippingAmount();
-            $shippingIncl       = (float) $this->_source->getShippingInclTax();
-            if (!$shippingIncl) {
-                $shippingIncl   = $shipping + (float) $this->_source->getShippingTaxAmount();
-            }
-            $baseShippingIncl   = (float) $this->_source->getBaseShippingInclTax();
-            if (!$baseShippingIncl) {
-                $baseShippingIncl = $baseShipping + (float) $this->_source->getBaseShippingTaxAmount();
-            }
+		if ($this->_config->displaySalesShippingBoth($store)) {
+			$shipping           = (float) $this->_source->getShippingAmount();
+			$baseShipping       = (float) $this->_source->getBaseShippingAmount();
+			$shippingIncl       = (float) $this->_source->getShippingInclTax();
+			if (!$shippingIncl) {
+				$shippingIncl   = $shipping + (float) $this->_source->getShippingTaxAmount();
+			}
+			$baseShippingIncl   = (float) $this->_source->getBaseShippingInclTax();
+			if (!$baseShippingIncl) {
+				$baseShippingIncl = $baseShipping + (float) $this->_source->getBaseShippingTaxAmount();
+			}
 
-            $totalExcl = new Varien_Object([
-                'code'      => 'shipping',
-                'value'     => $shipping,
-                'base_value' => $baseShipping,
-                'label'     => $this->__('Shipping & Handling (Excl.Tax)')
-            ]);
-            $totalIncl = new Varien_Object([
-                'code'      => 'shipping_incl',
-                'value'     => $shippingIncl,
-                'base_value' => $baseShippingIncl,
-                'label'     => $this->__('Shipping & Handling (Incl.Tax)')
-            ]);
-            $parent->addTotal($totalExcl, 'shipping');
-            $parent->addTotal($totalIncl, 'shipping');
-        } elseif ($this->_config->displaySalesShippingInclTax($store)) {
-            $shippingIncl       = $this->_source->getShippingInclTax();
-            if (!$shippingIncl) {
-                $shippingIncl = $this->_source->getShippingAmount()
-                    + $this->_source->getShippingTaxAmount();
-            }
-            $baseShippingIncl   = $this->_source->getBaseShippingInclTax();
-            if (!$baseShippingIncl) {
-                $baseShippingIncl = $this->_source->getBaseShippingAmount()
-                    + $this->_source->getBaseShippingTaxAmount();
-            }
-            $total = $parent->getTotal('shipping');
-            if ($total) {
-                $total->setValue($shippingIncl);
-                $total->setBaseValue($baseShippingIncl);
-            }
-        }
-        return $this;
-    }
+			$totalExcl = new Varien_Object([
+				'code'      => 'shipping',
+				'value'     => $shipping,
+				'base_value' => $baseShipping,
+				'label'     => $this->__('Shipping & Handling (Excl.Tax)')
+			]);
+			$totalIncl = new Varien_Object([
+				'code'      => 'shipping_incl',
+				'value'     => $shippingIncl,
+				'base_value' => $baseShippingIncl,
+				'label'     => $this->__('Shipping & Handling (Incl.Tax)')
+			]);
+			$parent->addTotal($totalExcl, 'shipping');
+			$parent->addTotal($totalIncl, 'shipping');
+		} elseif ($this->_config->displaySalesShippingInclTax($store)) {
+			$shippingIncl       = $this->_source->getShippingInclTax();
+			if (!$shippingIncl) {
+				$shippingIncl = $this->_source->getShippingAmount()
+					+ $this->_source->getShippingTaxAmount();
+			}
+			$baseShippingIncl   = $this->_source->getBaseShippingInclTax();
+			if (!$baseShippingIncl) {
+				$baseShippingIncl = $this->_source->getBaseShippingAmount()
+					+ $this->_source->getBaseShippingTaxAmount();
+			}
+			$total = $parent->getTotal('shipping');
+			if ($total) {
+				$total->setValue($shippingIncl);
+				$total->setBaseValue($baseShippingIncl);
+			}
+		}
+		return $this;
+	}
 
-    protected function _initDiscount()
-    {
-    }
+	protected function _initDiscount()
+	{
+	}
 
-    /**
-     * @return $this
-     */
-    protected function _initGrandTotal()
-    {
-        $store  = $this->getStore();
-        $parent = $this->getParentBlock();
-        $grandototal = $parent->getTotal('grand_total');
-        if (!$grandototal || !(float)$this->_source->getGrandTotal()) {
-            return $this;
-        }
+	/**
+	 * @return $this
+	 */
+	protected function _initGrandTotal()
+	{
+		$store  = $this->getStore();
+		$parent = $this->getParentBlock();
+		$grandototal = $parent->getTotal('grand_total');
+		if (!$grandototal || !(float)$this->_source->getGrandTotal()) {
+			return $this;
+		}
 
-        if ($this->_config->displaySalesTaxWithGrandTotal($store)) {
-            $grandtotal         = $this->_source->getGrandTotal();
-            $baseGrandtotal     = $this->_source->getBaseGrandTotal();
-            $grandtotalExcl     = $grandtotal - $this->_source->getTaxAmount();
-            $baseGrandtotalExcl = $baseGrandtotal - $this->_source->getBaseTaxAmount();
-            $grandtotalExcl     = max($grandtotalExcl, 0);
-            $baseGrandtotalExcl = max($baseGrandtotalExcl, 0);
-            $totalExcl = new Varien_Object([
-                'code'      => 'grand_total',
-                'strong'    => true,
-                'value'     => $grandtotalExcl,
-                'base_value' => $baseGrandtotalExcl,
-                'label'     => $this->__('Grand Total (Excl.Tax)')
-            ]);
-            $totalIncl = new Varien_Object([
-                'code'      => 'grand_total_incl',
-                'strong'    => true,
-                'value'     => $grandtotal,
-                'base_value' => $baseGrandtotal,
-                'label'     => $this->__('Grand Total (Incl.Tax)')
-            ]);
-            $parent->addTotal($totalExcl, 'grand_total');
-            $this->_addTax('grand_total');
-            $parent->addTotal($totalIncl, 'tax');
-        }
-        return $this;
-    }
+		if ($this->_config->displaySalesTaxWithGrandTotal($store)) {
+			$grandtotal         = $this->_source->getGrandTotal();
+			$baseGrandtotal     = $this->_source->getBaseGrandTotal();
+			$grandtotalExcl     = $grandtotal - $this->_source->getTaxAmount();
+			$baseGrandtotalExcl = $baseGrandtotal - $this->_source->getBaseTaxAmount();
+			$grandtotalExcl     = max($grandtotalExcl, 0);
+			$baseGrandtotalExcl = max($baseGrandtotalExcl, 0);
+			$totalExcl = new Varien_Object([
+				'code'      => 'grand_total',
+				'strong'    => true,
+				'value'     => $grandtotalExcl,
+				'base_value' => $baseGrandtotalExcl,
+				'label'     => $this->__('Grand Total (Excl.Tax)')
+			]);
+			$totalIncl = new Varien_Object([
+				'code'      => 'grand_total_incl',
+				'strong'    => true,
+				'value'     => $grandtotal,
+				'base_value' => $baseGrandtotal,
+				'label'     => $this->__('Grand Total (Incl.Tax)')
+			]);
+			$parent->addTotal($totalExcl, 'grand_total');
+			$this->_addTax('grand_total');
+			$parent->addTotal($totalIncl, 'tax');
+		}
+		return $this;
+	}
 
-    /**
-     * @return Mage_Sales_Model_Order
-     */
-    function getOrder()
-    {
-        return $this->_order;
-    }
+	/**
+	 * @return Mage_Sales_Model_Order
+	 */
+	function getOrder()
+	{
+		return $this->_order;
+	}
 
-    /**
-     * @return mixed
-     */
-    function getLabelProperties()
-    {
-        return $this->getParentBlock()->getLabelProperties();
-    }
+	/**
+	 * @return mixed
+	 */
+	function getLabelProperties()
+	{
+		return $this->getParentBlock()->getLabelProperties();
+	}
 
-    /**
-     * @return mixed
-     */
-    function getValueProperties()
-    {
-        return $this->getParentBlock()->getValueProperties();
-    }
+	/**
+	 * @return mixed
+	 */
+	function getValueProperties()
+	{
+		return $this->getParentBlock()->getValueProperties();
+	}
 }
