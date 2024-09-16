3fe7dccadacb80ba68bf98d75c25510bdc26fb1c
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-16 02:36:21 +0100

    refactoring

--- app/code/core/Mage/Catalog/Block/Seo/Sitemap/Category.php
+++ app/code/core/Mage/Catalog/Block/Seo/Sitemap/Category.php
@@ -24,30 +24,30 @@
  */
 class Mage_Catalog_Block_Seo_Sitemap_Category extends Mage_Catalog_Block_Seo_Sitemap_Abstract
 {
-    /**
-     * Initialize categories collection
-     *
-     * @return $this
-     */
-    protected function _prepareLayout()
-    {
-        $helper = Mage::helper('catalog/category');
-        /** @var Mage_Catalog_Helper_Category $helper */
-        $collection = $helper->getStoreCategories('name', true, false);
-        $this->setCollection($collection);
-        return $this;
-    }
+	/**
+	 * Initialize categories collection
+	 *
+	 * @return $this
+	 */
+	protected function _prepareLayout()
+	{
+		$helper = Mage::helper('catalog/category');
+		/** @var Mage_Catalog_Helper_Category $helper */
+		$collection = $helper->getStoreCategories('name', true, false);
+		$this->setCollection($collection);
+		return $this;
+	}
 
-    /**
-     * Get item URL
-     *
-     * @param Mage_Catalog_Model_Category $category
-     * @return string
-     */
-    function getItemUrl($category)
-    {
-        $helper = Mage::helper('catalog/category');
-        /** @var Mage_Catalog_Helper_Category $helper */
-        return $helper->getCategoryUrl($category);
-    }
+	/**
+	 * Get item URL
+	 *
+	 * @param Mage_Catalog_Model_Category $category
+	 * @return string
+	 */
+	function getItemUrl($category)
+	{
+		$helper = Mage::helper('catalog/category');
+		/** @var Mage_Catalog_Helper_Category $helper */
+		return $helper->getCategoryUrl($category);
+	}
 }
