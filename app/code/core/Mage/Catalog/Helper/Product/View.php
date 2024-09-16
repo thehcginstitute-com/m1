433005c05ceb8a94adeb18f330a37fda2fcf9819
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-16 02:03:48 +0100

    refactoring

--- app/code/core/Mage/Catalog/Helper/Product/View.php
+++ app/code/core/Mage/Catalog/Helper/Product/View.php
@@ -22,128 +22,128 @@
  */
 class Mage_Catalog_Helper_Product_View extends Mage_Core_Helper_Abstract
 {
-    // List of exceptions throwable during prepareAndRender() method
-    public $ERR_NO_PRODUCT_LOADED = 1;
-    public $ERR_BAD_CONTROLLER_INTERFACE = 2;
+	// List of exceptions throwable during prepareAndRender() method
+	public $ERR_NO_PRODUCT_LOADED = 1;
+	public $ERR_BAD_CONTROLLER_INTERFACE = 2;
 
-    protected $_moduleName = 'Mage_Catalog';
+	protected $_moduleName = 'Mage_Catalog';
 
-    /**
-     * Inits layout for viewing product page
-     *
-     * @param Mage_Catalog_Model_Product $product
-     * @param Mage_Core_Controller_Front_Action $controller
-     *
-     * @return $this
-     * @throws Mage_Core_Model_Store_Exception
-     * @throws Mage_Core_Exception
-     */
-    function initProductLayout($product, $controller)
-    {
-        $design = Mage::getSingleton('catalog/design');
-        $settings = $design->getDesignSettings($product);
+	/**
+	 * Inits layout for viewing product page
+	 *
+	 * @param Mage_Catalog_Model_Product $product
+	 * @param Mage_Core_Controller_Front_Action $controller
+	 *
+	 * @return $this
+	 * @throws Mage_Core_Model_Store_Exception
+	 * @throws Mage_Core_Exception
+	 */
+	function initProductLayout($product, $controller)
+	{
+		$design = Mage::getSingleton('catalog/design');
+		$settings = $design->getDesignSettings($product);
 
-        if ($settings->getCustomDesign()) {
-            $design->applyCustomDesign($settings->getCustomDesign());
-        }
+		if ($settings->getCustomDesign()) {
+			$design->applyCustomDesign($settings->getCustomDesign());
+		}
 
-        $update = $controller->getLayout()->getUpdate();
-        $update->addHandle('default');
-        $controller->addActionLayoutHandles();
+		$update = $controller->getLayout()->getUpdate();
+		$update->addHandle('default');
+		$controller->addActionLayoutHandles();
 
-        $update->addHandle('PRODUCT_TYPE_' . $product->getTypeId());
-        $update->addHandle('PRODUCT_' . $product->getId());
-        $controller->loadLayoutUpdates();
+		$update->addHandle('PRODUCT_TYPE_' . $product->getTypeId());
+		$update->addHandle('PRODUCT_' . $product->getId());
+		$controller->loadLayoutUpdates();
 
-        // Apply custom layout update once layout is loaded
-        $layoutUpdates = $settings->getLayoutUpdates();
-        if ($layoutUpdates) {
-            if (is_array($layoutUpdates)) {
-                foreach ($layoutUpdates as $layoutUpdate) {
-                    $update->addUpdate($layoutUpdate);
-                }
-            }
-        }
+		// Apply custom layout update once layout is loaded
+		$layoutUpdates = $settings->getLayoutUpdates();
+		if ($layoutUpdates) {
+			if (is_array($layoutUpdates)) {
+				foreach ($layoutUpdates as $layoutUpdate) {
+					$update->addUpdate($layoutUpdate);
+				}
+			}
+		}
 
-        $controller->generateLayoutXml()->generateLayoutBlocks();
+		$controller->generateLayoutXml()->generateLayoutBlocks();
 
-        // Apply custom layout (page) template once the blocks are generated
-        if ($settings->getPageLayout()) {
-            $controller->getLayout()->helper('page/layout')->applyTemplate($settings->getPageLayout());
-        }
+		// Apply custom layout (page) template once the blocks are generated
+		if ($settings->getPageLayout()) {
+			$controller->getLayout()->helper('page/layout')->applyTemplate($settings->getPageLayout());
+		}
 
-        $currentCategory = Mage::registry('current_category');
-        /** @var Mage_Page_Block_Html $root */
-        $root = $controller->getLayout()->getBlock('root');
-        if ($root) {
-            $controllerClass = $controller->getFullActionName();
-            if ($controllerClass !== 'catalog-product-view') {
-                $root->addBodyClass('catalog-product-view');
-            }
-            $root->addBodyClass('product-' . $product->getUrlKey());
-            if ($currentCategory instanceof Mage_Catalog_Model_Category) {
-                $root->addBodyClass('categorypath-' . $currentCategory->getUrlPath())
-                    ->addBodyClass('category-' . $currentCategory->getUrlKey());
-            }
-        }
+		$currentCategory = Mage::registry('current_category');
+		/** @var Mage_Page_Block_Html $root */
+		$root = $controller->getLayout()->getBlock('root');
+		if ($root) {
+			$controllerClass = $controller->getFullActionName();
+			if ($controllerClass !== 'catalog-product-view') {
+				$root->addBodyClass('catalog-product-view');
+			}
+			$root->addBodyClass('product-' . $product->getUrlKey());
+			if ($currentCategory instanceof Mage_Catalog_Model_Category) {
+				$root->addBodyClass('categorypath-' . $currentCategory->getUrlPath())
+					->addBodyClass('category-' . $currentCategory->getUrlKey());
+			}
+		}
 
-        return $this;
-    }
+		return $this;
+	}
 
-    /**
-     * Prepares product view page - inits layout and all needed stuff
-     *
-     * $params can have all values as $params in Mage_Catalog_Helper_Product - initProduct().
-     * Plus following keys:
-     *   - 'buy_request' - Varien_Object holding buyRequest to configure product
-     *   - 'specify_options' - boolean, whether to show 'Specify options' message
-     *   - 'configure_mode' - boolean, whether we're in Configure-mode to edit product configuration
-     *
-     * @param int $productId
-     * @param Mage_Core_Controller_Front_Action $controller
-     * @param null|Varien_Object $params
-     *
-     * @return $this
-     */
-    function prepareAndRender($productId, $controller, $params = null)
-    {
-        // Prepare data
-        $productHelper = Mage::helper('catalog/product');
-        if (!$params) {
-            $params = new Varien_Object();
-        }
+	/**
+	 * Prepares product view page - inits layout and all needed stuff
+	 *
+	 * $params can have all values as $params in Mage_Catalog_Helper_Product - initProduct().
+	 * Plus following keys:
+	 *   - 'buy_request' - Varien_Object holding buyRequest to configure product
+	 *   - 'specify_options' - boolean, whether to show 'Specify options' message
+	 *   - 'configure_mode' - boolean, whether we're in Configure-mode to edit product configuration
+	 *
+	 * @param int $productId
+	 * @param Mage_Core_Controller_Front_Action $controller
+	 * @param null|Varien_Object $params
+	 *
+	 * @return $this
+	 */
+	function prepareAndRender($productId, $controller, $params = null)
+	{
+		// Prepare data
+		$productHelper = Mage::helper('catalog/product');
+		if (!$params) {
+			$params = new Varien_Object();
+		}
 
-        // Standard algorithm to prepare and render a product view page
-        $product = $productHelper->initProduct($productId, $controller, $params);
-        if (!$product) {
-            throw new Mage_Core_Exception($this->__('Product is not loaded'), $this->ERR_NO_PRODUCT_LOADED);
-        }
+		// Standard algorithm to prepare and render a product view page
+		$product = $productHelper->initProduct($productId, $controller, $params);
+		if (!$product) {
+			throw new Mage_Core_Exception($this->__('Product is not loaded'), $this->ERR_NO_PRODUCT_LOADED);
+		}
 
-        $buyRequest = $params->getBuyRequest();
-        if ($buyRequest) {
-            $productHelper->prepareProductOptions($product, $buyRequest);
-        }
+		$buyRequest = $params->getBuyRequest();
+		if ($buyRequest) {
+			$productHelper->prepareProductOptions($product, $buyRequest);
+		}
 
-        if ($params->hasConfigureMode()) {
-            $product->setConfigureMode($params->getConfigureMode());
-        }
+		if ($params->hasConfigureMode()) {
+			$product->setConfigureMode($params->getConfigureMode());
+		}
 
-        Mage::dispatchEvent('catalog_controller_product_view', ['product' => $product]);
+		Mage::dispatchEvent('catalog_controller_product_view', ['product' => $product]);
 
-        if ($params->getSpecifyOptions()) {
-            $notice = $product->getTypeInstance(true)->getSpecifyOptionMessage();
-            Mage::getSingleton('catalog/session')->addNotice($notice);
-        }
+		if ($params->getSpecifyOptions()) {
+			$notice = $product->getTypeInstance(true)->getSpecifyOptionMessage();
+			Mage::getSingleton('catalog/session')->addNotice($notice);
+		}
 
-        Mage::getSingleton('catalog/session')->setLastViewedProductId($product->getId());
+		Mage::getSingleton('catalog/session')->setLastViewedProductId($product->getId());
 
-        $this->initProductLayout($product, $controller);
+		$this->initProductLayout($product, $controller);
 
 		# 2024-02-05 Dmitrii Fediuk https://upwork.com/fl/mage2pro
 		# "Delete the unused `Mage_Tag` module": https://github.com/thehcginstitute-com/m1/issues/372
-        $controller->initLayoutMessages(['catalog/session', 'checkout/session'])
-            ->renderLayout();
+		$controller->initLayoutMessages(['catalog/session', 'checkout/session'])
+			->renderLayout();
 
-        return $this;
-    }
+		return $this;
+	}
 }
