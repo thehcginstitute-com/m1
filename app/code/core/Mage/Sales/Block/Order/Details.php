0d9e14fd0a7131b962ec5fd302bf3b5d0c4f63be
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-16 01:06:03 +0100

    refactoring

--- app/code/core/Mage/Sales/Block/Order/Details.php
+++ app/code/core/Mage/Sales/Block/Order/Details.php
@@ -22,35 +22,35 @@
  */
 class Mage_Sales_Block_Order_Details extends Mage_Core_Block_Template
 {
-    function __construct()
-    {
-        parent::__construct();
-        $this->setTemplate('sales/order/details.phtml');
-        $this->setOrder(Mage::getModel('sales/order')->load($this->getRequest()->getParam('order_id')));
-        Mage::registry('action')->getLayout()->getBlock('root')->setHeaderTitle(Mage::helper('sales')->__('Order Details'));
-    }
+	function __construct()
+	{
+		parent::__construct();
+		$this->setTemplate('sales/order/details.phtml');
+		$this->setOrder(Mage::getModel('sales/order')->load($this->getRequest()->getParam('order_id')));
+		Mage::registry('action')->getLayout()->getBlock('root')->setHeaderTitle(Mage::helper('sales')->__('Order Details'));
+	}
 
-    /**
-     * @return string
-     */
-    function getBackUrl()
-    {
-        return Mage::getUrl('*/*/history');
-    }
+	/**
+	 * @return string
+	 */
+	function getBackUrl()
+	{
+		return Mage::getUrl('*/*/history');
+	}
 
-    /**
-     * @return mixed
-     */
-    function getInvoices()
-    {
-        return Mage::getResourceModel('sales/invoice_collection')->setOrderFilter($this->getOrder()->getId())->load();
-    }
+	/**
+	 * @return mixed
+	 */
+	function getInvoices()
+	{
+		return Mage::getResourceModel('sales/invoice_collection')->setOrderFilter($this->getOrder()->getId())->load();
+	}
 
-    /**
-     * @return string
-     */
-    function getPrintUrl()
-    {
-        return Mage::getUrl('*/*/print');
-    }
+	/**
+	 * @return string
+	 */
+	function getPrintUrl()
+	{
+		return Mage::getUrl('*/*/print');
+	}
 }
