27173fbf5531d6fefe77e7456ad3d75ac3d5f102
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-16 16:00:04 +0100

    refactoring

--- app/code/core/Mage/Sales/Block/Order/Invoice/Totals.php
+++ app/code/core/Mage/Sales/Block/Order/Invoice/Totals.php
@@ -20,54 +20,54 @@
  */
 class Mage_Sales_Block_Order_Invoice_Totals extends Mage_Sales_Block_Order_Totals
 {
-    protected $_invoice = null;
+	protected $_invoice = null;
 
-    /**
-     * @return mixed|null
-     */
-    function getInvoice()
-    {
-        if ($this->_invoice === null) {
-            if ($this->hasData('invoice')) {
-                $this->_invoice = $this->_getData('invoice');
-            } elseif (Mage::registry('current_invoice')) {
-                $this->_invoice = Mage::registry('current_invoice');
-            } elseif ($this->getParentBlock()->getInvoice()) {
-                $this->_invoice = $this->getParentBlock()->getInvoice();
-            }
-        }
-        return $this->_invoice;
-    }
+	/**
+	 * @return mixed|null
+	 */
+	function getInvoice()
+	{
+		if ($this->_invoice === null) {
+			if ($this->hasData('invoice')) {
+				$this->_invoice = $this->_getData('invoice');
+			} elseif (Mage::registry('current_invoice')) {
+				$this->_invoice = Mage::registry('current_invoice');
+			} elseif ($this->getParentBlock()->getInvoice()) {
+				$this->_invoice = $this->getParentBlock()->getInvoice();
+			}
+		}
+		return $this->_invoice;
+	}
 
-    /**
-     * @param Mage_Sales_Model_Order_Invoice $invoice
-     * @return $this
-     */
-    function setInvoice($invoice)
-    {
-        $this->_invoice = $invoice;
-        return $this;
-    }
+	/**
+	 * @param Mage_Sales_Model_Order_Invoice $invoice
+	 * @return $this
+	 */
+	function setInvoice($invoice)
+	{
+		$this->_invoice = $invoice;
+		return $this;
+	}
 
-    /**
-     * Get totals source object
-     *
-     * @return Mage_Sales_Model_Order
-     */
-    function getSource()
-    {
-        return $this->getInvoice();
-    }
+	/**
+	 * Get totals source object
+	 *
+	 * @return Mage_Sales_Model_Order
+	 */
+	function getSource()
+	{
+		return $this->getInvoice();
+	}
 
-    /**
-     * Initialize order totals array
-     *
-     * @return Mage_Sales_Block_Order_Totals
-     */
-    protected function _initTotals()
-    {
-        parent::_initTotals();
-        $this->removeTotal('base_grandtotal');
-        return $this;
-    }
+	/**
+	 * Initialize order totals array
+	 *
+	 * @return Mage_Sales_Block_Order_Totals
+	 */
+	protected function _initTotals()
+	{
+		parent::_initTotals();
+		$this->removeTotal('base_grandtotal');
+		return $this;
+	}
 }
