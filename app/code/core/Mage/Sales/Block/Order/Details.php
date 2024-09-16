67f2295289d11a4cba7bc52dd3bf18fc216f693a
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-16 01:06:19 +0100

    refactoring

--- app/code/core/Mage/Sales/Block/Order/Details.php
+++ app/code/core/Mage/Sales/Block/Order/Details.php
@@ -1,33 +1,10 @@
 <?php
-/**
- * OpenMage
- *
- * This source file is subject to the Open Software License (OSL 3.0)
- * that is bundled with this package in the file LICENSE.txt.
- * It is also available at https://opensource.org/license/osl-3-0-php
- *
- * @category   Mage
- * @package    Mage_Sales
- * @copyright  Copyright (c) 2006-2020 Magento, Inc. (https://www.magento.com)
- * @copyright  Copyright (c) 2020-2022 The OpenMage Contributors (https://www.openmage.org)
- * @license    https://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
- */
-
-/**
- * Sales order details block
- *
- * @category   Mage
- * @package    Mage_Sales
- * @author     Magento Core Team <core@magentocommerce.com>
- */
-class Mage_Sales_Block_Order_Details extends Mage_Core_Block_Template
-{
-	function __construct()
-	{
+class Mage_Sales_Block_Order_Details extends Mage_Core_Block_Template {
+	function __construct() {
 		parent::__construct();
 		$this->setTemplate('sales/order/details.phtml');
 		$this->setOrder(Mage::getModel('sales/order')->load($this->getRequest()->getParam('order_id')));
-		Mage::registry('action')->getLayout()->getBlock('root')->setHeaderTitle(Mage::helper('sales')->__('Order Details'));
+		Mage::registry('action')->getLayout()->getBlock('root')->setHeaderTitle('Order Details');
 	}
 
 	/**
