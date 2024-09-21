a62333a12e9be24b5a46cd09f9063c8fb359bdca
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-21 06:21:00 +0100

    refactoring

--- app/code/core/Mage/Wishlist/Block/Customer/Wishlist/Item/Column/Comment.php
+++ app/code/core/Mage/Wishlist/Block/Customer/Wishlist/Item/Column/Comment.php
@@ -1,27 +1,6 @@
 <?php
-/**
- * OpenMage
- *
- * This source file is subject to the Open Software License (OSL 3.0)
- * that is bundled with this package in the file LICENSE.txt.
- * It is also available at https://opensource.org/license/osl-3-0-php
- *
- * @category   Mage
- * @package    Mage_Wishlist
- * @copyright  Copyright (c) 2006-2020 Magento, Inc. (https://www.magento.com)
- * @copyright  Copyright (c) 2020-2022 The OpenMage Contributors (https://www.openmage.org)
- * @license    https://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
- */
-
-/**
- * Wishlist block customer item cart column
- *
- * @category   Mage
- * @package    Mage_Wishlist
- * @author     Magento Core Team <core@magentocommerce.com>
- */
-class Mage_Wishlist_Block_Customer_Wishlist_Item_Column_Comment extends Mage_Wishlist_Block_Customer_Wishlist_Item_Column
-{
+# 2024-09-21 Dmitrii Fediuk https://upwork.com/fl/mage2pro
+class Mage_Wishlist_Block_Customer_Wishlist_Item_Column_Comment extends Mage_Wishlist_Block_Customer_Wishlist_Item_Column {
 	/**
 	 * Retrieve column javascript code
 	 *
