f66486fc056c69927dd17f59886a9568f05876d5
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-21 05:55:48 +0100

    https://github.com/thehcginstitute-com/m1/issues/680

--- app/code/core/Mage/Page/Block/Html/Head.php
+++ app/code/core/Mage/Page/Block/Html/Head.php
@@ -452,6 +452,7 @@ class Mage_Page_Block_Html_Head extends Mage_Core_Block_Template {
 	 * "The names of arguments in `<action method="<methodName>">` calls should match the `methodName`'s arguments":
 	 * https://github.com/thehcginstitute-com/m1/issues/680
 	 * @used-by https://github.com/thehcginstitute-com/m1/blob/2024-09-21/app/design/adminhtml/default/default/layout/catalog.xml#L9
+	 * @used-by https://github.com/thehcginstitute-com/m1/blob/2024-09-21/app/design/adminhtml/default/default/layout/main.xml#L13
 	 */
 	function setTitle(string $v):self {
 		$this->_data['title'] = Mage::getStoreConfig('design/head/title_prefix') . ' ' . $v
