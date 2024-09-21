7cb2943a6c00cabc5931bb17fe519507a91bdfff
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-21 06:02:15 +0100

    refactoring

--- app/code/core/Mage/Page/Block/Template/Container.php
+++ app/code/core/Mage/Page/Block/Template/Container.php
@@ -1,33 +1,5 @@
 <?php
-/**
- * OpenMage
- *
- * This source file is subject to the Open Software License (OSL 3.0)
- * that is bundled with this package in the file LICENSE.txt.
- * It is also available at https://opensource.org/license/osl-3-0-php
- *
- * @category   Mage
- * @package    Mage_Page
- * @copyright  Copyright (c) 2006-2020 Magento, Inc. (https://www.magento.com)
- * @copyright  Copyright (c) 2020-2022 The OpenMage Contributors (https://www.openmage.org)
- * @license    https://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
- */
-
-/**
- * Abstract container block with header
- *
- * @category   Mage
- * @package    Mage_Page
- * @author     Magento Core Team <core@magentocommerce.com>
- */
-class Mage_Page_Block_Template_Container extends Mage_Core_Block_Template
-{
-	/**
-	 * Set default template
-	 *
-	 */
-	protected function _construct()
-	{
-		$this->setTemplate('page/template/container.phtml');
-	}
+# 2024-09-21 Dmitrii Fediuk https://upwork.com/fl/mage2pro
+class Mage_Page_Block_Template_Container extends Mage_Core_Block_Template {
+	protected function _construct() {$this->setTemplate('page/template/container.phtml');}
 }
