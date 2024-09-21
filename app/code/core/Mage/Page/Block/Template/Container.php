eb916d5f72f2d2b208f0b8a4f64a3a037b048f9e
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-21 06:07:31 +0100

    https://github.com/thehcginstitute-com/m1/issues/680

--- app/code/core/Mage/Page/Block/Template/Container.php
+++ app/code/core/Mage/Page/Block/Template/Container.php
@@ -7,4 +7,10 @@ class Mage_Page_Block_Template_Container extends Mage_Core_Block_Template {
 	 * @used-by Varien_Object::__construct()
 	 */
 	protected function _construct() {$this->setTemplate('page/template/container.phtml');}
+
+	/**
+	 * 2024-09-21 Dmitrii Fediuk https://upwork.com/fl/mage2pro
+	 * @const string
+	 */
+	private static $TITLE = 'title';
 }
