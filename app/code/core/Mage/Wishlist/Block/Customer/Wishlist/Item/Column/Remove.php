d114d9366450c89ce8e6bad099eff188ae4d9fd4
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-21 06:21:53 +0100

    refactoring

--- app/code/core/Mage/Wishlist/Block/Customer/Wishlist/Item/Column/Remove.php
+++ app/code/core/Mage/Wishlist/Block/Customer/Wishlist/Item/Column/Remove.php
@@ -21,21 +21,21 @@
  */
 class Mage_Wishlist_Block_Customer_Wishlist_Item_Column_Remove extends Mage_Wishlist_Block_Customer_Wishlist_Item_Column
 {
-    /**
-     * Retrieve block javascript
-     *
-     * @return string
-     */
-    function getJs()
-    {
-        return parent::getJs() . "
-        function confirmRemoveWishlistItem() {
-            return confirm('"
-            . Mage::helper('core')->jsQuoteEscape(
-                $this->__('Are you sure you want to remove this product from your wishlist?')
-            )
-            . "');
-        }
-        ";
-    }
+	/**
+	 * Retrieve block javascript
+	 *
+	 * @return string
+	 */
+	function getJs()
+	{
+		return parent::getJs() . "
+		function confirmRemoveWishlistItem() {
+			return confirm('"
+			. Mage::helper('core')->jsQuoteEscape(
+				$this->__('Are you sure you want to remove this product from your wishlist?')
+			)
+			. "');
+		}
+		";
+	}
 }
