d2a070015f1300786bd8855dcb76ec5c354084f5
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-21 06:22:06 +0100

    refactoring

--- app/code/core/Mage/Wishlist/Block/Customer/Wishlist/Item/Column/Image.php
+++ app/code/core/Mage/Wishlist/Block/Customer/Wishlist/Item/Column/Image.php
@@ -1,5 +1,3 @@
 <?php
 # 2024-09-21 Dmitrii Fediuk https://upwork.com/fl/mage2pro
-class Mage_Wishlist_Block_Customer_Wishlist_Item_Column_Image extends Mage_Wishlist_Block_Customer_Wishlist_Item_Column
-{
-}
+class Mage_Wishlist_Block_Customer_Wishlist_Item_Column_Image extends Mage_Wishlist_Block_Customer_Wishlist_Item_Column {}
--- app/code/core/Mage/Wishlist/Block/Customer/Wishlist/Item/Column/Remove.php
+++ app/code/core/Mage/Wishlist/Block/Customer/Wishlist/Item/Column/Remove.php
@@ -1,26 +1,6 @@
 <?php
-/**
- * OpenMage
- *
- * This source file is subject to the Open Software License (OSL 3.0)
- * that is bundled with this package in the file LICENSE.txt.
- * It is also available at https://opensource.org/license/osl-3-0-php
- *
- * @category   Mage
- * @package    Mage_Wishlist
- * @copyright  Copyright (c) 2006-2020 Magento, Inc. (https://www.magento.com)
- * @license    https://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
- */
-
-/**
- * Delete item column in customer wishlist table
- *
- * @category   Mage
- * @package    Mage_Wishlist
- * @author     Magento Core Team <core@magentocommerce.com>
- */
-class Mage_Wishlist_Block_Customer_Wishlist_Item_Column_Remove extends Mage_Wishlist_Block_Customer_Wishlist_Item_Column
-{
+# 2024-09-21 Dmitrii Fediuk https://upwork.com/fl/mage2pro
+class Mage_Wishlist_Block_Customer_Wishlist_Item_Column_Remove extends Mage_Wishlist_Block_Customer_Wishlist_Item_Column {
 	/**
 	 * Retrieve block javascript
 	 *
