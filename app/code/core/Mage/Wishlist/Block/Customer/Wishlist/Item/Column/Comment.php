716b48f4a62ce06046477a601437ef41ce01d29b
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-21 06:20:40 +0100

    refactoring

--- app/code/core/Mage/Wishlist/Block/Customer/Wishlist/Item/Column/Comment.php
+++ app/code/core/Mage/Wishlist/Block/Customer/Wishlist/Item/Column/Comment.php
@@ -22,24 +22,24 @@
  */
 class Mage_Wishlist_Block_Customer_Wishlist_Item_Column_Comment extends Mage_Wishlist_Block_Customer_Wishlist_Item_Column
 {
-    /**
-     * Retrieve column javascript code
-     *
-     * @return string
-     */
-    function getJs()
-    {
-        /** @var Mage_Wishlist_Helper_Data $helper */
-        $helper = $this->helper('wishlist');
+	/**
+	 * Retrieve column javascript code
+	 *
+	 * @return string
+	 */
+	function getJs()
+	{
+		/** @var Mage_Wishlist_Helper_Data $helper */
+		$helper = $this->helper('wishlist');
 
-        return parent::getJs() . "
-        function focusComment(obj) {
-            if( obj.value == '" . $helper->defaultCommentString() . "' ) {
-                obj.value = '';
-            } else if( obj.value == '' ) {
-                obj.value = '" . $helper->defaultCommentString() . "';
-            }
-        }
-        ";
-    }
+		return parent::getJs() . "
+		function focusComment(obj) {
+			if( obj.value == '" . $helper->defaultCommentString() . "' ) {
+				obj.value = '';
+			} else if( obj.value == '' ) {
+				obj.value = '" . $helper->defaultCommentString() . "';
+			}
+		}
+		";
+	}
 }
