0617e411b59d2e5bc873083847b24c18943df560
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-21 05:49:49 +0100

    refactoring

--- app/code/core/Mage/Core/Block/Html/Select.php
+++ app/code/core/Mage/Core/Block/Html/Select.php
@@ -30,230 +30,230 @@
  */
 class Mage_Core_Block_Html_Select extends Mage_Core_Block_Abstract
 {
-    protected $_options = [];
+	protected $_options = [];
 
-    /**
-     * Get options of the element
-     *
-     * @return array
-     */
-    function getOptions()
-    {
-        return $this->_options;
-    }
+	/**
+	 * Get options of the element
+	 *
+	 * @return array
+	 */
+	function getOptions()
+	{
+		return $this->_options;
+	}
 
-    /**
-     * Set options for the HTML select
-     *
-     * @param array $options
-     * @return $this
-     */
-    function setOptions($options)
-    {
-        $this->_options = $options;
-        return $this;
-    }
+	/**
+	 * Set options for the HTML select
+	 *
+	 * @param array $options
+	 * @return $this
+	 */
+	function setOptions($options)
+	{
+		$this->_options = $options;
+		return $this;
+	}
 
-    /**
-     * Add an option to HTML select
-     *
-     * @param string $value  HTML value
-     * @param string $label  HTML label
-     * @param array  $params HTML attributes
-     * @return $this
-     */
-    function addOption($value, $label, $params = [])
-    {
-        $this->_options[] = ['value' => $value, 'label' => $label, 'params' => $params];
-        return $this;
-    }
+	/**
+	 * Add an option to HTML select
+	 *
+	 * @param string $value  HTML value
+	 * @param string $label  HTML label
+	 * @param array  $params HTML attributes
+	 * @return $this
+	 */
+	function addOption($value, $label, $params = [])
+	{
+		$this->_options[] = ['value' => $value, 'label' => $label, 'params' => $params];
+		return $this;
+	}
 
-    /**
-     * Set element's HTML ID
-     *
-     * @param string $id ID
-     * @return $this
-     */
-    function setId($id)
-    {
-        $this->setData('id', $id);
-        return $this;
-    }
+	/**
+	 * Set element's HTML ID
+	 *
+	 * @param string $id ID
+	 * @return $this
+	 */
+	function setId($id)
+	{
+		$this->setData('id', $id);
+		return $this;
+	}
 
-    /**
-     * Set element's CSS class
-     *
-     * @param string $class Class
-     * @return $this
-     */
-    function setClass($class)
-    {
-        $this->setData('class', $class);
-        return $this;
-    }
+	/**
+	 * Set element's CSS class
+	 *
+	 * @param string $class Class
+	 * @return $this
+	 */
+	function setClass($class)
+	{
+		$this->setData('class', $class);
+		return $this;
+	}
 
-    /**
-     * Set element's HTML title
-     *
-     * @param string $title Title
-     * @return $this
-     */
-    function setTitle($title)
-    {
-        $this->setData('title', $title);
-        return $this;
-    }
+	/**
+	 * Set element's HTML title
+	 *
+	 * @param string $title Title
+	 * @return $this
+	 */
+	function setTitle($title)
+	{
+		$this->setData('title', $title);
+		return $this;
+	}
 
-    /**
-     * HTML ID of the element
-     *
-     * @return string
-     */
-    function getId()
-    {
-        return $this->getData('id');
-    }
+	/**
+	 * HTML ID of the element
+	 *
+	 * @return string
+	 */
+	function getId()
+	{
+		return $this->getData('id');
+	}
 
-    /**
-     * CSS class of the element
-     *
-     * @return string
-     */
-    function getClass()
-    {
-        return $this->getData('class');
-    }
+	/**
+	 * CSS class of the element
+	 *
+	 * @return string
+	 */
+	function getClass()
+	{
+		return $this->getData('class');
+	}
 
-    /**
-     * Returns HTML title of the element
-     *
-     * @return string
-     */
-    function getTitle()
-    {
-        return $this->getData('title');
-    }
+	/**
+	 * Returns HTML title of the element
+	 *
+	 * @return string
+	 */
+	function getTitle()
+	{
+		return $this->getData('title');
+	}
 
-    /**
-     * Render HTML
-     *
-     * @return string
-     */
-    protected function _toHtml()
-    {
-        if (!$this->_beforeToHtml()) {
-            return '';
-        }
+	/**
+	 * Render HTML
+	 *
+	 * @return string
+	 */
+	protected function _toHtml()
+	{
+		if (!$this->_beforeToHtml()) {
+			return '';
+		}
 
-        $html = '<select name="' . $this->getName() . '" id="' . $this->getId() . '" class="'
-            . $this->getClass() . '" title="' . $this->getTitle() . '" ' . $this->getExtraParams() . '>';
-        $values = $this->getValue();
+		$html = '<select name="' . $this->getName() . '" id="' . $this->getId() . '" class="'
+			. $this->getClass() . '" title="' . $this->getTitle() . '" ' . $this->getExtraParams() . '>';
+		$values = $this->getValue();
 
-        if (!is_array($values)) {
-            if (!is_null($values)) {
-                $values = [$values];
-            } else {
-                $values = [];
-            }
-        }
+		if (!is_array($values)) {
+			if (!is_null($values)) {
+				$values = [$values];
+			} else {
+				$values = [];
+			}
+		}
 
-        $isArrayOption = true;
-        foreach ($this->getOptions() as $key => $option) {
-            if ($isArrayOption && is_array($option)) {
-                $value  = $option['value'];
-                $label  = (string)$option['label'];
-                $params = (!empty($option['params'])) ? $option['params'] : [];
-            } else {
-                $value = (string)$key;
-                $label = (string)$option;
-                $isArrayOption = false;
-                $params = [];
-            }
+		$isArrayOption = true;
+		foreach ($this->getOptions() as $key => $option) {
+			if ($isArrayOption && is_array($option)) {
+				$value  = $option['value'];
+				$label  = (string)$option['label'];
+				$params = (!empty($option['params'])) ? $option['params'] : [];
+			} else {
+				$value = (string)$key;
+				$label = (string)$option;
+				$isArrayOption = false;
+				$params = [];
+			}
 
-            if (is_array($value)) {
-                $html .= '<optgroup label="' . $label . '">';
-                foreach ($value as $keyGroup => $optionGroup) {
-                    if (!is_array($optionGroup)) {
-                        $optionGroup = [
-                            'value' => $keyGroup,
-                            'label' => $optionGroup
-                        ];
-                    }
-                    $html .= $this->_optionToHtml(
-                        $optionGroup,
-                        in_array($optionGroup['value'], $values)
-                    );
-                }
-                $html .= '</optgroup>';
-            } else {
-                $html .= $this->_optionToHtml(
-                    [
-                        'value' => $value,
-                        'label' => $label,
-                        'params' => $params
-                    ],
-                    in_array($value, $values)
-                );
-            }
-        }
-        $html .= '</select>';
-        return $html;
-    }
+			if (is_array($value)) {
+				$html .= '<optgroup label="' . $label . '">';
+				foreach ($value as $keyGroup => $optionGroup) {
+					if (!is_array($optionGroup)) {
+						$optionGroup = [
+							'value' => $keyGroup,
+							'label' => $optionGroup
+						];
+					}
+					$html .= $this->_optionToHtml(
+						$optionGroup,
+						in_array($optionGroup['value'], $values)
+					);
+				}
+				$html .= '</optgroup>';
+			} else {
+				$html .= $this->_optionToHtml(
+					[
+						'value' => $value,
+						'label' => $label,
+						'params' => $params
+					],
+					in_array($value, $values)
+				);
+			}
+		}
+		$html .= '</select>';
+		return $html;
+	}
 
-    /**
-     * Return option HTML node
-     *
-     * @param array $option
-     * @param bool $selected
-     * @return string
-     */
-    protected function _optionToHtml($option, $selected = false)
-    {
-        $selectedHtml = $selected ? ' selected="selected"' : '';
-        if ($this->getIsRenderToJsTemplate() === true) {
-            $selectedHtml .= ' #{option_extra_attr_' . self::calcOptionHash($option['value']) . '}';
-        }
+	/**
+	 * Return option HTML node
+	 *
+	 * @param array $option
+	 * @param bool $selected
+	 * @return string
+	 */
+	protected function _optionToHtml($option, $selected = false)
+	{
+		$selectedHtml = $selected ? ' selected="selected"' : '';
+		if ($this->getIsRenderToJsTemplate() === true) {
+			$selectedHtml .= ' #{option_extra_attr_' . self::calcOptionHash($option['value']) . '}';
+		}
 
-        $params = '';
-        if (!empty($option['params']) && is_array($option['params'])) {
-            foreach ($option['params'] as $key => $value) {
-                if (is_array($value)) {
-                    foreach ($value as $keyMulti => $valueMulti) {
-                        $params .= sprintf(' %s="%s" ', $keyMulti, $valueMulti);
-                    }
-                } else {
-                    $params .= sprintf(' %s="%s" ', $key, $value);
-                }
-            }
-        }
+		$params = '';
+		if (!empty($option['params']) && is_array($option['params'])) {
+			foreach ($option['params'] as $key => $value) {
+				if (is_array($value)) {
+					foreach ($value as $keyMulti => $valueMulti) {
+						$params .= sprintf(' %s="%s" ', $keyMulti, $valueMulti);
+					}
+				} else {
+					$params .= sprintf(' %s="%s" ', $key, $value);
+				}
+			}
+		}
 
-        return sprintf(
-            '<option value="%s"%s %s>%s</option>',
-            $this->escapeHtml($option['value']),
-            $selectedHtml,
-            $params,
-            $this->escapeHtml($option['label'])
-        );
-    }
+		return sprintf(
+			'<option value="%s"%s %s>%s</option>',
+			$this->escapeHtml($option['value']),
+			$selectedHtml,
+			$params,
+			$this->escapeHtml($option['label'])
+		);
+	}
 
-    /**
-     * Alias for toHtml()
-     *
-     * @return string
-     */
-    function getHtml()
-    {
-        return $this->toHtml();
-    }
+	/**
+	 * Alias for toHtml()
+	 *
+	 * @return string
+	 */
+	function getHtml()
+	{
+		return $this->toHtml();
+	}
 
-    /**
-     * Calculate CRC32 hash for option value
-     *
-     * @param string $optionValue Value of the option
-     * @return string
-     */
-    function calcOptionHash($optionValue)
-    {
-        return sprintf('%u', crc32($this->getName() . $this->getId() . $optionValue));
-    }
+	/**
+	 * Calculate CRC32 hash for option value
+	 *
+	 * @param string $optionValue Value of the option
+	 * @return string
+	 */
+	function calcOptionHash($optionValue)
+	{
+		return sprintf('%u', crc32($this->getName() . $this->getId() . $optionValue));
+	}
 }
