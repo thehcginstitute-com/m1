b586d4f4af7f8f09ef7e34ca47d8f2e805bc0db6
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-21 13:18:50 +0100

    https://github.com/thehcginstitute-com/m1/issues/680

--- app/code/core/Mage/Catalog/Block/Product/List.php
+++ app/code/core/Mage/Catalog/Block/Product/List.php
@@ -148,6 +148,8 @@ class Mage_Catalog_Block_Product_List extends Mage_Catalog_Block_Product_Abstrac
      * 2024-09-21 Dmitrii Fediuk https://upwork.com/fl/mage2pro
 	 * "The names of arguments in `<action method="<methodName>">` calls should match the `methodName`'s arguments":
 	 * https://github.com/thehcginstitute-com/m1/issues/680
+	 * @used-by https://github.com/thehcginstitute-com/m1/tree/2024-09-21--6/app/design/frontend/default/mobileshoppe/layout/catalog.xml#L48
+	 * @used-by https://github.com/thehcginstitute-com/m1/tree/2024-09-21--6/app/design/frontend/default/mobileshoppe/layout/catalog.xml#L83
 	 */
 	final function setToolbarBlockName(string $v):void {$this[self::$TOOLBAR_BLOCK_NAME] = $v;}
 
