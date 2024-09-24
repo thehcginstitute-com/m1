7cebd8b4998b390648b76577546fe671dfbb7310
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-24 11:53:19 +0100

    https://github.com/thehcginstitute-com/m1/issues/686

--- app/code/local/Df/Xml/lib/cdata.php
+++ app/code/local/Df/Xml/lib/cdata.php
@@ -10,7 +10,7 @@ function df_cdata(string $s):string {return df_cdata_m()->mark($s);}
 /**
  * 2024-09-24
  * @used-by df_cdata()
- * @used-by \Df\Xml\G::importString()
+ * @used-by Df\Xml\G::importString()
  */
 function df_cdata_m():M {return dfcf(function():M {return new M('[[', ']]');});}
 
@@ -24,7 +24,7 @@ function df_cdata_raw_if_needed(string $s):string {return !df_needs_cdata($s) ? 
  * 2021-12-12 https://3v4l.org/3SDsT
  * @see df_cdata()
  * @used-by df_cdata_raw_if_needed()
- * @used-by \Df\Xml\G::importString()
+ * @used-by Df\Xml\G::importString()
  */
 function df_needs_cdata(string $s):bool {
 	$s1 = htmlspecialchars_decode($s, ENT_XML1 | ENT_QUOTES); /** @var string $s1 */
