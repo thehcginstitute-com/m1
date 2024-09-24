5c199f4b20acad261a2d40b47def7a3f635b8412
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-24 11:11:01 +0100

    https://github.com/thehcginstitute-com/m1/issues/686

--- app/code/local/Df/Core/lib/array/select/cached/method.php
+++ app/code/local/Df/Core/lib/array/select/cached/method.php
@@ -7,7 +7,7 @@ use Closure as F;
  * `object` as an argument type is not supported by PHP < 7.2: https://github.com/mage2pro/core/issues/174#user-content-object
  * 2024-06-03 We need to support PHP ≥ 7.1: https://github.com/mage2pro/core/issues/368
  * @see dfac()
- * @used-by \Df\Core\Visitor::r()
+ * @used-by Df\Core\Visitor::r()
  * @param object $o
  * @param string|string[] $k [optional]
  * @param mixed|callable|null $d [optional]
