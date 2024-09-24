c7ed36dd74c393da6221330d4a90b3a36d598a71
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-24 09:47:10 +0100

    https://github.com/mage2pro/core/issues/437

--- app/code/local/Df/Qa/lib/exception.php
+++ app/code/local/Df/Qa/lib/exception.php
@@ -67,7 +67,7 @@ function df_xf(T $t):T {while ($t->getPrevious()) {$t = $t->getPrevious();} retu
  * @used-by df_lxts()
  * @used-by df_message_error()
  * @used-by df_sprintf_strict()
- * @used-by df_xml_parse()
+ * @used-by df_xml_x()
  * @used-by Mage_Adminhtml_CustomerController::validateAction()
  * @used-by \Df\Qa\Failure\Error::check()
  * @used-by \Df\Qa\Failure\Error::log()
