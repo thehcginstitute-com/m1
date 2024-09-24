0d979c8066ecfdb008ba97cf561bf8758f8f5588
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-24 09:14:20 +0100

    refactoring

--- app/code/local/Df/Core/lib/array/unpack.php
+++ app/code/local/Df/Core/lib/array/unpack.php
@@ -1,9 +1,26 @@
 <?php
 /**
+ * 2022-11-26
+ * 1) https://3v4l.org/C09vn
+ * 2) It is similar to @see dfa_unpack(), but df_arg() does not call dfa_flatten().
+ * 3)
+ * 		[$v] => $v
+ * 		[[$v]] => [$v]
+ * 		[[$v1, $v2]] => [$v1, $v2]
+ * 		[$v1, $v2] => [$v1, $v2]
+ * 		[$v1, $v2, [$v3]] => [$v1, $v2, [$v3]] - The difference from @see dfa_unpack()
+ * 		[] => []
+ * @see df_args()
+ * @see dfa_unpack()
+ * @used-by df_contains()
+ * @return mixed|mixed[]
+ */
+function df_arg(array $a) {return isset($a[0]) && !isset($a[1]) ? $a[0] : $a;}
+
+/**
  * 2015-12-25
  * Этот загадочный метод призван заменить код вида: `is_array($a) ? $a : func_get_args()`.
  * Теперь можно писать так: df_args(func_get_args()).
- * 2024-02-03 "Port `df_args()` from `mage2pro/core`": https://github.com/thehcginstitute-com/m1/issues/348
  * @see df_arg()
  * @see dfa_unpack()
  * @used-by df_clean()
@@ -11,8 +28,24 @@
  * @used-by df_csv()
  * @used-by df_csv_pretty_quote()
  * @used-by df_format()
+ * @used-by df_mvar_name()
  * @used-by dfa_combine_self()
  * @used-by dfa_unset()
  * @see dfa_unpack()
  */
 function df_args(array $a):array {return !$a || !is_array($a[0]) ? $a : $a[0];}
+
+/**
+ * 2020-01-29
+ * 2022-11-26 It is similar to @see df_arg(), but dfa_unpack() also calls dfa_flatten().
+ * @see df_arg()
+ * @see df_args()
+ * 		[$v] => $v
+ * 		[[$v]] => [$v]
+ * 		[[$v1, $v2]] => [$v1, $v2]
+ * 		[$v1, $v2] => [$v1, $v2]
+ * 		[$v1, $v2, [$v3]] => [$v1, $v2, $v3] - The difference from @see df_arg()
+ * @used-by dfp_iia()
+ * @return mixed|mixed[]
+ */
+function dfa_unpack(array $a) {return !($c = count($a)) ? null : (1 === $c ? $a[0] : dfa_flatten($a));}
