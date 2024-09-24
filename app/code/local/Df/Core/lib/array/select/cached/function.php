149f0b05b891e223e6e36889d52b5e7f88d27159
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-24 11:10:54 +0100

    https://github.com/thehcginstitute-com/m1/issues/686

--- app/code/local/Df/Core/lib/array/select/cached/function.php
+++ app/code/local/Df/Core/lib/array/select/cached/function.php
@@ -4,9 +4,6 @@ use Closure as F;
 /**
  * 2020-01-29
  * @see dfaoc()
- * @used-by df_credentials()
- * @used-by dfe_portal_module()
- * @used-by Df\Framework\Request::extra()
  * @param string|string[] $k [optional]
  * @param mixed|callable|null $d [optional]
  * @return mixed
