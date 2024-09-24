63a4609246fcb87318139d60458c63340bfd896a
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-24 09:02:54 +0100

    refactoring

--- app/code/local/Df/Core/lib/text/n/prepend.php
+++ app/code/local/Df/Core/lib/text/n/prepend.php
@@ -2,6 +2,5 @@
 /**
  * 2015-12-25
  * @used-by \Df\Qa\Dumper::dumpObject()
- * @used-by \Dfe\Frontend\Block\ProductView\Css::_toHtml()
  */
 function df_n_prepend(string $s):string {return df_es($s) ? $s : "\n$s";}
