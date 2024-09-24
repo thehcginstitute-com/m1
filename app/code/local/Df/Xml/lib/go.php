018892d90d3556eb78f408cf4e2c5ea72a71f584
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-24 09:59:51 +0100

    https://github.com/mage2pro/core/issues/437

--- app/code/local/Df/Xml/lib/go.php
+++ app/code/local/Df/Xml/lib/go.php
@@ -3,9 +3,6 @@ use Df\Xml\G;
 
 /**
  * @used-by df_xml_g()
- * @used-by \Dfe\SecurePay\Refund::process()
- * @used-by \Dfe\Vantiv\Charge::pCharge()
- * @used-by \Dfe\Vantiv\Test\CaseT\Charge::t04()
  * @param array(string => string) $attr [optional]
  * @param array(string => mixed) $contents [optional]
  */
