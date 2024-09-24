7a32a7f65d51cd0fb5a014d6dd9157dc367e950f
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-24 11:39:29 +0100

    refactoring

--- app/code/local/Df/Core/lib/mage.php
+++ app/code/local/Df/Core/lib/mage.php
@@ -1,12 +1,13 @@
 <?php
+use Mage_Core_Helper_Http as Http;
+use Mage_Core_Helper_Url as Url;
+
 /**
  * @used-by df_visitor_ip()
- * @return Mage_Core_Helper_Http
  */
-function df_mage_http_h() {return Mage::helper('core/http');}
+function df_mage_http_h():Http {return Mage::helper('core/http');}
 
 /**
  * @used-by df_current_url()
- * @return Mage_Core_Helper_Url
  */
-function df_mage_url_h() {return Mage::helper('core/url');}
+function df_mage_url_h():Url {return Mage::helper('core/url');}
