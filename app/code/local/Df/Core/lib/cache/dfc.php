693a6ac43035d8d055f03d7e3182f786a293a18a
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-24 08:42:33 +0100

    https://github.com/mage2pro/core/issues/437

--- app/code/local/Df/Core/lib/cache/dfc.php
+++ app/code/local/Df/Core/lib/cache/dfc.php
@@ -118,6 +118,7 @@ function dfc($o, Closure $f, array $a = [], $unique = true, $offset = 0) {
  * 2017-01-02 Задавайте параметр $offset в том случае, когда dfc() вызывается опосредованно. Например, так делает @see dfac().
  * @see dfac()
  * @used-by df_category_children_map()
+ * @used-by df_cdata_m()
  * @used-by df_currency()
  * @used-by df_google_init_service_account()
  * @used-by df_module_file_read()
