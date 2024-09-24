03eb480451dd45454347ab0844502c89beac4f35
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-24 08:27:28 +0100

    refactoring

--- app/code/local/Df/Core/Helper/Text.php
+++ app/code/local/Df/Core/Helper/Text.php
@@ -1,9 +1,6 @@
 <?php
 namespace Df\Core\Helper;
 final class Text {
-	/** @used-by \Df\Core\Text\Regex::isSubjectMultiline() */
-	function isMultiline(string $s):bool {return df_contains($s, "\n") || df_contains($s, "\r");}
-
 	/**
 	 * @used-by df_quote_double()
 	 * @used-by df_quote_russian()
@@ -29,48 +26,6 @@ final class Text {
 		$f = function(string $s) use($quotes):string {return implode($s, $quotes);};
 		return !is_array($s) ? $f($s) : array_map($f, $s);
 	}
-
-	/**
-	 * 2015-03-03 Алгоритм аналогичен @see singleLine()
-	 * 2015-07-07
-	 * 1) Раньше алгоритм был таким: `strtr($text, "\r\n", '  ')`.
-	 * Однако он не совсем правилен, потому что если перенос строки записан в формате Windows
-	 * (то есть, в качестве переноса строки используется последовательность \r\n),
-	 * то прошлый алгоритм заменит эту последовательность на 2 пробела, а надо — на один:
-	 * «If given three arguments,
-	 * this function returns a copy of str where all occurrences of each (single-byte) character in from
-	 * have been translated to the corresponding character in to,
-	 * i.e., every occurrence of $from[$n] has been replaced with $to[$n],
-	 * where $n is a valid offset in both arguments.
-	 * If from and to have different lengths,
-	 * the extra characters in the longer of the two are ignored.
-	 * The length of str will be the same as the return value's.»
-	 * https://php.net/strtr
-	 * Новый алгоритм взял отсюда:  http://stackoverflow.com/a/20717751
-	 * 2021-12-13 @deprecated It is unused.
-	 */
-	function removeLineBreaks(string $s):string {return str_replace(["\r\n", "\r", "\n"], ' ', $s);}
-
-	/**
-	 * 2015-03-03 Алгоритм аналогичен @see self::removeLineBreaks()
-	 * 2015-07-07
-	 * Раньше алгоритм был таким: `strtr($text, "\r\n", '  ')`.
-	 * Однако он не совсем правилен, потому что если перенос строки записан в формате Windows
-	 * (то есть, в качестве переноса строки используется последовательность \r\n),
-	 * то прошлый алгоритм заменит эту последовательность на 2 пробела, а надо — на один:
-	 * «If given three arguments,
-	 * this function returns a copy of str where all occurrences of each (single-byte) character in from
-	 * have been translated to the corresponding character in to,
-	 * i.e., every occurrence of $from[$n] has been replaced with $to[$n],
-	 * where $n is a valid offset in both arguments.
-	 * If from and to have different lengths,
-	 * the extra characters in the longer of the two are ignored.
-	 * The length of str will be the same as the return value's.»
-	 * https://php.net/strtr
-	 * Новый алгоритм взял отсюда: http://stackoverflow.com/a/20717751
-	 * 2023-07-29 @deprecated It is unused.
-	 */
-	function singleLine(string $s):string {return str_replace(["\r\n", "\r", "\n", "\t"], ' ', $s);}
 
 	/**
 	 * @used-by df_quote_double()
