7f7f40522a532630a12df86c70f50e9d3fdba500
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-24 11:33:25 +0100

    https://github.com/thehcginstitute-com/m1/issues/686

--- app/code/local/Df/Core/lib/object/get-data.php
+++ app/code/local/Df/Core/lib/object/get-data.php
@@ -7,7 +7,7 @@ use Varien_Object as _DO;
  * 2024-05-14 "Port `df_gd()` from `mage2pro/core`": https://github.com/thehcginstitute-com/m1/issues/604
  * @used-by dfad()
  * @used-by dfa_remove_objects()
- * @used-by \Df\Qa\Dumper::dumpObject()
+ * @used-by Df\Qa\Dumper::dumpObject()
  * @param mixed|_DO $v
  * @param F|bool|mixed $onE [optional]
  * @return array(string => mixed)
@@ -18,7 +18,7 @@ function df_gd($v, $onE = true):array {return df_try(function() use($v) {return 
  * 2020-02-04
  * @used-by df_assert_gd()
  * @used-by df_call()
- * @used-by \Df\Qa\Dumper::dumpObject()
+ * @used-by Df\Qa\Dumper::dumpObject()
  * @param mixed $v
  * @return bool
  */
