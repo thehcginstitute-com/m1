02182f1fe51092bb83dded00baf755ebe034c8b8
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-24 11:20:00 +0100

    https://github.com/mage2pro/core/issues/437

--- app/code/local/Df/Framework/lib/module/enabled.php
+++ app/code/local/Df/Framework/lib/module/enabled.php
@@ -4,7 +4,7 @@ use Varien_Simplexml_Element as MX;
  * @used-by df_caller_entry_m()
  * @used-by df_log_l()
  */
-function df_module_enabled(string $m):bool {return dfcf(function($m) {
+function df_module_enabled(string $m):bool {return dfcf(function(string $m):bool {
 	$moduleConfig = Mage::app()->getConfig()->getModuleConfig($m); /** @var MX|null $moduleConfig */
 	return $moduleConfig && df_leaf_b($moduleConfig->{'active'});
 }, [$m]);}
