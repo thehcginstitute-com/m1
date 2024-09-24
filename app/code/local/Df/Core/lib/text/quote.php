97267bcd3d54cab6027e35607a153ca8b52a8ce8
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-24 09:09:23 +0100

    refactoring

--- app/code/local/Df/Core/lib/text/quote.php
+++ app/code/local/Df/Core/lib/text/quote.php
@@ -1,28 +1,23 @@
 <?php
 use Df\Core\Helper\Text as T;
-use Magento\Framework\Phrase as P;
 /**
  * 2015-11-22
  * 2022-10-31 @deprecated It is unused.
- * @param string|string[]|P|P[] $s
+ * @param string|string[] $s
  * @return string|string[]
  */
 function df_quote_double($s) {return df_t()->quote($s, T::QUOTE__DOUBLE);}
 
 /**
  * @used-by df_csv_pretty_quote()
- * @used-by \Df\Config\Backend::label()
- * @used-by \Df\Framework\Validator\Currency::message()
- * @param string|string[]|P|P[] $s
+ * @param string|string[] $s
  * @return string|string[]
  */
 function df_quote_russian($s) {return df_t()->quote($s, T::QUOTE__RUSSIAN);}
 
 /**
- * @used-by df_ejs()
- * @used-by \Df\Typography\Font::css()
- * @used-by \Dfe\Frontend\Block\ProductView\Css::customCss()
- * @param string|string[]|P|P[] $s
+ * @used-by df_ejs(
+ * @param string|string[] $s
  * @return string|string[]
  */
 function df_quote_single($s) {return df_t()->quote($s, T::QUOTE__SINGLE);}
