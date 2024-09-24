e8f341b049e6a24983f8ca951884257b41766052
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-24 11:34:50 +0100

    https://github.com/thehcginstitute-com/m1/issues/686

--- app/code/local/Df/Core/lib/text/n/explode.php
+++ app/code/local/Df/Core/lib/text/n/explode.php
@@ -5,7 +5,7 @@
  * @used-by df_parse_colon()
  * @used-by df_tab()
  * @used-by df_zf_http_last_req()
- * @used-by \Df\Core\Text\Regex::getSubjectSplitted()
+ * @used-by Df\Core\Text\Regex::getSubjectSplitted()
  * @return string[]
  */
 function df_explode_n(string $s):array {return explode("\n", df_normalize(df_trim($s)));}
