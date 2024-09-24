bd0e10d7f55f02dc7e17ea7dd67876b5142ea4c4
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-24 11:35:10 +0100

    https://github.com/thehcginstitute-com/m1/issues/686

--- app/code/local/Df/Core/lib/text/n/prepend.php
+++ app/code/local/Df/Core/lib/text/n/prepend.php
@@ -1,6 +1,6 @@
 <?php
 /**
  * 2015-12-25
- * @used-by \Df\Qa\Dumper::dumpObject()
+ * @used-by Df\Qa\Dumper::dumpObject()
  */
 function df_n_prepend(string $s):string {return df_es($s) ? $s : "\n$s";}
