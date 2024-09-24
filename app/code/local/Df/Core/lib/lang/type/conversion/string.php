a34c00d439d3fa16b169a4f564887414609fd759
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-24 11:16:00 +0100

    https://github.com/thehcginstitute-com/m1/issues/686

--- app/code/local/Df/Core/lib/lang/type/conversion/string.php
+++ app/code/local/Df/Core/lib/lang/type/conversion/string.php
@@ -16,8 +16,8 @@
  * @see df_dump()
  * @see df_is_stringable()
  * @used-by df_type()
- * @used-by \Df\Qa\Dumper::dumpObject()
- * @used-by \Df\Xml\G::importString()
+ * @used-by Df\Qa\Dumper::dumpObject()
+ * @used-by Df\Xml\G::importString()
  * @param mixed $v
  */
 function df_string($v):string {return df_dump(strval(df_assert_stringable($v)));}
